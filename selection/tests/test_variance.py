a569f619057cf2cf9e326962bb04f61a19c83dc6
Jonathan Taylor <jonathan.taylor@stanford.edu>
2013-12-13 11:34:09 -0800

    getting close to newton being more stable

--- selection/tests/test_variance.py
+++ selection/tests/test_variance.py
@@ -6,9 +6,9 @@ from selection.constraints import constraints
 from scipy.optimize import bisect
  
 from mpmath import mp
-mp.dps = 40
+mp.dps = 100
 
-n, p, s, sigma = 100, 200, 10, 10
+n, p, s, sigma = 500, 1000, 50, 10
 
 X = np.random.standard_normal((n,p)) + np.random.standard_normal(n)[:,None]
 X -= X.mean(0)[None,:]
@@ -53,11 +53,11 @@ class variance_estimator(object):
             state = self.sample[-1].copy()
         else:
             state = self.Y.copy()
-        self.sample = draw_truncated(self.Y, self.constraint,
+        self.sample = (draw_truncated(self.Y, self.constraint,
                                      ndraw=ndraw,
-                                     burnin=burnin)
+                                     burnin=burnin)**2).sum(1)
 
-    def objective(self, gamma):
+    def cumulants(self, gamma):
         """
         Compute 
 
@@ -70,51 +70,57 @@ class variance_estimator(object):
         where $F$ is the empirical distribution of `self.sample`.
 
         """
-        norm_squared = (self.sample**2).sum(1)
-        if gamma > 0:
-            M = norm_squared.max()
-            M0 = np.mean([mp.exp(gamma*(ns-M)) for ns in norm_squared])
-            M1 = np.mean([ns*mp.exp(gamma*(ns-M)) for ns in norm_squared])
-            M2 = np.mean([(ns**2)*mp.exp(gamma*(ns-M)) for ns in norm_squared])
-        else:
-            M = norm_squared.min()
-            M0 = np.mean([mp.exp(gamma*(ns-M)) for ns in norm_squared])
-            M1 = np.mean([ns*mp.exp(gamma*(ns-M)) for ns in norm_squared])
-            M2 = np.mean([(ns**2)*mp.exp(gamma*(ns-M)) for ns in norm_squared])
-            
-        return M0*mp.exp(gamma*M), M1/M0, (M2-M1**2)/M0
+        norm_squared = self.sample
+        M = norm_squared.mean()
+        M0 = float(np.mean([mp.exp(gamma*(ns-M)) for ns in norm_squared]))
+        M0 *= mp.exp(gamma*M)
+        M1 = np.mean([np.exp(float(gamma*ns+np.log(ns)-mp.log(M0))) for ns in norm_squared])
+        M2 = np.mean([np.exp(float(gamma*ns+2*np.log(ns)-mp.log(M0))) for ns in norm_squared])
+        return M0, M1, (M2-M1**2)
 
-    def newton(self, initial=0, niter=10):
+    def newton(self, initial=0, niter=20):
         """
         Match the gamma so that expected norm squared
         matches `(self.Y**2).sum()`.
         """
         gamma = initial
+        n2 = (self.Y**2).sum()
+        value = np.inf
         for _ in range(niter):
-            V, grad, hess = self.objective(gamma)
-            grad -= (self.Y**2).sum()
-            gamma = gamma - grad / hess
-            print grad, hess
-        sigma = np.sqrt(-1 / (2 * (gamma - 1/(2*self.sigma**2))))
+            V, grad, hess = self.cumulants(gamma)
+            grad -= n2
+            step = 1.
+            gamma_trial = gamma - step * grad / hess
+            objective_trial = self.cumulants(gamma_trial)[0]
+            while objective_trial - gamma_trial*n2 > value:
+                step /= 2.
+                gamma_trial = gamma - step * grad / hess
+
+            gamma = gamma_trial
+            sigma2 = self.sigma**2 / (1 - 2 * self.sigma**2 * gamma)
+            sigma = np.sqrt(sigma2)
+            print grad, hess, gamma, sigma2
         return sigma
 
     def bisection(self):
         def f(sigma_hat):
-            print 'here', sigma_hat
-            gamma = 1/(2*V.sigma**2) - 1 /(2*sigma_hat**2)
-            return V.objective(gamma)[1] - (V.Y**2).sum()
-        factor = 10
+            gamma = 1/(2*self.sigma**2) - 1 /(2*sigma_hat**2)
+            v = self.cumulants(gamma)[1] - (self.Y**2).sum()
+            print 'here', v, sigma_hat
+            print v
+        factor = 3
         try:
-            return bisect(f, self.sigma, factor*self.sigma, maxiter=5)
+            return bisect(f, self.sigma/factor, factor*self.sigma, maxiter=20)
         except ValueError:
-            factor *= 5
-            return bisect(f, self.sigma, factor*self.sigma, maxiter=5)
+            factor *= 2
+            return bisect(f, self.sigma/factor, factor*self.sigma, maxiter=20)
             
-
 initial = np.linalg.norm(Z) / np.sqrt(Z.shape[0])
-V = variance_estimator(Z, Z_constraint, initial=initial)
+V = variance_estimator(Z, Z_constraint, initial=sigma)
 V.draw_sample(ndraw=5000)
-print V.objective(0.01)
+print V.cumulants(0), (V.Y**2).sum()
+print V.cumulants(0.01)
+#ghat2 = V.newton()
 ghat = V.bisection()
 print ghat
 
