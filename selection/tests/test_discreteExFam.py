c92979b079df31e8baad8e2c67b1c1f148e1841d
Jonathan Taylor <jonathan.taylor@stanford.edu>
2014-05-13 12:25:54 -0700

    some renaming, made private API, edited docstrings

--- selection/tests/test_discreteExFam.py
+++ selection/tests/test_discreteExFam.py
@@ -2,41 +2,42 @@
 import numpy as np
 import nose.tools as nt
 from scipy.stats import poisson
-import discreteExFam; reload(discreteExFam)
-from discreteExFam import DiscreteExFam
+from selection.discrete_family import discrete_family
 
 
-X = np.arange(100)
-pois = DiscreteExFam(X, poisson.pmf(X, 1))
-tol = 1e-5
+def test_discreteExFam():
 
-print (pois.leftCutFromRight(theta=0.4618311,rightCut=(5,.5)), pois.test2RejectsLeft(theta=2.39,x=5,auxVar=.5))
-print pois.interval(x=5,alpha=.05,randomize=True,auxVar=.5)
+    X = np.arange(100)
+    pois = discrete_family(X, poisson.pmf(X, 1))
+    tol = 1e-5
 
-print abs(1-sum(pois.pdf(0)))
-pois.ccdf(0, 3, .4)
+    print (pois._leftCutFromRight(theta=0.4618311,rightCut=(5,.5)), pois._test2RejectsLeft(theta=2.39,observed=5,auxVar=.5))
+    print pois.interval(observed=5,alpha=.05,randomize=True,auxVar=.5)
 
-print pois.Var(np.log(2))
-print pois.Cov(np.log(2), lambda x: x, lambda x: x)
+    print abs(1-sum(pois.pdf(0)))
+    pois.ccdf(0, 3, .4)
 
-lc = pois.rightCutFromLeft(0, (0,.01))
-print (0,0.01), pois.leftCutFromRight(0, lc)
+    print pois.Var(np.log(2), lambda x: x)
+    print pois.Cov(np.log(2), lambda x: x, lambda x: x)
 
-pois.rightCutFromLeft(-10, (0,.01))
-#[pois.test2Cutoffs(t)[1] for t in range(-10,3)]
-pois.critCovFromLeft(-10, (0,.01))
+    lc = pois._rightCutFromLeft(0, (0,.01))
+    print (0,0.01), pois._leftCutFromRight(0, lc)
 
-pois.critCovFromLeft(0, (0,.01))
-pois.critCovFromRight(0, lc)
+    pois._rightCutFromLeft(-10, (0,.01))
+    #[pois.test2Cutoffs(t)[1] for t in range(-10,3)]
+    pois._critCovFromLeft(-10, (0,.01))
 
-pois.critCovFromLeft(5, (5, 1))
+    pois._critCovFromLeft(0, (0,.01))
+    pois._critCovFromRight(0, lc)
 
-pois.test2RejectsLeft(np.log(5),5)
-pois.test2RejectsRight(np.log(5),5)
+    pois._critCovFromLeft(5, (5, 1))
 
-pois.test2RejectsLeft(np.log(20),5)
-pois.test2RejectsRight(np.log(.1),5)
+    pois._test2RejectsLeft(np.log(5),5)
+    pois._test2RejectsRight(np.log(5),5)
 
-print pois.inter2Upper(5,auxVar=.5)
-print pois.interval(5,auxVar=.5)
+    pois._test2RejectsLeft(np.log(20),5)
+    pois._test2RejectsRight(np.log(.1),5)
+
+    print pois._inter2Upper(5,auxVar=.5)
+    print pois.interval(5,auxVar=.5)
 
