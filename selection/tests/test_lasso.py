3532f5e0bfd3a6c117aa670ab19f9fe2e9a668be
Jonathan Taylor <jonathan.taylor@stanford.edu>
2014-09-02 10:33:28 -0700

    new function to form constraints; fix broken test

--- selection/tests/test_lasso.py
+++ selection/tests/test_lasso.py
@@ -1,20 +1,21 @@
 import numpy as np
 from selection.lasso import lasso # , _howlong
 
-def test_class(n=100, p=20, frac=0.9):
+def test_class(n=100, p=20, frac=0.5):
     y = np.random.standard_normal(n)
     X = np.random.standard_normal((n,p))
-    L = lasso(y,X,frac=frac)
+    lam_theor = frac * np.fabs(np.dot(X.T, np.random.standard_normal((n,10000)))).max(0).mean()
+    L = lasso(y,X,lam=lam_theor)
     L.fit(tol=1.e-7)
     L.form_constraints()
     C = L.constraints
 
-    np.testing.assert_array_less(np.dot(L.constraints.inequality, L.y), L.constraints.inequality_offset)
+    np.testing.assert_array_less(np.dot(L.constraints.linear_part, L.y), L.constraints.offset)
 
     I = L.intervals
     P = L.active_pvalues
 
-    return L.centered_test, L.basic_test, L, C, I, P
+    return L, C, I, P
 
 # def test_fit_and_test(n=100, p=20, frac=0.9):
 
