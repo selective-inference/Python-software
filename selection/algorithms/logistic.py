c1d11e16ff0a151a3a2b777cf241366bf04b6eb1
Jonathan Taylor <jonathan.taylor@stanford.edu>
2015-08-28 14:19:17 -0700

    added preliminary version of intervals and unbiased estimates

--- selection/algorithms/logistic.py
+++ selection/algorithms/logistic.py
@@ -19,7 +19,7 @@ from selection.constraints.affine import (constraints, selection_interval,
 from selection.distributions.discrete_family import discrete_family
 from selection.algorithms.lasso import lasso as OLS_lasso, _constraint_from_data
 
-class lasso(OLS_lasso):
+class lasso(object):
 
     r"""
     A class for the LASSO for post-selection inference.
@@ -154,33 +154,17 @@ class lasso(OLS_lasso):
         X_E = self.X[:,self.active]
 
         self._linpred_unpenalized = np.dot(X_E, self._beta_unpenalized)
-        self._fitted_unpenalized = p_unpen = np.exp(self._linpred_unpenalized) / (1. + np.exp(self._linpred_unpenalized))
+        self._fitted_unpenalized = p_unpen = \
+            self.inverse_link(self._linpred_unpenalized)
         self._resid_unpenalized = self.y - self._fitted_unpenalized
-        self._weight_unpenalized = p_unpen * (1 - p_unpen)
-
-        # estimate covariance of (\bar{\beta}_E, X_{-E}^T(y-\pi_E(\bar{\beta}_E))
-        # \approx (\bar{\beta}_E, X_{-E}^T(y-\pi_E(\beta_E^*)) - C_E(\beta_E^*) (\bar{\beta}_E - \beta_E^*)
-        # \approx (\beta_E^* + Q_E(\beta^*_E)^{-1} X_E^T(y - \pi_E(\beta^*_E), X_{-E}^T(y-\pi_E(\beta_E^*)) - I_E(\beta_E^*) X_E^T(y - \pi_E(\beta^*_E))
+        self._weight_unpenalized = self.variance_function(p_unpen)
 
         Q_Ei = self.quadratic_form_inv # plugin estimate of Q_E(\beta_E^*)^{-1}
         I_E = self._irrepresentable # plugin estimate of C_E(\beta_E^*) Q_E(\beta_E^*)^{-1}
 
-        transform = np.zeros((p, p))
-        transform[:s,self.active] = Q_Ei
-        transform[s:,self.inactive] = np.identity(p-s)
-        transform[s:,self.active] = -I_E
-
-        # this is the covariance used in the constraint
-
-        cov_asymp = np.zeros((2*p, 2*p))
-        cov_asymp[:p,:p] = np.dot(transform, np.dot(self.covariance, transform.T))
-        cov_asymp[p:,p:] = np.dot(self.randomization_chol,
-                                  self.randomization_chol.T)
-
         linear_part = np.zeros((s + 2*(p-s), 2*p))
         linear_part[:s,:s] = -np.diag(self.z_E) 
         linear_part[:s,p+self.active] = self.z_E[:,None] * Q_Ei
-
         linear_part[s:p,s:p] = np.identity(p-s)
         linear_part[s:p,p+self.active] = I_E
         linear_part[s:p,p+self.inactive] = -np.identity(p-s)
@@ -193,7 +177,7 @@ class lasso(OLS_lasso):
 
         self._constraints = constraints(linear_part,
                                         offset,
-                                        covariance=cov_asymp)
+                                        covariance=self.covariance)
 
         DEBUG = False
         if DEBUG: 
@@ -233,34 +217,101 @@ class lasso(OLS_lasso):
         if not self._constraints(self.feasible):
             warnings.warn('initial point not feasible for constraints')
 
+    def inverse_link(self, eta):
+        return np.exp(eta) / (1 + np.exp(eta))
+
+    def variance_function(self, pi):
+        return pi * (1 - pi)
+
     @property
-    def covariance(self, doc="Covariance of sufficient statistic $X^Ty$."):
+    def covariance(self, doc="Estimate of covariance of statistical functional."):
         if not hasattr(self, "_cov"):
 
             # nonparametric bootstrap for covariance of X^Ty
 
             X, y = self.X, self. y
             n, p = X.shape
-            nsample = 2000
 
-            def pi(X):
-                w = np.exp(np.dot(X[:,self.active], self._beta_unpenalized))
-                return w / (1 + w)
+            use_parametric = True
+            if not use_parametric:
+                nsample = 2000
+                _mean_cum = 0
+                _cov_boot = np.zeros((p, p))
 
-            _mean_cum = 0
-            self._cov = np.zeros((p, p))
-            
-            for _ in range(nsample):
-                indices = np.random.choice(n, size=(n,), replace=True)
-                y_star = y[indices]
-                X_star = X[indices]
-                Z_star = np.dot(X_star.T, y_star - pi(X_star))
-                _mean_cum += Z_star
-                self._cov += np.multiply.outer(Z_star, Z_star)
-            self._cov /= nsample
-            _mean = _mean_cum / nsample
-            self._cov -= np.multiply.outer(_mean, _mean)
+                for _ in range(nsample):
+                    indices = np.random.choice(n, size=(n,), replace=True)
+                    y_star = y[indices]
+                    X_star = X[indices]
+                    Z_star = np.dot(X_star.T, y_star - 
+                                    self.inverse_link(np.dot(X_star[:,self.active], 
+                                                             self._beta_unpenalized)))
+                    _mean_cum += Z_star
+                    _cov_boot += np.multiply.outer(Z_star, Z_star)
+                _cov_boot /= nsample
+                _mean = _mean_cum / nsample
+                _cov_boot -= np.multiply.outer(_mean, _mean)
+                cov_XtY = _cov_boot
+            else:
+                cov_XtY = np.dot(X.T, self._weight_unpenalized[:, None] * X)
+
+            # the noisier unbiased estimate
+            # is equal to the original estimate
+            # plus a linear function of the randomization
+
+            # this is the negative of that linear function
+
+            Q_Ei = self.quadratic_form_inv # plugin estimate of Q_E(\beta_E^*)^{-1}
+            I_E = self._irrepresentable # plugin estimate of C_E(\beta_E^*) Q_E(\beta_E^*)^{-1}
+
+            sparsity = s = self.active.shape[0]
+
+            self._transform = np.zeros((p, p))
+            self._transform[:s,self.active] = Q_Ei
+            self._transform[s:,self.inactive] = np.identity(p-s)
+            self._transform[s:,self.active] = -I_E
+
+            # now form covariance 
+
+            self._cov = np.zeros((2*p,2*p))
+            self._cov[:p,:p] = np.dot(self._transform, np.dot(cov_XtY, self._transform.T))
+            self._cov[p:,p:] = self.randomization_covariance
+
         return self._cov
+
+    @property
+    def unbiased_estimate(self, doc=("Selectively unbiased estimate of first half of" + 
+                                     " statistical functional.")):
+        if not hasattr(self, "_unbiased_estimate"):
+            n, p = self.X.shape
+            _selective_unbiased_estimate = 0
+            linear_func = np.zeros((p, 2*p))
+            linear_func[:,:p] = np.identity(p)
+            conditional_law = self.constraints.conditional(linear_func,
+                                                           self.statistical_func[:p])
+            
+            burnin = 10000
+            ndraw = 30000
+
+            sample = sample_from_constraints(conditional_law,
+                                             self.feasible,
+                                             self.feasible,
+                                             burnin=burnin,
+                                             ndraw=ndraw)[:,p:]
+
+            C = self.covariance[:p,:p]
+            Cr = self.randomization_covariance
+            L = -self._transform
+
+            resid_forming_matrix = R = \
+                np.dot(C, 
+                        np.linalg.pinv(C + np.dot(L, np.dot(Cr, L.T))))
+            self._debiasing_matrix = np.linalg.pinv(np.identity(p) - R)
+
+            self._unbiased_estimate = (self.statistical_func[:p] -
+                                       np.mean(np.dot(sample, np.dot(L.T, self._debiasing_matrix.T)), 0))
+            self.constraints.mean[:p] = self._unbiased_estimate
+
+        return self._unbiased_estimate
 
     @property
     def quadratic_form(self, doc="Quadratic form in active block of KKT conditions."):
@@ -326,6 +377,7 @@ class lasso(OLS_lasso):
     def hypothesis_test(self, linear_func, null_value=0,
                         alternative='twosided',
                         saturated=True,
+                        compute_interval=True,
                         ndraw=8000,
                         burnin=2000):
         """
@@ -357,6 +409,9 @@ class lasso(OLS_lasso):
             One "selected" model assumes this has mean 0.
             The saturated model does not make this assumption.
 
+        compute_interval : bool
+             Compute a selective interval?
+
         ndraw : int (optional)
             Defaults to 8000.
 
@@ -366,8 +421,13 @@ class lasso(OLS_lasso):
         Returns
         -------
 
-        pvalue : np.float
+        pvalue : float
               P-value based on specified alternative.
+
+        interval : (float, float)
+              If compute_interval, returns a selective
+              interval for that linear functional of the data.
+              Otherwise, returns (np.nan, np.nan).
 
         """
 
@@ -392,44 +452,158 @@ class lasso(OLS_lasso):
         expanded_linear_func = np.zeros(2*p)
         expanded_linear_func[:s] = linear_func
 
-        conditioning_func = np.dot(conditioning_func, np.linalg.pinv(self.constraints.covariance))
+        conditioning_func = np.dot(conditioning_func, np.linalg.pinv(self.covariance))
 
-        conditional_con = self.constraints.conditional(conditioning_func,
+        conditional_law = self.constraints.conditional(conditioning_func,
                                                        np.dot(conditioning_func, self.statistical_func))
         if not self.constraints(self.feasible):
             raise ValueError('need a feasible point for sampling')
 
-        _, _, _, family = gibbs_test(conditional_con,
-                                     self.feasible,
-                                     expanded_linear_func,
-                                     UMPU=False,
-                                     sigma_known=True,
-                                     ndraw=ndraw,
-                                     burnin=burnin,
-                                     alternative=alternative)
+        sample = sample_from_constraints(conditional_law,
+                                         self.feasible,
+                                         expanded_linear_func,
+                                         ndraw=ndraw,
+                                         burnin=burnin)
+
+        W = np.exp(-np.dot(sample, np.dot(np.linalg.pinv(self.constraints.covariance),
+                                             self.constraints.mean)))
+        family = discrete_family(np.dot(sample, expanded_linear_func),
+                                 W)
 
         observed = (expanded_linear_func * self.statistical_func).sum()
-        if alternative == 'greater':
-            pval = family.ccdf(0, observed)
-        elif alternative == 'less':
-            pval = family.cdf(0, observed)
+
+        if compute_interval:
+            eta = expanded_linear_func
+            lower_lim, upper_lim = family.equal_tailed_interval(observed)
+            lower_lim_final = np.dot(eta, np.dot(self.covariance, eta)) * lower_lim
+            upper_lim_final = np.dot(eta, np.dot(self.covariance, eta)) * upper_lim
+
+            L_nominal = observed - 1.96 * np.sqrt((eta * np.dot(self.covariance, eta)).sum())
+            U_nominal = observed + 1.96 * np.sqrt((eta * np.dot(self.covariance, eta)).sum())
+            interval = (lower_lim_final, upper_lim_final)
+            print interval, (L_nominal, U_nominal)
         else:
-            pval = family.cdf(0, observed)
+            interval = (np.nan, np.nan)
+
+        if alternative == 'greater':
+            pval = family.ccdf(null_value, observed)
+        elif alternative == 'less':
+            pval = family.cdf(null_value, observed)
+        else:
+            pval = family.cdf(null_value, observed)
             pval = 2 * min(pval, 1 - pval)
-        return pval
+        return pval, interval
+
+def instance(n=100, p=200, s=7, rho=0.3, snr=7,
+             random_signs=False, df=np.inf,
+             scale=True, center=True):
+    """
+    A testing instance for the LASSO.
+    Design is equi-correlated in the population,
+    normalized to have columns of norm 1.
+
+    Parameters
+    ----------
+
+    n : int
+        Sample size
+
+    p : int
+        Number of features
+
+    s : int
+        True sparsity
+
+    rho : float
+        Equicorrelation value (must be in interval [0,1])
+
+    snr : float
+        Size of each coefficient
+
+    random_signs : bool
+        If true, assign random signs to coefficients.
+        Else they are all positive.
+
+    df : int
+        Degrees of freedom for noise (from T distribution).
+
+    Returns
+    -------
+
+    X : np.float((n,p))
+        Design matrix.
+
+    y : np.float(n)
+        Response vector.
+
+    beta : np.float(p)
+        True coefficients.
+
+    active : np.int(s)
+        Non-zero pattern.
+
+    """
+
+    X = (np.sqrt(1-rho) * np.random.standard_normal((n,p)) + 
+        np.sqrt(rho) * np.random.standard_normal(n)[:,None])
+    if center:
+        X -= X.mean(0)[None,:]
+    if scale:
+        X /= X.std(0)[None,:]
+    X /= np.sqrt(n)
+    beta = np.zeros(p) 
+    beta[:s] = snr 
+    if random_signs:
+        beta[:s] *= (2 * np.random.binomial(1, 0.5, size=(s,)) - 1.)
+    active = np.zeros(p, np.bool)
+    active[:s] = True
+
+    # noise model
+
+    def _noise(n, df=np.inf):
+        if df == np.inf:
+            return np.random.standard_normal(n)
+        else:
+            sd_t = np.std(tdist.rvs(df,size=50000))
+            return tdist.rvs(df, size=n) / sd_t
+
+    eta = linpred = np.dot(X, beta) 
+    pi = np.exp(eta) / (1 + np.exp(eta))
+    Y = np.random.binomial(1, pi)
+    return X, Y, beta, np.nonzero(active)[0]
+
 
 if __name__ == "__main__":
-    from selection.algorithms.lasso import instance
+    # from selection.algorithms.lasso import instance
 
-    def simulate(n=100, p=20):
+    def simulate(n=400, p=20, burnin=2000, ndraw=8000,
+                 compute_interval=False):
 
-        X, y = instance(n=n,p=p)[:2]
+        X, y, beta, active = instance(n=n,p=p,snr=8, scale=False, center=False)
         n, p = X.shape
 
-        ybin = np.random.binomial(1, 0.5, size=(n,))
-        L = lasso(ybin, X, 0.03 * np.sqrt(n), 
-                  randomization_covariance = 0.2 * np.dot(X.T, X) / 4.)
+        ybin = y 
+        #ybin = np.random.binomial(1, 0.5, size=(n,))
+        L = lasso(ybin, X, 0.02 * np.sqrt(n), 
+                  randomization_covariance = 0.4 * np.diag(np.diag(np.dot(X.T, X))) / 4.)
         L.fit()
-        print L.active.shape
-        v = np.ones_like(L.active)
-        return L.hypothesis_test(v), L
+        if (set(range(7)).issubset(L.active) and 
+            L.active.shape[0] > 7):
+            # L.constraints.mean[:p] = 0 * L.unbiased_estimate
+            
+            v = np.zeros_like(L.active)
+            v[7] = 1.
+            P0, interval = L.hypothesis_test(v, burnin=burnin, ndraw=ndraw,
+                                             compute_interval=compute_interval,
+                                             saturated=False)
+            target = (beta[L.active]*v).sum()
+            estimate = (L.unbiased_estimate[:L.active.shape[0]]*v).sum()
+            low, hi = interval
+
+            v = np.zeros_like(L.active)
+            v[0] = 1.
+            PA, _ = L.hypothesis_test(v, burnin=burnin, ndraw=ndraw,
+                                      compute_interval=compute_interval,
+                                      saturated=False)
+
+            return P0, PA, L
