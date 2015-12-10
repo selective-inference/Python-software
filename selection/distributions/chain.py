65955aac057d598dfb44b351611afe7219a5a987
Jonathan Taylor <jonathan.taylor@stanford.edu>
2015-12-10 11:16:42 -0800

    besag and clifford tests

---
+++ selection/distributions/chain.py
@@ -0,0 +1,199 @@
+import numpy as np
+
+class markov_chain(object):
+
+    # API
+
+    # A Markov chain is iterable
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.forward_step()
+
+    def forward_step(self):
+        raise NotImplementedError('abstract method')
+
+    def backward_step(self):
+        raise NotImplementedError('abstract method - not all chains will implement this')
+
+    # A Markov chain has a state
+
+    def get_state(self):
+        return self._state
+
+    def set_state(self, state):
+        self._state = state
+
+    state = property(get_state, set_state)
+
+class reversible_markov_chain(markov_chain):
+
+    # For reversible chains forward_step
+    # and backward_step are the same
+
+    def step(self):
+        return NotImplementedError('abstract method')
+
+    def forward_step(self):
+        return self.step()
+
+    def backward_step(self):
+        return self.step()
+
+def parallel_test(reversible_chain, null_state, test_statistic, ndraw=20):
+    """
+
+    Besag and Clifford's parallel test for reversible
+    Markov chains.
+
+    Parameters
+    ----------
+
+    reversible_chain : iterable
+        An object implementing a Markov chain,
+        with `forward_step` and `backward_step` methods.
+
+    null_state : object
+        An object nominally drawn from the
+        stationary distribution.
+
+    test_statistic : callable
+        A test statistic to compute on each state
+        of the chain. The overall
+        test statistic is the ranking of
+        `test_statistic(state)` in a sample
+        of `ndraw` steps of the chain.
+        
+    ndraw : int
+        How many total draws of the chain should be made?
+        Includes `state` as one of these draws.
+
+    Returns
+    -------
+
+    rank : int
+        How many of the draws had a test statistic
+        less than the observed value?
+
+    Notes
+    -----
+
+    This does not handle ties in any clever way at the moment.
+
+    """
+
+    chain = reversible_chain
+
+    observed = test_statistic(null_state)
+
+    results = []
+
+    old_state, chain.state = chain.state, null_state
+    
+    intermed_state = chain.backward_step()
+    
+    for _ in range(ndraw-1):
+        # take a step from intermediate state
+        chain.forward_step()
+
+        # compute the test statistic
+        results.append(test_statistic(chain.state))
+
+        # reset the state to the intermediate state
+        chain.state = intermed_state
+
+    results = sorted(results)
+
+    rank = np.sum([observed < r for r in results]) 
+    
+    # reset the chain's state to previous value
+
+    chain.state = old_state
+
+    return rank
+
+# make sure nose does not try to test this function
+parallel_test.__test__ = False
+
+def serial_test(reversible_chain, null_state, test_statistic, ndraw=20):
+    """
+
+    Besag and Clifford's parallel test for reversible
+    Markov chains.
+
+    Parameters
+    ----------
+
+    reversible_chain : iterable
+        An object implementing a Markov chain,
+        with next method returning current state.
+
+    null_state : object
+        An object nominally drawn from the
+        stationary distribution.
+
+    test_statistic : callable
+        A test statistic to compute on each state
+        of the chain. The overall
+        test statistic is the ranking of
+        `test_statistic(state)` in a sample
+        of `ndraw` steps of the chain.
+        
+    ndraw : int
+        How many total draws of the chain should be made?
+        Includes `state` as one of these draws.
+
+    Returns
+    -------
+
+    rank : int
+        How many of the draws had a test statistic
+        less than the observed value?
+
+    Notes
+    -----
+
+    This does not handle ties in any clever way at the moment.
+
+    """
+
+    chain = reversible_chain
+
+    observed = test_statistic(null_state)
+
+    results = []
+
+    old_state, chain.state = chain.state, null_state
+    
+    random_idx = np.random.random_integers(low=0, high=ndraw-1)
+
+    # go forward from null_state
+
+    for _ in range(random_idx):
+        chain.forward_step()
+
+        # compute the test statistic
+        results.append(test_statistic(chain.state))
+
+    # reset the state
+
+    chain.state = null_state
+
+    # go backward from null_state
+
+    for _ in range(ndraw - 1 - random_idx):
+        chain.backward_step()
+
+        # compute the test statistic
+        results.append(test_statistic(chain.state))
+
+    results = sorted(results)
+
+    rank = np.sum([observed < r for r in results]) 
+    
+    return rank
+
+# make sure nose does not try to test this function
+serial_test.__test__ = False
