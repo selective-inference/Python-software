Gradient corrected. Added bootstrap.

selection/sampling/randomized/losses/gaussian.py
@@ -80,13 +80,27 @@ class gaussian_Xfixed(selective_loss):
         self.linear_part = linear_part
 
 
-    def proposal(self, data):
+    def proposal(self, data, val):
         n, p = self.X.shape
-        stepsize = 2. / np.sqrt(n)
-        new = data + stepsize * np.dot(self.R, 
-                                       self.sigma * np.random.standard_normal(n))
+        #stepsize = 2. / np.sqrt(n)  # originally 2. / np.sqrt(n)
+
+        # new = data + stepsize * np.dot(self.R,
+        #                               self.sigma * np.random.standard_normal(n))
+
+        stepsize = 5./n
+        sign_vector =  np.sign(val)
+
+        grad_log_pi = -(data + np.dot(self.X,sign_vector))
+
+        #grad_log_pi = 0
+
+        new = data + np.dot(self.R,
+                            (stepsize*grad_log_pi) + (np.sqrt(2*stepsize)*np.random.standard_normal(data.shape[0])))
+
         log_transition_p = self.logpdf(new) - self.logpdf(data)
+
         return new, log_transition_p
+
 
     def logpdf(self, y):
         ### Up to some constant...
@@ -94,6 +108,7 @@ class gaussian_Xfixed(selective_loss):
 
     def update_proposal(self, state, proposal, logpdf):
         pass
+
 
 
 class sqrt_Lasso_Xfixed(gaussian_Xfixed):

selection/sampling/randomized/losses/lasso_randomX.py
@@ -74,6 +74,7 @@ class lasso_randomX(selective_loss):
 
             self._XTX = np.dot(self.X.T,self.X)
             self._XETXE = np.dot(X_E.T,X_E)
+
             self._XTXE = np.dot(self.X.T, X_E)
 
             #loss_E = logistic_loss(X_E, self.y)
@@ -109,7 +110,7 @@ class lasso_randomX(selective_loss):
 
             X, y = self.X, self.y
             n, p = X.shape
-            nsample = 2000
+            nsample = 5000
 
             active = self.active
             inactive = ~active
@@ -121,22 +122,32 @@ class lasso_randomX(selective_loss):
             #    return w / (1 + w)
 
             _mean_cum_Z = 0
-            self._cov_XTepsilon = np.zeros((p, p))    # this will become the bootstrapped covaraince of X^T\epsilon
-            self._cov_XETepsilon = np.zeros((self.size_active, self.size_active)) # this will become the bootstrapped covariance of X_E^T\epsilon
+            # The following will become the bootstrapped covariance of X^T\epsilon.
+            self._cov_XTepsilon = np.zeros((p, p))
+            # The following will become the bootstrapped covariance of X_E^T\epsilon.
+            self._cov_XETepsilon = np.zeros((self.size_active, self.size_active))
 
             _mean_cum_N = 0
-            self._cov_N=np.zeros((p-self.size_active, p-self.size_active))  # this will become the bootstrapped covariance of
-                                                                            # the null statistic X_{-E}^T(y-X_E\bar{\beta}_E)
+            # The following will become the bootstrapped covariance of the null statistic X_{-E}^T(y-X_E\bar{\beta}_E).
+            self._cov_N = np.zeros((p-self.size_active, p-self.size_active))
 
             _mean_cum_beta_bar = 0
             # bootstrapped covariance of \bar{\beta}_E, (X_E^{*T}X_E^*)^{-1}X_E^{*T}(y^*-X_E^*\bar{\beta}_E)
-            self._cov_beta_bar=np.zeros((self.size_active, self.size_active))
+            self._cov_beta_bar = np.zeros((self.size_active, self.size_active))
+
+            self._XTX_b = np.zeros((p,p))
+
 
             for _ in range(nsample):
                 indices = np.random.choice(n, size=(n,), replace=True)
                 y_star = y[indices]
                 X_star = X[indices]
-                Z_star = np.dot(X_star.T, y_star - pi(X_star))  # X^{*T}(y^*-X^{*T}_E\bar{\beta}_E)
+
+                self._XTX_b += np.dot(X_star.T, X_star)
+
+                #Z_star = np.dot(X_star.T, y_star - pi(X_star))  # X^{*T}(y^*-X^{*T}_E\bar{\beta}_E)
+                Z_star = np.dot(X_star.T, y_star - np.dot(X_star[:, self.active], self._beta_unpenalized))
+
                 _mean_cum_Z += Z_star
                 self._cov_XTepsilon += np.multiply.outer(Z_star, Z_star)
 
@@ -147,13 +158,16 @@ class lasso_randomX(selective_loss):
                 self._cov_N += np.multiply.outer(N_star, N_star)
 
                 beta_star =  np.dot(mat_XEstar, Z_star[active,])
+                beta_star
+                #np.linalg.lstsq(X_star[:, self.active], y_star)[0]-self._beta_unpenalized
                 _mean_cum_beta_bar += beta_star
                 self._cov_beta_bar += np.multiply.outer(beta_star, beta_star)
 
-
+            self._XTX_b /= nsample
 
             self._cov_XTepsilon /= nsample
             _mean_Z = _mean_cum_Z / nsample
+            print 'mean Z', _mean_Z
             self._cov_XTepsilon -= np.multiply.outer(_mean_Z, _mean_Z)
 
             self._cov_N /= nsample
@@ -162,12 +176,13 @@ class lasso_randomX(selective_loss):
             self._inv_cov_N = np.linalg.inv(self._cov_N)
 
             # to get _cov_XETepsilon we need to get [active,:]\times[:,active] block of _cov_XTepsilon
-            mat = self._cov_XTepsilon[self.active,:]
-            self._cov_XETepsilon = mat[:,self.active]
+            #mat = self._cov_XTepsilon[self.active,:]
+            self._cov_XETepsilon = self._cov_XTepsilon[self.active][:,self.active]
 
 
             self._cov_beta_bar /= nsample
             _mean_beta_star = _mean_cum_beta_bar/nsample
+            print 'mean beta', _mean_beta_star
             self._cov_beta_bar -= np.multiply.outer(_mean_beta_star, _mean_beta_star)
             self._inv_cov_beta_bar = np.linalg.inv(self._cov_beta_bar)
 
@@ -198,18 +213,25 @@ class lasso_randomX(selective_loss):
             self.bootstrap_covariance()
 
         # g = -(data - np.dot(self._cov, beta))
+
         data1 = data.copy()
 
-        data0 = data1[range(self.size_active)]  # \bar{beta}_E, the first |E| coordinates of 'data' vector
+        #data0 = data1[range(self.size_active)].copy()  # \bar{beta}_E, the first |E| coordinates of 'data' vector
 
-        data1[range(self.size_active), ] = 0 # last p-|E| coordinates of data vector kept, first |E| become zeros
+        data1[:self.size_active] = 0 # last p-|E| coordinates of data vector kept, first |E| become zeros
                                              # (0, N), N is the null statistic, N=X_{-E}^Y(y-X_E\bar{\beta}_E)
-        g = - data1 + np.dot(self._XTXE, beta[range(self.size_active)]-data0)
+        #print data1
+
+        # g = - data1 + np.dot(self._XTXE, beta[self.active]-data[:self.size_active])
+
+        g = - data1 + np.dot(self._XTX_b[:,  self.active], beta[self.active]-data[:self.size_active])
+
+        #g =  - data1 + np.dot(self._XTXE, beta[self.active]-data[:self.size_active])
 
         return g
 
 
-    def hessian(self): #, data, beta):
+    def hessian(self):#, data, beta):
         """
         hessian is constant in this case.
         """
@@ -219,7 +241,7 @@ class lasso_randomX(selective_loss):
         return self._XTX
 
 
-    def setup_sampling(self, data, mean, linear_part, value):
+    def setup_sampling(self, data, beta, linear_part, value):
         """
         Set up the sampling conditioning on the linear constraints L * data = value
 
@@ -248,8 +270,9 @@ class lasso_randomX(selective_loss):
         I = np.identity(linear_part.shape[1])
 
         self.data = data
-        self.mean = mean
+        self.beta = beta[self.active]
 
+        # print 'beta', self.beta
         # L(I-P)=LR=0, hence for new_data = data+R*proposal, we have L*new_data = L*data+LR*proposal = L*data=constant
         self.R = I - P
 
@@ -258,20 +281,61 @@ class lasso_randomX(selective_loss):
         self.linear_part = linear_part
 
 
-    def proposal(self, data):
+    def proposal(self, data, val):
         # if not hasattr(self, "L"):  # don't know what this is for
         #    self.bootstrap_covariance()
 
         n, p = self.X.shape
-        stepsize = 1. / np.sqrt(p)
+        stepsize = 15. / np.sqrt(p)   # 20 for the selected model
 
+        # stepsize=15./p
+
+        # stepsize = 1/float(p)
         # the new data point proposed will change the current one only along the direction
         # perpendicular to the column space of L^T (or the residual leftover after projection onto the
         # column space of L^T)
 
-        new = data + stepsize * np.dot(self.R,
-                                       np.random.standard_normal(p))
+        # new = data + stepsize * np.dot(self.R,
+        #                               np.random.standard_normal(p))
+
+
+        ## bootstrap
+        active = self.active
+        inactive = ~active
+        size_active = self.size_active
+        size_inactive = data.shape[0] - size_active
+
+        indices = np.random.choice(n, size=(n,), replace=True)
+        y_star = self.y[indices]
+        X_star = self.X[indices]
+        X_star_E = X_star[:,active]
+
+        mat_XEstar = np.linalg.inv(np.dot(X_star_E.T, X_star_E))  # (X^{*T}_E X^*_E)^{-1}
+        Z_star = np.dot(X_star_E.T, y_star - np.dot(X_star_E, self._beta_unpenalized))  # X^{*T}_E(y^*-X^{*T}_E\bar{\beta}_E)
+
+        # selected, additionally bootstrap N
+        #Z_star = np.dot(X_star.T, y_star - np.dot(X_star[:,self.active], data[:size_active]))  # X^{*T}(y^*-X^{*T}_E\bar{\beta}_E)
+
+        #mat_XEstar = np.linalg.inv(np.dot(X_star[:,active].T, X_star[:,active]))  # (X^{*T}_E X^*_E)^{-1}
+        #mat_star = np.dot(np.dot(X_star[:, inactive].T, X_star[:,active]), mat_XEstar)
+        #N_star = Z_star[inactive, ]-np.dot(mat_star, Z_star[active, ])
+
+
+        #data_star = np.concatenate((Z_star,
+        #                           N_star-data[-size_inactive:]), axis=0)
+
+        # saturated
+
+        data_star = np.concatenate((np.dot(mat_XEstar,Z_star), np.zeros(size_inactive)), axis=0)
+
+        # data_star = np.concatenate((np.dot(mat_XEstar,Z_star), data[:size_inactive]), axis=0)
+
+
+        new = data + stepsize * np.dot(self.R, data_star)
+
+
         log_transition_p = self.logpdf(new) - self.logpdf(data)
+
         return new, log_transition_p
 
 
@@ -284,7 +348,7 @@ class lasso_randomX(selective_loss):
         # R_cut = mat[:,self.active]
         #print 'R_cut size', R_cut.shape
 
-        beta_unpen = data[range(self.size_active)]
+        beta_unpen = data[:self.size_active,]
 
         # beta_unpen = sampling_data[range(self.size_active)]
         # cov_beta_unpen = np.dot(np.dot(R_cut, self._cov_beta_bar),R_cut.T)
@@ -293,7 +357,7 @@ class lasso_randomX(selective_loss):
         # Z = sampling_data[(self.size_active):data.shape[0]]
 
 
-        N = data[(self.size_active):data.shape[0]]
+        #N = data[(self.size_active):data.shape[0]]
 
         logl_beta_unpen = - np.dot(np.dot(beta_unpen.T, self._inv_cov_beta_bar), beta_unpen) # not true but what to put?
 
@@ -301,8 +365,11 @@ class lasso_randomX(selective_loss):
         #logl_beta_unpen = - np.dot(np.dot(beta_unpen.T, inv_cov_beta_unpen), beta_unpen)
         #print 'cov N size', self._covN.shape
         #print 'length Z', Z.shape
-        logl_N = - np.dot(np.dot(N.T, self._inv_cov_N), N)
-        return logl_beta_unpen + logl_N
+        #logl_N = - np.dot(np.dot(N.T, self._inv_cov_N), N)
+        return logl_beta_unpen #+ logl_N
+
+
+        #return - np.dot(np.dot((beta_unpen-self.beta).T, self._inv_cov_beta_bar), beta_unpen-self.beta)
 
         #return -((data-self.mean)*np.dot(np.linalg.pinv(self._cov), data-self.mean)).sum() / 2
 

selection/sampling/randomized/norms/lasso_new.py
@@ -6,6 +6,7 @@ from base import selective_penalty
 
 
 # same as lasso.py except uses Langevin Metropolis Hastings for simplex_step
+# relies on the fact that the randomization used is Laplace to compute the \grad log \pi in the Langevin update
 # Sampling from a log-concave distribution with Projected Langevin Monte Carlo (Bubeck et al)
 # http://arxiv.org/pdf/1507.02564v1.pdf
 
@@ -203,7 +204,7 @@ class selective_l1norm_new(rr.l1norm, selective_penalty):
         nactive = simplex.shape[0]
 
         #stepsize = 1/np.sqrt(nactive)
-        stepsize = 0.75/float(nactive)  # eta below
+        stepsize = 1/float(nactive)  # eta below
 
         # new for projected Langevin MCMC
 
@@ -255,7 +256,7 @@ class selective_l1norm_new(rr.l1norm, selective_penalty):
             self.accept_l1_part += 1
 
         return simplex
-
+        #return proposal
 
     def step_cube(self, state, randomization, gradient):
         """

selection/sampling/randomized/tests/pvalues_new.py
@@ -7,7 +7,8 @@ def pval_new(sampler,
          linear_part, # not used anymore
          data,
          nonzero,
-         Sigma):
+         Sigma,
+         true_beta):
     """
     The function computes the null and alternative pvalues for a regularized problem.
     The null p-values correspond to the coefficients whose corresponding predictors
@@ -61,7 +62,7 @@ def pval_new(sampler,
                                               # \beta_j^E corresponds to the predictor X[:,idx] in the model X[:,E]
             if linear_part is not None:
                 eta = np.zeros(size_active)
-                eta[j]=1    # eta = e_j \in R^{|E|}
+                eta[j] = 1    # eta = e_j \in R^{|E|}
 
                 sigma_eta_sq = np.dot(np.dot(eta.T, Sigma), eta)    # \eta^T \Sigma \eta, for the eta above this should be Sigma[j,j]
                 # print 'sigma_eta', sigma_eta
@@ -73,8 +74,16 @@ def pval_new(sampler,
                 #keep = np.copy(active)
                 #keep[idx] = False
 
-                # adding |E|\times(p-|E|) matrix of zero on L1 to get L, then conditioning done on L * data
-                L = np.concatenate((L1, np.zeros((size_active, size_data-size_active))), axis=1)
+                # adding |E|\times(p-|E|) matrix of zero on L1 to get L, then conditioning done on L * data fixed
+                # for the selected model
+                L2 = np.concatenate((L1, np.zeros((size_active, size_data-size_active))), axis=1)
+
+                # for the saturated model
+                L3 = np.concatenate((np.zeros((size_data-size_active, size_active )), np.identity(size_data-size_active) ), axis=1)
+                L = np.concatenate((L2, L3), axis=0)
+                # print L
+                # print L.shape
+
                 #print L[:,j]
                 #print L[:, 3]
                 eta1 = np.concatenate((eta, np.zeros(p-size_active)), axis=0)
@@ -83,6 +92,12 @@ def pval_new(sampler,
 
                 loss_args['linear_part'] = L
                 loss_args['value'] = np.dot(L, data)  # conditioning is on L*data=value
+                loss_args['beta'] = true_beta.copy()
+                #print 'true_beta', true_beta
+                loss_args['beta'][j]=0
+
+                #print 'loss_args',loss_args['beta']
+
                 sampler.setup_sampling(data, loss_args=loss_args)
                 samples = sampler.sampling(ndraw=5000, burnin=1000)
                 pop = [np.dot(eta1,z) for z, _, in samples]

selection/sampling/randomized/tests/test_lasso.py
@@ -6,9 +6,10 @@ import selection.sampling.randomized.api as randomized
 from pvalues import pval
 from matplotlib import pyplot as plt
 
-def test_lasso(s=5, n=100, p=30):
+def test_lasso(s=5, n=1000, p=30):
     
     X, y, _, nonzero, sigma = instance(n=n, p=p, random_signs=True, s=s, sigma=1.,rho=0)
+    print 'sigma', sigma
     lam_frac = 1.
 
     randomization = laplace(loc=0, scale=1.)
@@ -29,9 +30,6 @@ def test_lasso(s=5, n=100, p=30):
 
     loss_args = {'mean':np.zeros(n), 
                  'sigma':sigma}
-
-    ##
-
 
     null, alt = pval(sampler1,
                      loss_args,

selection/sampling/randomized/tests/test_lasso_randomX.py
@@ -11,9 +11,10 @@ from pvalues_new import pval_new
 from matplotlib import pyplot as plt
 
 
-def test_lasso_randomX(s=5, n=1000, p=20):
+def test_lasso_randomX(s=5, n=1000, p=30):
 
-    X, y, _, nonzero, sigma = instance(n=n, p=p, random_signs=True, s=s, sigma=1.)
+    X, y, true_beta, nonzero, sigma = instance(n=n, p=p, random_signs=True, s=s, sigma=1., rho=0)
+
     lam_frac = 1.
 
     randomization = laplace(loc=0, scale=1.)
@@ -25,6 +26,7 @@ def test_lasso_randomX(s=5, n=1000, p=20):
 
     # this is the initial randomization based on which the active set is chosen
     # done is sampler.py
+
     random_Z = randomization.rvs(p)
 
     penalty = randomized.selective_l1norm_new(p, lagrange=lam)
@@ -45,11 +47,14 @@ def test_lasso_randomX(s=5, n=1000, p=20):
 
     # the estimate of the covariance of \bar{\beta}_E-\beta_E = (X_E^TX_E)^{-1}X_E^T\epsilon
     # is Sigma=(X_E^TX_E)^{-1}COV(X_E^T\epsilon), where COV(X_E^T\epsilon) is the pairs bootstrap estimate
-    # Sigma = np.dot(np.linalg.inv(sampler1.loss._XETXE), sampler1.loss._cov_XETepsilon)
+    #Sigma = np.dot(np.linalg.inv(sampler1.loss._XETXE), sampler1.loss._cov_XETepsilon)
 
     # bootstrapped covariance of \bar{\beta}_E, (X_E^{*T}X_E^*)^{-1}X_E^{*T}(y^*-X_E^*\bar{\beta}_E)
     Sigma_b = sampler1.loss._cov_beta_bar
     #print Sigma_b.diagonal()
+
+    #Sigma_oracle = np.linalg.inv(sampler1.loss._XETXE)
+
 
     inactive_set = ~active_set
 
@@ -61,7 +66,7 @@ def test_lasso_randomX(s=5, n=1000, p=20):
 
     data = np.concatenate((beta_unpenalized, N))  # (\bar{\beta}_E, X_{-E}^T(y-X_E\bar{\beta}^E))
 
-    loss_args = {'mean':np.zeros(p)}
+    loss_args = {'beta':true_beta.copy()}
 
     # linear_part=np.identity(p)
     # data=np.dot(X.T, y)
@@ -72,14 +77,15 @@ def test_lasso_randomX(s=5, n=1000, p=20):
                          linear_part,
                          data,
                          nonzero,
-                         Sigma_b)
+                         Sigma_b,
+                         true_beta)
 
     return null, alt
 
 if __name__ == "__main__":
 
     P0, PA = [], []
-    for i in range(3):
+    for i in range(5):
         print "iteration", i
         p0, pA = test_lasso_randomX()
         P0.extend(p0); PA.extend(pA)

selection/sampling/randomized/tests/vezba.py
@@ -33,14 +33,23 @@ W = np.array([[1,5, 10],[3, 10, 18], [8, 5, 11]], dtype=float)
 #print np.dot(R, eta.T)
 
 Sigma=np.dot(W,W.T)
-print Sigma
-print np.linalg.inv(Sigma)
+#print Sigma
+#print np.linalg.inv(Sigma)
 mu = np.random.normal(size=Sigma.shape[0])
 j=0
 mu[j]=0
-print np.dot(Sigma[:,j].T, np.linalg.inv(Sigma))
-print np.dot(np.dot(Sigma[:,j].T, np.linalg.inv(Sigma)), mu)
+#rint np.dot(Sigma[:,j].T, np.linalg.inv(Sigma))
+#print np.dot(np.dot(Sigma[:,j].T, np.linalg.inv(Sigma)), mu)
 
 
 a=np.array([1,2,3,4,-3])
-print np.clip(a, 0, np.inf)
+#print np.clip(a, 0, np.inf)
+
+d={'Jelena':1, 'Bojana':2}
+def proba(Jelena=0, Bojana=0):
+    return Jelena+1
+
+print proba(**d)
+#print map(proba,**d)
+
+