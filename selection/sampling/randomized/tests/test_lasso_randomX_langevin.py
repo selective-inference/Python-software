added nonparametric cov es for logistic. fixed scaling.

selection/sampling/randomized/tests/test_lasso_randomX_langevin.py
@@ -9,7 +9,7 @@ import regreg.api as rr
 import selection.sampling.randomized.losses.lasso_randomX as lasso_randomX
 
 
-def test_lasso(s=5, n=100, p=20, covariance_estimate = "nonparametric",
+def test_lasso(s=5, n=200, p=20, covariance_estimate = "parametric",
                Langevin_steps = 7000, burning=0):
 
     step_size = 1./p
@@ -59,7 +59,6 @@ def test_lasso(s=5, n=100, p=20, covariance_estimate = "nonparametric",
     init_vec_state[ndata:(ndata+nactive)] = betaE
     init_vec_state[(ndata+nactive):] = cube
 
-
     def bootstrap_covariance(X=X, y=y, active=active, beta_unpenalized=beta_unpenalized):
         n, p = X.shape
         nsample = 5000
@@ -68,14 +67,13 @@ def test_lasso(s=5, n=100, p=20, covariance_estimate = "nonparametric",
         _mean_cum_data = 0
         _cov_data = np.zeros((p, p))
 
-
         for _ in range(nsample):
             indices = np.random.choice(n, size=(n,), replace=True)
             y_star = y[indices]
             X_star = X[indices]
 
             # Z_star = np.dot(X_star.T, y_star - pi(X_star))  # X^{*T}(y^*-X^{*T}_E\bar{\beta}_E)
-            Z_star = np.dot(X_star.T, y_star - np.dot(X_star[:, active], beta_unpenalized))
+            Z_star = np.dot(X_star.T, y_star - np.dot(X_star[:,active], beta_unpenalized))
 
             mat_XEstar = np.linalg.inv(np.dot(X_star[:, active].T, X_star[:, active]))  # (X^{*T}_E X^*_E)^{-1}
             mat_star = np.dot(np.dot(X_star[:, inactive].T, X_star[:, active]), mat_XEstar)
@@ -86,14 +84,13 @@ def test_lasso(s=5, n=100, p=20, covariance_estimate = "nonparametric",
             _mean_cum_data += data_star
             _cov_data += np.multiply.outer(data_star, data_star)
 
-
         _cov_data /= nsample
         _mean_cum_data = _mean_cum_data / nsample
         _cov_data -= np.multiply.outer(_mean_cum_data, _mean_cum_data)
 
         return _cov_data
 
-    if covariance_estimate=="nonparametrid":
+    if covariance_estimate=="nonparametric":
         Sigma_full = bootstrap_covariance()
     else:
         # parametric coveriance estimate

selection/sampling/randomized/tests/test_logistic_langevin.py
@@ -9,8 +9,11 @@ import regreg.api as rr
 import selection.sampling.randomized.losses.lasso_randomX as lasso_randomX
 
 
-def test_lasso(s=5, n=500, p=20, Langevin_steps=10000, burning=0):
+def test_lasso(s=5, n=200, p=20, Langevin_steps=10000, burning=0,
+               randomization_dist = "laplace", randomization_scale=1,
+               covariance_estimate="nonparametric"):
 
+    "randomization_dist: laplace or logistic"
     step_size = 1./p
     # problem setup
 
@@ -19,21 +22,23 @@ def test_lasso(s=5, n=500, p=20, Langevin_steps=10000, burning=0):
     nonzero = np.where(beta)[0]
     lam_frac = 1.
 
-    randomization = laplace(loc=0, scale=1.)
+    if randomization_dist=="laplace":
+        randomization = laplace(loc=0, scale=1.)
+        random_Z = randomization.rvs(p)
+    if randomization_dist=="logistic":
+        random_Z = np.random.logistic(loc=0, scale=1,size=p)
     loss = randomized.logistic_Xrandom_new(X, y)
     epsilon = 1.
 
-    lam = (n/2)*lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.binomial(1, 1. / 2, (n, 10000)))).max(0))
-    random_Z = randomization.rvs(p)
+    lam = lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.binomial(1, 1. / 2, (n, 10000)))).max(0))
     penalty = randomized.selective_l1norm_lan_logistic(p, lagrange=lam)
 
-    random_Z = randomization.rvs(p)
 
     # initial solution
 
     problem = rr.simple_problem(loss, penalty)
-    scale = n
-    random_term = rr.identity_quadratic(epsilon, 0, scale*random_Z, 0)
+
+    random_term = rr.identity_quadratic(epsilon, 0, randomization_scale*random_Z, 0)
     solve_args = {'tol': 1.e-10, 'min_its': 100, 'max_its': 500}
 
     initial_soln = problem.solve(random_term, **solve_args)
@@ -47,14 +52,14 @@ def test_lasso(s=5, n=500, p=20, Langevin_steps=10000, burning=0):
 
     w_initial = np.exp(np.dot(X, initial_soln))
     pi_initial = w_initial/(1+w_initial)
-    initial_grad = -np.dot(X.T, y-pi_initial)*n/2
-    print 'ini', initial_grad
-    subgradient = -(initial_grad+epsilon*initial_soln+random_Z)
+    initial_grad = -np.dot(X.T, y-pi_initial)
+    print "initial gradient manual", initial_grad
+    subgradient = -(initial_grad+epsilon*initial_soln+randomization_scale*random_Z)
     cube = subgradient[inactive]/lam
     print 'cube', cube
 
     initial_grad = loss.smooth_objective(initial_soln,  mode='grad')
-    print 'ini', initial_grad
+    print 'initial gradient from loss', initial_grad
     betaE, cube = penalty.setup_sampling(initial_grad,
                                          initial_soln,
                                          random_Z,
@@ -78,29 +83,29 @@ def test_lasso(s=5, n=500, p=20, Langevin_steps=10000, burning=0):
     ndata = data.shape[0];  nactive = betaE.shape[0];  ninactive = cube.shape[0]
 
 
-    # parametric coveriance estimate
-    def pi(X):
-        w = np.exp(np.dot(X[:, active], beta_unpenalized))
-        return w / (1 + w)
 
-    pi_E = pi(X)
-    W = np.diag(np.diag(np.outer(pi_E, 1 - pi_E)))
-    Q = np.dot(X[:, active].T, np.dot(W, X[:, active]))
-    Q_inv = np.linalg.inv(Q)
+    if covariance_estimate == "parametric":
+        # parametric coveriance estimate
+        def pi(X):
+            w = np.exp(np.dot(X[:, active], beta_unpenalized))
+            return w / (1 + w)
 
-    mat = np.zeros((nactive+ninactive, n))
-    mat[:nactive,:] = Q_inv.dot(X[:, active].T)
-    mat1 = np.dot(np.dot(W,X[:, active]), np.dot(Q_inv, X[:, active].T))
-    mat[nactive:,:] = X[:, inactive].T.dot(np.identity(n)-mat1)
+        pi_E = pi(X)
+        W = np.diag(np.diag(np.outer(pi_E, 1 - pi_E)))
+        Q = np.dot(X[:, active].T, np.dot(W, X[:, active]))
+        Q_inv = np.linalg.inv(Q)
 
-    Sigma_full = np.dot(mat, np.dot(W, mat.T))
+        mat = np.zeros((nactive+ninactive, n))
+        mat[:nactive,:] = Q_inv.dot(X[:, active].T)
+        mat1 = np.dot(np.dot(W,X[:, active]), np.dot(Q_inv, X[:, active].T))
+        mat[nactive:,:] = X[:, inactive].T.dot(np.identity(n)-mat1)
+
+        Sigma_full = np.dot(mat, np.dot(W, mat.T))
+    else:
+        # non-parametric covariance estimate
+        Sigma_full = loss._cov
+
     Sigma_full_inv = np.linalg.inv(Sigma_full)
-    #Sigma_T = Sigma_full[:nactive,:nactive]
-    #Sigma_T_inv = np.linalg.inv(Sigma_T)
-
-    # non-parametric covariance estimate
-    #Sigma_full = loss._Sigma_full
-    #Sigma_full_inv = np.linalg.inv(Sigma_full)
 
 
     init_vec_state = np.zeros(ndata+nactive+ninactive)
@@ -141,14 +146,21 @@ def test_lasso(s=5, n=500, p=20, Langevin_steps=10000, burning=0):
         opt_vars = [betaE, cube]
         params , _ , opt_vec = penalty.form_optimization_vector(opt_vars) # opt_vec=\epsilon(\beta 0)+u, u=\grad P(\beta), P penalty
 
-        gradient = loss.gradient(data, params)*n/2
-        hessian = loss.hessian * n/2
+        gradient = loss.gradient(data, params)
+        hessian = loss.hessian
 
         ndata = data.shape[0]
         nactive = betaE.shape[0]
         ninactive = cube.shape[0]
 
-        sign_vec = - np.sign(gradient + opt_vec)  # sign(w), w=grad+\epsilon*beta+lambda*u
+        omega = -(gradient+opt_vec)
+        if randomization_dist == "laplace":
+            randomization_derivative = np.sign(omega)/randomization_scale
+
+        if randomization_dist == "logistic":
+            omega_scaled = omega/randomization_scale
+            randomization_derivative = -(np.exp(-omega_scaled)-1)/(np.exp(-omega_scaled)+1)
+            randomization_derivative /=randomization_scale
 
         A = hessian + epsilon * np.identity(nactive + ninactive)
         A_restricted = A[:, active]
@@ -156,33 +168,42 @@ def test_lasso(s=5, n=500, p=20, Langevin_steps=10000, burning=0):
         T = data[:nactive]
         _gradient = np.zeros(ndata + nactive + ninactive)
 
-        # saturated model
         _gradient[:ndata] = - np.dot(Sigma_full_inv, data)
-        _gradient[:nactive] -= hessian[:,active].T.dot(sign_vec)
-        _gradient[nactive:(ndata)] -= sign_vec[inactive]
+        _gradient[:nactive] -= hessian[:,active].T.dot(randomization_derivative)
+        _gradient[nactive:(ndata)] -= randomization_derivative[inactive]
 
-        # selected model
-        #gradient[:nactive] = - (np.dot(Sigma_T_inv, data[:nactive]) + np.dot(hessian[:, active].T, sign_vec))
+        _gradient[ndata:(ndata + nactive)] = np.dot(A_restricted.T, randomization_derivative)
+        _gradient[(ndata + nactive):] = lam * randomization_derivative[inactive]
 
-        _gradient[ndata:(ndata + nactive)] = np.dot(A_restricted.T, sign_vec)
-        _gradient[(ndata + nactive):] = lam * sign_vec[inactive]
-
-        return _gradient/scale
+        return _gradient
 
 
     null, alt = pval(init_vec_state, full_gradient, full_projection,
-                      Sigma_full[:nactive, :nactive], data, nonzero, active)
+                      Sigma_full[:nactive, :nactive], data, nonzero, active,
+                     Langevin_steps, burning, step_size)
 
     return null, alt
 
 if __name__ == "__main__":
 
     P0, PA = [], []
+
+    plt.figure()
+    plt.ion()
+
     for i in range(20):
         print "iteration", i
         p0, pA = test_lasso()
         P0.extend(p0); PA.extend(pA)
+        plt.clf()
+        plt.xlim([0, 1])
+        plt.ylim([0, 1])
+        probplot(P0, dist=uniform, sparams=(0, 1), plot=plt, fit=False)
+        plt.plot([0, 1], color='k', linestyle='-', linewidth=2)
+        plt.pause(0.01)
 
+    while True:
+        plt.pause(0.05)
 
     print "done! mean: ", np.mean(P0), "std: ", np.std(P0)
     plt.figure()