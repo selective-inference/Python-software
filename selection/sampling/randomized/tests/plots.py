1707959f0906dcca12d7596e0db9ebb7472cb027
Jelena Markovic <jelenam@stanford.edu>
2016-08-07 14:31:24 -0700

    plots for the paper prepared for cluster

--- selection/sampling/randomized/tests/plots.py
+++ selection/sampling/randomized/tests/plots.py
@@ -4,6 +4,8 @@ from test_logistic_langevin import test_lasso as test_logistic
 from test_lasso_randomX_langevin import test_lasso as test_randomX
 from matplotlib import pyplot as plt
 from scipy.stats import probplot, uniform
+import statsmodels.api as sm
+
 
 np.random.seed(2)
 
@@ -14,45 +16,66 @@ plot_logistic = fig.add_subplot(133)
 
 P0, PA = [], []
 
-for i in range(100):
+for i in range(3):
     print "iteration", i
-    p0, pA = test_randomX(s=5, n=200, p=20, Langevin_steps=7000)
+    p0, pA = test_randomX(s=5, n=200, p=20, Langevin_steps=5000)
     P0.extend(p0); PA.extend(pA)
 
 print "random X done! mean: ", np.mean(P0), "std: ", np.std(P0)
-probplot(P0, dist=uniform, sparams=(0,1), plot=plot_randomX, fit=False)
-plot_randomX.plot([0, 1], color='k', linestyle='-', linewidth=2)
+#probplot(P0, dist=uniform, sparams=(0,1), plot=plot_randomX, fit=False)
+#plot_randomX.plot([0, 1], color='k', linestyle='-', linewidth=2)
+
+ecdf = sm.distributions.ECDF(P0)
+x = np.linspace(min(P0), max(P0))
+y = ecdf(x)
+plot_randomX.plot(x, y, lw=2)
+plot_randomX.plot([0, 1], [0, 1], 'k-', lw=2)
+
 plot_randomX.set_title("Lasso random X")
 plot_randomX.set_xlim([0,1])
 plot_randomX.set_ylim([0,1])
 
 
 P0, PA = [], []
-for i in range(100):
+for i in range(3):
     print "iteration", i
-    p0, pA = test_fixedX(s=5, n=200, p=20, Langevin_steps=7000)
+    p0, pA = test_fixedX(s=5, n=200, p=20, Langevin_steps=5000)
     P0.extend(p0); PA.extend(pA)
 
 print "fixed X done! mean: ", np.mean(P0), "std: ", np.std(P0)
-probplot(P0, dist=uniform, sparams=(0,1), plot=plot_fixedX, fit=False)
-plot_fixedX.plot([0, 1], color='k', linestyle='-', linewidth=2)
+# probplot(P0, dist=uniform, sparams=(0,1), plot=plot_fixedX, fit=False)
+# plot_fixedX.plot([0, 1], color='k', linestyle='-', linewidth=2)
+
+ecdf = sm.distributions.ECDF(P0)
+x = np.linspace(min(P0), max(P0))
+y = ecdf(x)
+plot_fixedX.plot(x, y, lw=2)
+plot_fixedX.plot([0, 1], [0, 1], 'k-', lw=2)
+
 plot_fixedX.set_title("Lasso fixed X")
 plot_fixedX.set_xlim([0,1])
 plot_fixedX.set_ylim([0,1])
 
 
 P0, PA = [], []
-for i in range(100):
+for i in range(3):
     print "iteration", i
-    p0, pA = test_logistic(s=5, n=200, p=20, Langevin_steps=7000)
+    p0, pA = test_logistic(s=5, n=200, p=20, Langevin_steps=5000)
     P0.extend(p0); PA.extend(pA)
 print "logistic done! mean: ", np.mean(P0), "std: ", np.std(P0)
-probplot(P0, dist=uniform, sparams=(0,1), plot=plot_logistic, fit=False)
-plot_logistic.plot([0, 1], color='k', linestyle='-', linewidth=2)
+# probplot(P0, dist=uniform, sparams=(0,1), plot=plot_logistic, fit=False)
+# plot_logistic.plot([0, 1], color='k', linestyle='-', linewidth=2)
+
+ecdf = sm.distributions.ECDF(P0)
+x = np.linspace(min(P0), max(P0))
+y = ecdf(x)
+plot_logistic.plot(x, y, lw=2)
+plot_logistic.plot([0, 1], [0, 1], 'k-', lw=2)
+
 plot_logistic.set_title("Logistic random X")
 plot_logistic.set_xlim([0,1])
 plot_logistic.set_ylim([0,1])
 
 
 plt.show()
-
+plt.savefig('three_Lassos_Langevin.pdf')
