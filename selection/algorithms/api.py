281774116e2e805363b18e57fb31ecfca558e827
Jonathan Taylor <jonathan.taylor@stanford.edu>
2016-09-09 11:43:33 -0700

    updates to api -- should now go through tests to use api for relevant imports where possible

--- selection/algorithms/api.py
+++ selection/algorithms/api.py
@@ -1,4 +1,7 @@
-from .lasso import lasso, data_carving as data_carving_lasso
-from .sqrt_lasso import (sqrt_lasso, 
-                         choose_lambda as choose_lambda_sqrt_lasso,
-                         data_carving as data_carving_sqrt_lasso)
+from .lasso import lasso, data_carving as data_carving_lasso, additive_noise as additive_noise_lasso
+
+from .sqrt_lasso import choose_lambda as choose_lambda_sqrt_lasso, solve_sqrt_lasso
+
+from .forward_step import forward_step, info_crit_stop
+
+from .covtest import covtest, selected_covtest
--- selection/api.py
+++ selection/api.py
@@ -1,4 +1,5 @@
 from .constraints.api import *
 from .algorithms.api import *
 from .distribution.api import *
+from .randomized.api import *
 from .truncated.api import *
