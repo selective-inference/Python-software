201500082ddfbebe913c6561b3c46a3bb220e5bf
Snigdha Panigrahi <snigdhapanigrahi@DN525j9n.SUNet>
2016-09-22 14:08:45 -0700

    finished a first version of sel_prob

--- selection/bayesian_selective/sel_probability.py
+++ selection/bayesian_selective/sel_probability.py
@@ -18,15 +18,20 @@ lam, epsilon, active, betaE, cube, initial_soln = selection(X,y, random_Z)
 class selection_probability(object):
 
     #defining class variables
-    def __init__(self, V,B_E,gamma_E,sigma,tau):
+    def __init__(self, V,B_E,gamma_E,sigma,tau,lam):
 
-        (self.V, self.B_E,self.gamma_E,self.sigma,self.tau) = (V,B_E,gamma_E,sigma,tau)
+        (self.V, self.B_E,self.gamma_E,self.sigma,self.tau,self.lam) = (V,B_E,gamma_E,sigma,tau,lam)
         self.sigma_sq = self.sigma ** 2
         self.tau_sq = self.tau ** 2
         self.signs = np.sign(self.betaE)
         self.n, self.p = V.shape
         self.nactive = betaE.shape[0]
         self.ninactive=self.p-self.nactive
+        #be careful here to permute the active columns
+        self.V_E = self.V[:,:self.nactive]
+        self.V_E_comp=self.V[:,self.nactive:]
+        self.C_E=self.B_E[:self.nactive,:self.nactive]
+        self.D_E=self.B_E.T[:self.nactive,self.nactive:]
 
     def optimization(self,param):
 
@@ -64,14 +69,63 @@ class selection_probability(object):
                                                                                     self.tau_sq))-vec_inter
         Sigma_tilde=np.dot(np.dot(self.B_E.T,Sigma_inter),self.B_E)
 
-        def objective_1(z):
+        def objective_noise(z):
 
             z_1=z[1:self.nactive]
             z_2=z[self.nactive:]
             return np.true_divide(np.dot(np.dot(z.T,Sigma_tilde),z),2)+ barrier_sel(z_1)\
                    +barrier_subgrad(z_2)-np.dot(z.T,mu_tilde)
 
-        def
+        def objective_subgrad(z_3,z_1,z_2):
+
+            mu_z3=np.true_divide(np.dot(self.V_E_comp.T,z_1)-np.dot(self.D_E.T,z_2),self.tau_sq)
+            return -np.dot(z_3.T,mu_z3)+np.true_divide(np.inner(z_3.T,z_3),self.tau_sq)+barrier_subgrad(z_3)
+
+        def value_subgrad(z_1,z_2):
+            initial_subgrad=np.random.uniform(-1, 1, self.ninactive)
+            res = minimize(objective_subgrad, x0=initial_subgrad,args=(z_1,z_2))
+            return -res.fun
+
+        def objective_data_noise(z):
+            z_1=z[:self.n]
+            z_2=z[self.n:]
+            Sigma_z2=np.true_divide(np.dot(self.C_E,self.C_E)+np.dot(self.D_E,self.D_E.T),2*self.tau_sq)
+            mu_z2=-self.lam*np.dot(self.C_E,self.signs)-np.dot((np.true_divide(np.dot(self.C_E,self.V_E.T),self.tau_sq)\
+                  +np.true_divide(self.D_E,self.V_E.T)),z_1)
+            mu_z1=np.true_divide(mean(param),self.sigma_sq) - np.true_divide(np.dot(self.V, self.gamma_E),self.tau_sq)
+            return -np.dot(z_1.T,mu_z1)+np.true_divide(np.dot(np.dot(z_1.T,Sigma),z_1),2)-value_subgrad(z_1,z_2)-\
+                   np.dot(z_2.T,mu_z2)+np.dot(np.dot(z_2.T,Sigma_z2),z_2)
+
+        if self.p< self.n+self.nactive:
+            initial_noise = np.zeros(self.p)
+            initial_noise[:self.nactive] = self.betaE
+            initial_noise[self.nactive:] = np.random.uniform(-1, 1, self.ninactive)
+            res=minimize(objective_noise,x0=initial_noise)
+            return -res.fun
+        else:
+            initial_data_noise=np.zeros(self.p+self.n)
+            initial_data_noise[self.n:]=self.betaE
+            res=minimize(objective_data_noise,x0=initial_data_noise)
+            return -res.fun
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
