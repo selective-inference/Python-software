801226759bdb99a1dd792aca59362eca3e2be283
Snigdha Panigrahi <snigdhapanigrahi@DN525igc.SUNet>
2016-09-28 14:02:12 -0700

    changed instance import

--- selection/bayesian_selective/test_sel_probability.py
+++ selection/bayesian_selective/test_sel_probability.py
@@ -1,7 +1,7 @@
 import numpy as np
 from initial_soln import selection
 from scipy.optimize import minimize
-from selection.algorithms.lasso import instance
+from selection.tests.instance import gaussian_instance as instance
 #from matplotlib import pyplot as plt
 #####for debugging currently; need to change this part
 
