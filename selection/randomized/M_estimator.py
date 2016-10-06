hessian corrected

selection/randomized/M_estimator.py
@@ -409,13 +409,13 @@ class split_M_estimator(M_estimator):
 
         beta_full1 = np.zeros(overall.shape)
         beta_full1[overall] = _beta_unpenalized1
-        _hessian1 = loss.sub_loss.hessian(beta_full1)
+        _hessian = loss.sub_loss.hessian(beta_full1)
         self._beta_full1 = beta_full1
 
         _beta_unpenalized = restricted_Mest(loss.full_loss, overall, solve_args=solve_args)
         beta_full = np.zeros(overall.shape)
         beta_full[overall] = _beta_unpenalized
-        _hessian = loss.sub_loss.hessian(beta_full)
+        #_hessian = loss.sub_loss.hessian(beta_full)
         self._beta_full = beta_full
 
         # observed state for score
@@ -423,12 +423,13 @@ class split_M_estimator(M_estimator):
         #self.observed_score_state = np.hstack([_beta_unpenalized * _sqrt_scaling,
         #                                       -loss.smooth_objective(beta_full, 'grad')[inactive] / _sqrt_scaling])
 
-        self.observed_score_state = np.dot(loss.X1.T,loss.sub_loss.saturated_loss.smooth_objective(loss.X1.dot(beta_full1), 'grad') + loss.y1)
+        #self.observed_score_state = np.dot(loss.X1.T,loss.sub_loss.saturated_loss.smooth_objective(loss.X1.dot(beta_full1), 'grad') + loss.y1)
 
-        self.observed_score_state += -np.dot(loss.X.T, loss.y)*loss.fraction
+        #self.observed_score_state += -np.dot(loss.X.T, loss.y)*loss.fraction-np.dot(_hessian1, beta_full1)
 
-        self.observed_score_state += -np.dot(_hessian1, beta_full1)
+        self.observed_score_state = loss.smooth_objective(initial_soln, 'grad')
 
+        #print loss.smooth_objective(initial_soln, 'grad')
         # form linear part
 
         self.num_opt_var = p = loss.shape[0]  # shorthand for p
@@ -453,10 +454,10 @@ class split_M_estimator(M_estimator):
 
         # N_{-(E \cup U)} piece -- inactive coordinates of score of M estimator at unpenalized solution
 
-        null_idx = range(overall.sum(), p)
+        #null_idx = range(overall.sum(), p)
         inactive_idx = np.nonzero(inactive)[0]
-        for _i, _n in zip(inactive_idx, null_idx):
-            _score_linear_term[_i, _n] = -_sqrt_scaling
+        #for _i, _n in zip(inactive_idx, null_idx):
+        #    _score_linear_term[_i, _n] = -_sqrt_scaling
 
         # c_E piece
 

selection/randomized/randomization.py
@@ -170,7 +170,7 @@ class split(randomization):
             return randomization.__init__(self,(p,), density, grad_negative_log_density, sampler,
                                  lipschitz=np.linalg.svd(precision)[1].max())
 
-        gaussian(self.covariance*2)
+        gaussian(self.covariance)
         #randomization.__init__(self, 1, density, grad_negative_log_density, sampler)
 
 

selection/randomized/tests/test_split.py
@@ -36,7 +36,7 @@ class randomized_loss(rr.smooth_atom):
             self.fraction = self.m/float(self.n)
 
         def smooth_objective(self, beta, mode='both', check_feasibility=False):
-            linear = -np.dot(self.X.T, self.y)*self.fraction#+np.dot(self.X1.T,self.y1)
+            linear = -np.dot(self.X.T, self.y)*self.fraction #+np.dot(self.X1.T,self.y1)
             if mode=='grad':
                 return self.sub_loss.smooth_objective(beta, 'grad') + linear
             if mode=='func':
@@ -47,12 +47,12 @@ class randomized_loss(rr.smooth_atom):
 
 
 def test_splits(ndraw=10000, burnin=2000, nsim=None, solve_args={'min_its':50, 'tol':1.e-10}): # nsim needed for decorator
-    s, n, p = 3, 200, 10
+    s, n, p = 3, 400, 10
 
     #randomizer = randomization.laplace((p,), scale=1.)
-    X, y, beta, _ = logistic_instance(n=n, p=p, s=s, rho=0.1, snr=5)
+    X, y, beta, _ = logistic_instance(n=n, p=p, s=s, rho=0, snr=5)
 
-    m = int(n/2)
+    m = int(n/1.5)
 
     nonzero = np.where(beta)[0]
     lam_frac = 1.