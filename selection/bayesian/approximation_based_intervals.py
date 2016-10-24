cdc83cadb2d4d106ec6ed12a5cee92907bd9d97a
Snigdha Panigrahi <snigdhapanigrahi@DN0a22c918.SUNet>
2016-10-24 14:22:28 -0700

    added class for approximate conditional selection probability

--- selection/bayesian/approximation_based_intervals.py
+++ selection/bayesian/approximation_based_intervals.py
@@ -1,11 +1,13 @@
 import numpy as np
 import regreg.api as rr
-from selection.algorithms.softmax import nonnegative_softmax
+from selection.bayesian.selection_probability_rr import nonnegative_softmax_scaled, cube_subproblem_scaled, \
+    cube_barrier_scaled, cube_gradient_scaled, cube_hessian_scaled, cube_objective
 
 #class should return approximate probability of (\beta_E,u_{-E}) in K conditional on s:
 class approximate_density(rr.smooth_atom):
 
     def __init__(self,
+                 y,
                  X,
                  feasible_point,
                  active,
@@ -15,19 +17,27 @@ class approximate_density(rr.smooth_atom):
                  noise_variance,
                  randomizer,
                  epsilon,
-                 coef=1.,
-                 offset=None,
-                 quadratic=None,
-                 nstep=10):
+                 j, #index of interest amongst active variables
+                 s, #point at which density is to computed
+                 coef = 1.,
+                 offset= None,
+                 quadratic= None,
+                 nstep = 10):
 
         n, p = X.shape
         E = active.sum()
         self._X = X
+
+        self.y = y
+
         self.active = active
+
         self.noise_variance = noise_variance
+
         self.randomization = randomizer
 
         self.inactive_conjugate = self.active_conjugate = randomizer.CGF_conjugate
+
         if self.active_conjugate is None:
             raise ValueError(
                 'randomization must know its CGF_conjugate -- currently only isotropic_gaussian and laplace are implemented and are assumed to be randomization with IID coordinates')
@@ -36,36 +46,84 @@ class approximate_density(rr.smooth_atom):
 
         initial = feasible_point
 
-        self.feasible_point = feasible_point
+        #self.feasible_point = feasible_point
 
         rr.smooth_atom.__init__(self,
-                                (p,),
+                                (E,),
                                 offset=offset,
                                 quadratic=quadratic,
                                 initial=initial,
                                 coef=coef)
 
-        #self.coefs[:] = initial
+        self.coefs[:] = initial
 
-        nonnegative = nonnegative_softmax(E)
-        self.cube_bool = np.zeros(p, bool)
-        self.cube_bool[E:] = 1
-
-        self._opt_selector = rr.selector(~self.cube_bool, (p,))
-        self.nonnegative_barrier = nonnegative.linear(self._opt_selector)
+        self.nonnegative_barrier = nonnegative_softmax_scaled(E)
 
         X_E = self.X_E = X[:, active]
+        self.X_inactive = X[:, ~active]
+
         B = X.T.dot(X_E)
 
         B_E = B[active]
         B_mE = B[~active]
 
-        self.B_active = np.hstack([(B_E + epsilon * np.identity(E)) * active_signs[None, :], np.zeros((E, p - E))])
-        self.B_inactive = np.hstack([B_mE * active_signs[None, :], np.identity((p - E))])
-        self.B_p = np.vstack((self.B_active, self.B_inactive))
+        self.B_active = (B_E + epsilon * np.identity(E)) * active_signs[None, :]
+        self.B_inactive = B_mE * active_signs[None, :]\
 
-        self.offset_active = (active_signs * lagrange[active])-(s*(self.X_E.T.dot(self.c)))- (self.X_E.T.dot(self.Z))
-        self.offset_inactive = -()
+        subgrad_offset = active_signs * lagrange[active]
+
+        self.offset_active = self.offset(j, s, subgrad_offset)[:E,]
+
+        self.offset_inactive = self.offset(j, s, subgrad_offset)[E:, ]
+
+        self.active_conj_loss = rr.affine_smooth(self.active_conjugate,
+                                                 rr.affine_transform(self.B_active, self.offset_active))
+
+        cube_obj = cube_objective(self.inactive_conjugate,
+                                  lagrange[~active],
+                                  nstep=nstep)
+
+        self.cube_loss = rr.affine_smooth(cube_obj, rr.affine_transform(self.B_inactive, self.offset_inactive))
+
+        self.total_loss = rr.smooth_sum([self.active_conj_loss,
+                                         self.cube_loss,
+                                         self.nonnegative_barrier])
+
+    def offset(self,j, s, subgrad_offset):
+
+        eta = self.XE_pinv[j, :]
+        c = np.true_divide(eta, self.eta_norm_sq[j])
+        fixed_part = np.dot(np.identity(self.y.shape) - np.outer(c, eta), self.y)
+        _offset_active = subgrad_offset - self.X_E.T.dot(fixed_part) - s*(self.X_E.T.dot(c))
+        _offset_inactive = - self.X_inactive.T.dot(fixed_part) - s*(self.X_inactive.T.dot(c))
+        return np.vstack[_offset_active,_offset_inactive]
+
+    def smooth_objective(self, param, mode='both', check_feasibility=False):
+
+        param = self.apply_offset(param)
+
+        if mode == 'func':
+            f = self.total_loss.smooth_objective(param, 'func')
+            return self.scale(f)
+        elif mode == 'grad':
+            g = self.total_loss.smooth_objective(param, 'grad')
+            return self.scale(g)
+        elif mode == 'both':
+            f, g = self.total_loss.smooth_objective(param, 'both')
+            return self.scale(f), self.scale(g)
+        else:
+            raise ValueError("mode incorrectly specified")
+
+
+class approximate_CI():
+    
+
+
+
+
+
+
+
 
 
 
