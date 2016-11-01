44b0f89c6334506e75457b84bf8759d21f70cbb0
Snigdha Panigrahi <snigdhapanigrahi@DN51ski4.SUNet>
2016-11-01 02:22:39 -0700

    finished writing dual for marginal screening

--- selection/bayesian/marginal_screening.py
+++ selection/bayesian/marginal_screening.py
@@ -2,6 +2,7 @@ import numpy as np
 from selection.algorithms.softmax import nonnegative_softmax
 from selection.bayesian.selection_probability_rr import cube_subproblem_scaled, cube_barrier_scaled,\
     cube_gradient_scaled, cube_hessian_scaled, cube_objective
+from selection.bayesian.barrier import barrier_conjugate_softmax_scaled_rr
 import regreg.api as rr
 
 class selection_probability_objective_ms(rr.smooth_atom):
@@ -266,7 +267,7 @@ class dual_selection_probability_ms(rr.smooth_atom):
 
         self.B_p_inv = np.linalg.inv(self.B_p.T)
 
-        self.offset_active = threshold[active]
+        self.offset_active = active_signs * threshold[active]
         self.inactive_subgrad = np.zeros(p - E)
 
         self.cube_bool = np.zeros(p, np.bool)
@@ -276,5 +277,101 @@ class dual_selection_probability_ms(rr.smooth_atom):
         self.dual_arg = self.B_p_inv.dot(np.append(self.offset_active, self.inactive_subgrad))
 
         self._opt_selector = rr.selector(~self.cube_bool, (p,))
+
+        self.set_parameter(mean_parameter, noise_variance)
+
+        _barrier_star = barrier_conjugate_softmax_scaled_rr(self.cube_bool, threshold[~active])
+
+        self.conjugate_barrier = rr.affine_smooth(_barrier_star, np.identity(p))
+
+        self.CGF_randomizer = rr.affine_smooth(self.CGF_randomization, -self.B_p_inv)
+
+        self.constant = np.true_divide(mean_parameter.dot(mean_parameter), 2 * noise_variance)
+
+        self.linear_term = rr.identity_quadratic(0, 0, self.dual_arg, -self.constant)
+
+        self.total_loss = rr.smooth_sum([self.conjugate_barrier,
+                                         self.CGF_randomizer,
+                                         self.likelihood_loss])
+
+        self.total_loss.quadratic = self.linear_term
+
+    def set_parameter(self, mean_parameter, noise_variance):
+
+        mean_parameter = np.squeeze(mean_parameter)
+
+        self.likelihood_loss = rr.signal_approximator(mean_parameter, coef=1. / noise_variance)
+
+        self.likelihood_loss = rr.affine_smooth(self.likelihood_loss, self.B_p_inv)
+
+    def _smooth_objective(self, param, mode='both', check_feasibility=False):
+
+        param = self.apply_offset(param)
+
+        if mode == 'func':
+            f = self.total_loss.smooth_objective(param, 'func')
+            return self.scale(f)
+        elif mode == 'grad':
+            g = self.total_loss.smooth_objective(param, 'grad')
+            return self.scale(g)
+        elif mode == 'both':
+            f, g = self.total_loss.smooth_objective(param, 'both')
+            return self.scale(f), self.scale(g)
+        else:
+            raise ValueError("mode incorrectly specified")
+
+    def minimize2(self, step=1, nstep=30, tol=1.e-8):
+
+        n, p = self._X.shape
+
+        current = self.feasible_point
+        current_value = np.inf
+
+        objective = lambda u: self.total_loss.objective(u)
+        grad = lambda u: self.total_loss.smooth_objective(u, 'grad') + self.dual_arg
+
+        for itercount in range(nstep):
+            newton_step = grad(current) * self.noise_variance
+
+            # make sure proposal is feasible
+
+            count = 0
+            while True:
+                count += 1
+                proposal = current - step * newton_step
+                if np.all(proposal[self.active_slice] < 0):
+                    break
+                step *= 0.5
+                if count >= 40:
+                    raise ValueError('not finding a feasible point')
+
+            # make sure proposal is a descent
+
+            count = 0
+            while True:
+                proposal = current - step * newton_step
+                proposed_value = objective(proposal)
+                # print(current_value, proposed_value, 'minimize')
+                if proposed_value <= current_value:
+                    break
+                step *= 0.5
+
+            # stop if relative decrease is small
+
+            if np.fabs(current_value - proposed_value) < tol * np.fabs(current_value):
+                current = proposal
+                current_value = proposed_value
+                break
+
+            current = proposal
+            current_value = proposed_value
+
+            if itercount % 4 == 0:
+                step *= 2
+
+        # print('iter', itercount)
+        value = objective(current)
+        return current, value
+
 
 
