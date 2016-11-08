0682a4c40b86fa8193541e83a998c0ab648e2ad7
Jelena Markovic <jelenam@stanford.edu>
2016-11-08 11:03:02 -0800

    new intervals branch

--- selection/randomized/query.py
+++ selection/randomized/query.py
@@ -1242,11 +1242,13 @@ class translate_intervals(object): # intervals_from_sample):
         if alternative not in ['greater', 'less', 'twosided']:
             raise ValueError("alternative should be one of ['greater', 'less', 'twosided']")
 
-        observed_delta = self.observed #- candidate
+        observed_delta = self.observed - candidate
         observed_stat = test_statistic(observed_delta)
         
         candidate_sample, weights = self._weights(candidate)
-        sample_stat = np.array([test_statistic(s) for s in candidate_sample[:, self.targeted_sampler.target_slice]])
+        #sample_stat = np.array([test_statistic(s) for s in candidate_sample[:, self.targeted_sampler.target_slice]])
+        sample_stat = np.array([test_statistic(s) for s in self._delta[:, self.targeted_sampler.target_slice]])
+
         pivot = np.mean((sample_stat <= observed_stat) * weights) / np.mean(weights)
 
         if alternative == 'twosided':
