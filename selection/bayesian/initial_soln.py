909323e9275c3eaa5bd964d756f32455db4c36f4
Snigdha Panigrahi <snigdhapanigrahi@DN0a22c537.SUNet>
2016-11-15 15:50:45 -0800

    trying Hiv data set

--- selection/bayesian/initial_soln.py
+++ selection/bayesian/initial_soln.py
@@ -1,12 +1,16 @@
 import numpy as np
 import regreg.api as rr
 
-def selection(X, y, random_Z, randomization_scale=1, sigma=1):
+def selection(X, y, random_Z, randomization_scale=1, sigma=None, lam=None):
     n, p = X.shape
     loss = rr.glm.gaussian(X,y)
     epsilon = 1. / np.sqrt(n)
     lam_frac = 1.
-    lam = sigma * lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 10000)))).max(0))
+    if sigma is None:
+        sigma = 1.
+    if lam is None:
+        lam = sigma * lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 10000)))).max(0))
+
     W = np.ones(p)*lam
     penalty = rr.group_lasso(np.arange(p), weights = dict(zip(np.arange(p), W)), lagrange=1.)
 
