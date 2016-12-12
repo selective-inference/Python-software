cddaf10c671198f23cf20888eab33c9c1b3aa20f
Snigdha Panigrahi <snigdhapanigrahi@DN525jor.SUNet>
2016-12-12 13:56:12 -0800

    wrote test for inference with random X

--- selection/bayesian/tests/random_lasso_test.py
+++ selection/bayesian/tests/random_lasso_test.py
@@ -8,7 +8,9 @@ from selection.bayesian.initial_soln import selection, instance
 from selection.randomized.api import randomization
 from selection.bayesian.paired_bootstrap import pairs_bootstrap_glm, bootstrap_cov
 from selection.bayesian.randomX_lasso_primal import selection_probability_objective_randomX
+from selection.bayesian.inference_randomX_lasso import sel_prob_gradient_map_randomX, selective_map_credible_randomX
 
+#test for selection probability for random X
 def test_approximation_randomlasso():
     n = 200
     p = 20
@@ -63,7 +65,63 @@ def test_approximation_randomlasso():
     print('selection prob', regreg[0])
     print('minimizer', regreg[1])
 
-test_approximation_randomlasso()
+#test_approximation_randomlasso()
+
+#test for inference in random_X
+def test_inference_randomlasso():
+    n = 200
+    p = 20
+    s = 5
+    snr = 3
+
+    sample = instance(n=n, p=p, s=s, sigma=1, rho=0, snr=snr)
+    X_1, y, true_beta, nonzero, noise_variance = sample.generate_response()
+    random_Z = np.random.standard_normal(p)
+    sel = selection(X_1, y, random_Z, randomization_scale=1, sigma=None, lam=None)
+    lam, epsilon, active, betaE, cube, initial_soln = sel
+    print(true_beta, active)
+
+    noise_variance = 1
+    nactive = betaE.shape[0]
+    active_signs = np.sign(betaE)
+    tau = 1  # randomization_variance
+
+    bootstrap_score = pairs_bootstrap_glm(rr.glm.gaussian(X_1,y), active, beta_full=None, inactive = ~active)[0]
+    sampler = lambda: np.random.choice(n, size=(n,),replace = True)
+    cov = bootstrap_cov(sampler, bootstrap_score)
+
+    primal_feasible = np.fabs(betaE)
+    lagrange = lam * np.ones(p)
+    generative_X = X_1[:, active]
+    prior_variance = 1000.
+
+    inf_rr = selective_map_credible_randomX(y,
+                                            X_1,
+                                            primal_feasible,
+                                            active,
+                                            active_signs,
+                                            lagrange,
+                                            cov,
+                                            generative_X,
+                                            noise_variance,
+                                            prior_variance,
+                                            randomization.isotropic_gaussian((p,), tau),
+                                            epsilon)
+
+    map = inf_rr.map_solve_2(nstep=100)[::-1]
+    print("selective map", map[1])
+
+    toc = time.time()
+    samples = inf_rr.posterior_samples()
+    tic = time.time()
+    print('sampling time', tic - toc)
+    adjusted_intervals = np.vstack([np.percentile(samples, 5, axis=0), np.percentile(samples, 95, axis=0)])
+    adjusted_intervals = np.vstack([map[1], adjusted_intervals])
+    return(adjusted_intervals)
+
+
+test_inference_randomlasso()
+
 
 
 
