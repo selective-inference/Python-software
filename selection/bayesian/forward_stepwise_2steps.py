b8a2eb2296c2205629722d064081644ac815cdd5
Snigdha Panigrahi <snigdhapanigrahi@DN51sk0f.SUNet>
2016-12-27 15:52:20 -0800

    added losses for 2 step fs

--- selection/bayesian/forward_stepwise_2steps.py
+++ selection/bayesian/forward_stepwise_2steps.py
@@ -3,6 +3,7 @@ import regreg.api as rr
 from selection.bayesian.selection_probability_rr import cube_barrier_scaled, cube_gradient_scaled, cube_hessian_scaled
 from selection.algorithms.softmax import nonnegative_softmax
 from selection.bayesian.barrier_fs import linear_map, fs_conjugate, barrier_conjugate_fs_rr
+from selection.bayesian.forward_step_reparametrized import cube_subproblem_fs_linear, cube_objective_fs_linear
 
 
 class selection_probability_objective_fs_2steps(rr.smooth_atom):
@@ -84,6 +85,15 @@ class selection_probability_objective_fs_2steps(rr.smooth_atom):
         opt_vars[n:] = 1
 
         self._opt_selector = rr.selector(opt_vars, (n + E,))
+
+        arg_inactive_loss = np.zeros(n + E, bool)
+        arg_inactive_loss_1 = arg_inactive_loss
+        arg_inactive_loss_1[:n+1] = 1
+        arg_inactive_loss_2 = arg_inactive_loss
+        arg_inactive_loss_2[:n] = 1
+        arg_inactive_loss_2[n+1] = 1
+
+
         self.nonnegative_barrier = nonnegative.linear(self._opt_selector)
         self._response_selector = rr.selector(~opt_vars, (n + E,))
 
@@ -98,13 +108,19 @@ class selection_probability_objective_fs_2steps(rr.smooth_atom):
         self.A_active_1 = np.hstack([-X[:, active_1].T, sign_1, np.zeros((1,1))])
         self.A_active_2 = np.hstack([-X[:, active_2].T.dot(P_1), np.zeros((1, 1)), sign_2])
 
-        self.A_in_1 = np.hstack([-X[:, ~active_1].T, np.zeros((p-1,1)), np.zeros((p-1,1))])
-        self.A_in_2 = np.hstack([np.zeros((n,1)).T, np.ones((1,1)), np.zeros((1,1))])
+        self.A_in_1 = np.hstack([-X[:, ~active_1].T, np.zeros((p-1,1))])
+        self.A_in_2 = np.hstack([np.zeros((n,1)).T, np.ones((1,1))])
         self.A_inactive_1 = np.vstack([self.A_in_1, self.A_in_2])
 
-        self.A_in2_1 = np.hstack([-X[:, ~active_2].T.dot(P_1), np.zeros((p - 2, 1)), np.zeros((p - 2, 1))])
-        self.A_in2_2 = np.hstack([np.zeros((n, 1)).T, np.zeros((1, 1)), np.ones((1, 1))])
+        self.A_in2_1 = np.hstack([-X[:, ~active_2].T.dot(P_1), np.zeros((p - 2, 1))])
+        self.A_in2_2 = np.hstack([np.zeros((n, 1)).T, np.ones((1, 1))])
         self.A_inactive_2 = np.vstack([self.A_in2_1, self.A_in2_2])
+
+        self.A_inactive_sel_1 = rr.selector(arg_inactive_loss_1, (n + E,),
+                                            rr.affine_transform(self.A_inactive_1, np.zeros(n + 1)))
+
+        self.A_inactive_sel_2 = rr.selector(arg_inactive_loss_2, (n + E,),
+                                            rr.affine_transform(self.A_inactive_2, np.zeros(n + 1)))
 
         self.set_parameter(mean_parameter, noise_variance)
 
@@ -113,9 +129,56 @@ class selection_probability_objective_fs_2steps(rr.smooth_atom):
 
         cube_obj = cube_objective_fs_linear(self.inactive_conjugate)
 
-        self.cube_loss = rr.affine_smooth(cube_obj, self.A_inactive)
+        self.cube_loss_1 = rr.affine_smooth(cube_obj, self.A_inactive_sel_1)
+        self.cube_loss_2 = rr.affine_smooth(cube_obj, self.A_inactive_sel_2)
 
-        self.total_loss = rr.smooth_sum([self.active_conj_loss,
-                                         self.cube_loss,
+        self.total_loss = rr.smooth_sum([self.active_conj_loss_1,
+                                         self.active_conj_loss_2,
+                                         self.cube_loss_1,
+                                         self.cube_loss_2,
                                          self.likelihood_loss,
                                          self.nonnegative_barrier])
+
+    def set_parameter(self, mean_parameter, noise_variance):
+        """
+        Set $\beta_E^*$.
+        """
+        mean_parameter = np.squeeze(mean_parameter)
+        likelihood_loss = rr.signal_approximator(mean_parameter, coef=1. / noise_variance)
+        self.likelihood_loss = rr.affine_smooth(likelihood_loss, self._response_selector)
+
+    def smooth_objective(self, param, mode='both', check_feasibility=False):
+        """
+        Evaluate the smooth objective, computing its value, gradient or both.
+        Parameters
+        ----------
+        mean_param : ndarray
+            The current parameter values.
+        mode : str
+            One of ['func', 'grad', 'both'].
+        check_feasibility : bool
+            If True, return `np.inf` when
+            point is not feasible, i.e. when `mean_param` is not
+            in the domain.
+        Returns
+        -------
+        If `mode` is 'func' returns just the objective value
+        at `mean_param`, else if `mode` is 'grad' returns the gradient
+        else returns both.
+        """
+
+        param = self.apply_offset(param)
+
+        if mode == 'func':
+            f = self.total_loss.smooth_objective(param, 'func')
+            return self.scale(f)
+        elif mode == 'grad':
+            g = self.total_loss.smooth_objective(param, 'grad')
+            return self.scale(g)
+        elif mode == 'both':
+            f, g = self.total_loss.smooth_objective(param, 'both')
+            return self.scale(f), self.scale(g)
+        else:
+            raise ValueError("mode incorrectly specified")
+
+
