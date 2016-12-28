4a2a99b141d92e90e822fe5f342e8c6dfb49d713
Snigdha Panigrahi <snigdhapanigrahi@DN51sk0f.SUNet>
2016-12-27 21:44:57 -0800

    added gradient function

--- selection/bayesian/forward_stepwise_2steps.py
+++ selection/bayesian/forward_stepwise_2steps.py
@@ -232,4 +232,67 @@ class selection_probability_objective_fs_2steps(rr.smooth_atom):
         return current, value
 
 
+class sel_prob_gradient_map_fs_2steps(rr.smooth_atom):
+    def __init__(self,
+                 X,
+                 primal_feasible,
+                 active_1,
+                 active_2,
+                 active_sign_1,
+                 active_sign_2,
+                 generative_X,
+                 noise_variance,
+                 randomizer,
+                 coef=1.,
+                 offset=None,
+                 quadratic=None):
+
+        self.E = 2
+        self.n, self.p = X.shape
+        self.dim = generative_X.shape[1]
+
+        self.noise_variance = noise_variance
+
+        (self.X, self.primal_feasible, self.active_1, self.active_2, self.active_sign_1, self.active_sign_2,
+         self.generative_X, self.noise_variance, self.randomizer) = (X, primal_feasible,
+                                                                     active_1, active_2, active_sign_1, active_sign_2,
+                                                                     generative_X, noise_variance, randomizer)
+
+        rr.smooth_atom.__init__(self,
+                                (self.dim,),
+                                offset=offset,
+                                quadratic=quadratic,
+                                coef=coef)
+
+    def smooth_objective(self, true_param, mode='both', check_feasibility=False, tol=1.e-6):
+        true_param = self.apply_offset(true_param)
+
+        mean_parameter = np.squeeze(self.generative_X.dot(true_param))
+
+        primal_sol = selection_probability_objective_fs_2steps(self.X,
+                                                               self.primal_feasible,
+                                                               self.active_1,
+                                                               self.active_2,
+                                                               self.active_sign_1,
+                                                               self.active_sign_2,
+                                                               mean_parameter,
+                                                               self.noise_variance,
+                                                               self.randomizer)
+
+        sel_prob_primal = primal_sol.minimize2(nstep=60)[::-1]
+        optimal_primal = (sel_prob_primal[1])[:self.n]
+        sel_prob_val = -sel_prob_primal[0]
+        optimizer = self.generative_X.T.dot(np.true_divide(optimal_primal - mean_parameter, self.noise_variance))
+
+        if mode == 'func':
+            return sel_prob_val
+        elif mode == 'grad':
+            return optimizer
+        elif mode == 'both':
+            return sel_prob_val, optimizer
+        else:
+            raise ValueError('mode incorrectly specified')
+
+
+
 
