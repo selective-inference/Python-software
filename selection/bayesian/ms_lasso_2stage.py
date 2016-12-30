8d2258a7f662012d09cc71a447d59b270c3d81a4
Snigdha Panigrahi <snigdhapanigrahi@DN0a22c765.SUNet>
2016-12-29 16:49:03 -0800

    added sel prob for ms followed by Lasso

--- selection/bayesian/ms_lasso_2stage.py
+++ selection/bayesian/ms_lasso_2stage.py
@@ -26,6 +26,8 @@ class selection_probability_objective_fs_2steps(rr.smooth_atom):
                  nstep=10):
 
         n, p = X.shape
+        self._X = X
+        
         E_1 = active_1.sum()
         E_2 = active_2.sum()
 
@@ -63,8 +65,24 @@ class selection_probability_objective_fs_2steps(rr.smooth_atom):
         arg_lasso[:self.n] = 1
         arg_lasso = np.append(arg_lasso, np.ones(E_2, bool))
 
-        X_E_1 = X[:, active_1]
-        X_mE_1 = X[:, ~active_1]
+        self.A_active_1 = np.hstack([np.true_divide(-X[:, active_1].T,noise_variance),np.identity(E_1)
+                                   *active_signs_1[None, :] ])
+        self.A_inactive_1 = np.hstack([np.true_divide(-X[:, ~active_1].T, noise_variance), np.zeros((p - E_1, E_1))])
+
+        self.offset_active_1 = active_signs_1 * threshold[active_1]
+        self.offset_inactive_1 = np.zeros(p - E_1)
+
+        self._active_ms = rr.selector(arg_ms, (self.n + E_1 + E_2,),
+                                         rr.affine_transform(self.A_active_1, self.offset_active_1))
+
+        self._inactive_ms = rr.selector(arg_ms, (self.n + E_1 + E_2,),
+                                           rr.affine_transform(self.A_inactive_1, self.offset_inactive_1))
+
+        self.active_conj_loss_1 = rr.affine_smooth(self.active_conjugate, self._active_ms)
+
+        cube_obj_1 = cube_objective(self.inactive_conjugate, lagrange[~active_2], nstep=nstep)
+
+        self.cube_loss_1 = rr.affine_smooth(cube_obj_1, self._inactive_ms)
 
         X_E_2 = X[:, active_2]
         B = X.T.dot(X_E_2)
@@ -79,7 +97,6 @@ class selection_probability_objective_fs_2steps(rr.smooth_atom):
 
         self.offset_inactive_2 = np.zeros(p-E_2)
 
-        self._ms_selector = rr.selector(arg_ms, (self.n + E_1 + E_2,))
 
         self._active_lasso = rr.selector(arg_lasso, (self.n + E_1 + E_2,),
                                            rr.affine_transform(self.A_active_2, self.offset_active_2))
@@ -92,6 +109,110 @@ class selection_probability_objective_fs_2steps(rr.smooth_atom):
         cube_obj_2 = cube_objective(self.inactive_conjugate, lagrange[~active_2], nstep=nstep)
 
         self.cube_loss_2 = rr.affine_smooth(cube_obj_2, self._inactive_lasso)
+
+        self.total_loss = rr.smooth_sum([self.active_conj_loss_1,
+                                         self.active_conj_loss_2,
+                                         self.cube_loss_1,
+                                         self.cube_loss_2,
+                                         self.likelihood_loss,
+                                         self.nonnegative_barrier])
+
+    def set_parameter(self, mean_parameter, noise_variance):
+        """
+        Set $\beta_E^*$.
+        """
+        mean_parameter = np.squeeze(mean_parameter)
+        likelihood_loss = rr.signal_approximator(mean_parameter, coef=1. / noise_variance)
+        self.likelihood_loss = rr.affine_smooth(likelihood_loss, self._response_selector)
+
+    def smooth_objective(self, param, mode='both', check_feasibility=False):
+        """
+        Evaluate the smooth objective, computing its value, gradient or both.
+        Parameters
+        ----------
+        mean_param : ndarray
+            The current parameter values.
+        mode : str
+            One of ['func', 'grad', 'both'].
+        check_feasibility : bool
+            If True, return `np.inf` when
+            point is not feasible, i.e. when `mean_param` is not
+            in the domain.
+        Returns
+        -------
+        If `mode` is 'func' returns just the objective value
+        at `mean_param`, else if `mode` is 'grad' returns the gradient
+        else returns both.
+        """
+
+        param = self.apply_offset(param)
+
+        if mode == 'func':
+            f = self.total_loss.smooth_objective(param, 'func')
+            return self.scale(f)
+        elif mode == 'grad':
+            g = self.total_loss.smooth_objective(param, 'grad')
+            return self.scale(g)
+        elif mode == 'both':
+            f = self.total_loss.smooth_objective(param, 'func')
+            g = self.total_loss.smooth_objective(param, 'grad')
+            return self.scale(f), self.scale(g)
+        else:
+            raise ValueError("mode incorrectly specified")
+
+    def minimize2(self, step=1, nstep=30, tol=1.e-8):
+
+        n, p = self._X.shape
+
+        current = self.coefs
+        current_value = np.inf
+
+        objective = lambda u: self.smooth_objective(u, 'func')
+        grad = lambda u: self.smooth_objective(u, 'grad')
+
+        for itercount in range(nstep):
+            newton_step = grad(current) * self.noise_variance
+
+            # make sure proposal is feasible
+
+            count = 0
+            while True:
+                count += 1
+                proposal = current - step * newton_step
+                if np.all(proposal[n:] > 0):
+                    break
+                step *= 0.5
+                if count >= 40:
+                    raise ValueError('not finding a feasible point')
+
+            # make sure proposal is a descent
+
+            count = 0
+            while True:
+                proposal = current - step * newton_step
+                proposed_value = objective(proposal)
+                # print(current_value, proposed_value, 'minimize')
+                if proposed_value <= current_value:
+                    break
+                step *= 0.5
+
+            # stop if relative decrease is small
+
+            if np.fabs(current_value - proposed_value) < tol * np.fabs(current_value):
+                current = proposal
+                current_value = proposed_value
+                break
+
+            current = proposal
+            current_value = proposed_value
+
+            if itercount % 4 == 0:
+                step *= 2
+
+        # print('iter', itercount)
+        value = objective(current)
+        return current, value
+
 
 
 
