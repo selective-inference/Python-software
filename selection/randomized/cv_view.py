fixed a bug in scaling randomization1 and 2

selection/randomized/cv_view.py
@@ -12,7 +12,7 @@ from selection.api import randomization
 
 class CV_view(query):
 
-    def __init__(self, loss, scale1=0.5, scale2=0.5, K=5):
+    def __init__(self, loss, scale1=1, scale2=1, K=5):
 
         self.loss = loss
         X, y = loss.data
@@ -34,9 +34,10 @@ class CV_view(query):
         self.randomization1 = randomization.isotropic_gaussian((self.num_opt_var,), scale=scale1)
         self.randomization2 = randomization.isotropic_gaussian((self.num_opt_var,), scale=scale2)
         query.__init__(self, self.randomization2)
+
         self.nboot = 1
 
-    def solve(self, scale1=0.5, scale2=0.5):
+    def solve(self):
 
 
         lam_CVR, CVR_val, CV1_val = choose_lambda_CV(self.loss, self.lam_seq, self.folds, self.randomization1, self.randomization2)
@@ -46,9 +47,9 @@ class CV_view(query):
          self.observed_score_state) = (lam_CVR,
                                        CVR_val,
                                        CV1_val)
-        self.lam_idx = list(self.lam_seq).index(self.lam_CVR) # index of the minimizer
+        lam_idx = list(self.lam_seq).index(self.lam_CVR) # index of the minimizer
         self._solved = True
-        self.projection_map = projection_cone(self.num_opt_var, self.lam_idx,1)
+        #self.projection_map = projection_cone(self.num_opt_var, lam_idx, -np.sign(CVR_val[lam_idx]))
         self.opt_transform = (np.identity(self.num_opt_var), np.zeros(self.num_opt_var))
         self.score_transform = (-np.identity(self.num_opt_var), np.zeros(self.num_opt_var))
 
@@ -59,12 +60,12 @@ class CV_view(query):
         return CV1_boot
 
     def projection(self, opt_state):
-        return -self.projection_map(-opt_state)
-
+        return opt_state #-self.projection_map(-opt_state)
 
     def condition_on_opt_state(self):
         self.num_opt_var = 0
         self.opt_transform = (None, self.observed_opt_state)
+
 
 
 def projection_cone(p, max_idx, max_sign):