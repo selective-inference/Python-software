1d3eb83134c06c81086528064d0cbfbed118fc57
Jelena Markovic <jelenam@stanford.edu>
2017-01-12 13:11:31 -0800

    added cv in test power file

--- selection/randomized/tests/test_power.py
+++ selection/randomized/tests/test_power.py
@@ -25,54 +25,62 @@ from selection.api import (randomization,
 from selection.randomized.query import naive_confidence_intervals
 
 from selection.randomized.glm import glm_parametric_covariance, glm_nonparametric_bootstrap, restricted_Mest, set_alpha_matrix
+from selection.randomized.cv_view import CV_view
 
 @register_report(['pvalue','BH_decisions', 'active_var'])
 @set_sampling_params_iftrue(SMALL_SAMPLES, ndraw=10, burnin=10)
 @set_seed_iftrue(SET_SEED)
 @wait_for_return_value()
-def test_power(s=30,
+def test_power(s=10,
                n=3000,
                p=1000,
                rho=0.,
                snr=3.5,
-               lam_frac = 0.8,
+               lam_frac = 1.,
                q = 0.2,
+               cross_validation = False,
                ndraw=10000,
                burnin=1000,
                loss='gaussian',
-               nviews=1,
                scalings=False,
                subgrad =True):
 
     if loss=="gaussian":
         X, y, beta, nonzero, sigma = gaussian_instance(n=n, p=p, s=s, rho=rho, snr=snr, sigma=1)
-        lam = lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 2000)))).max(0)) * sigma
+        lam = np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 2000)))).max(0)) * sigma
         loss = rr.glm.gaussian(X, y)
     elif loss=="logistic":
         X, y, beta, _ = logistic_instance(n=n, p=p, s=s, rho=rho, snr=snr)
         loss = rr.glm.logistic(X, y)
-        lam = lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.binomial(1, 1. / 2, (n, 10000)))).max(0))
+        lam = np.mean(np.fabs(np.dot(X.T, np.random.binomial(1, 1. / 2, (n, 10000)))).max(0))
 
     #randomizer = randomization.laplace((p,), scale=sigma)
-    randomizer = randomization.isotropic_gaussian((p,), scale=0.8)
+    randomizer = randomization.isotropic_gaussian((p,), scale=0.5)
 
     epsilon = 1. / np.sqrt(n)
 
-    W = np.ones(p) * lam
-    #W[0] = 0 # use at least some unpenalized
-    penalty = rr.group_lasso(np.arange(p),
-                             weights=dict(zip(np.arange(p), W)), lagrange=1.)
-
     views = []
-    for i in range(nviews):
-        views.append(glm_group_lasso(loss, epsilon, penalty, randomizer))
+    if cross_validation:
+        cv = CV_view(loss)
+        cv.solve()
+        views.append(cv)
+        condition_on_CVR = False
+        if condition_on_CVR:
+            cv.condition_on_opt_state()
+        lam = cv.lam_CVR
+
+    W = lam_frac * np.ones(p) * lam
+    #W[0] = 0 # use at least some unpenalized
+    penalty = rr.group_lasso(np.arange(p), weights=dict(zip(np.arange(p), W)), lagrange=1.)
+    Mest = glm_group_lasso(loss, epsilon, penalty, randomizer)
+
+    views.append(Mest)
 
     queries = multiple_queries(views)
     queries.solve()
 
     active_union = np.zeros(p, np.bool)
-    for view in views:
-        active_union += view.selection_variable['variables']
+    active_union += Mest.selection_variable['variables']
 
     nactive = np.sum(active_union)
     print("nactive", nactive)
@@ -84,16 +92,9 @@ def test_power(s=30,
     if check_screen==False:
 
         if scalings: # try condition on some scalings
-            for i in range(nviews):
-                views[i].condition_on_subgradient()
-                views[i].condition_on_scalings()
+             Mest.condition_on_scalings()
         if subgrad:
-            for i in range(nviews):
-               conditioning_groups = np.zeros(p,dtype=bool)
-               conditioning_groups[:(p/2)] = True
-               marginalizing_groups = np.zeros(p, dtype=bool)
-               marginalizing_groups[(p/2):] = True
-               views[i].decompose_subgradient(conditioning_groups=np.zeros(p, dtype=bool), marginalizing_groups=np.ones(p, bool))
+             Mest.decompose_subgradient(conditioning_groups=np.zeros(p, dtype=bool), marginalizing_groups=np.ones(p, bool))
 
         active_set = np.nonzero(active_union)[0]
         active_var = np.zeros(nactive, np.bool)
@@ -112,9 +113,13 @@ def test_power(s=30,
                                                      sample=target_sample)
 
         from statsmodels.sandbox.stats.multicomp import multipletests
-        BH_desicions = multipletests(pvalues, alpha=q, method="fdr_bh")[0]
+        BH_decisions = multipletests(pvalues, alpha=q, method="fdr_bh")[0]
 
-        return pvalues, BH_desicions, active_var
+        BH_TP = BH_decisions[active_var].sum()
+        FDP = np.true_divide(BH_decisions.sum() - BH_TP, max(BH_decisions.sum(), 1))
+        power = np.true_divide(BH_TP, s)
+        #return pvalues, BH_decisions, active_var # report
+        return FDP, power
 
 def report(niter=50, **kwargs):
 
@@ -129,5 +134,14 @@ def report(niter=50, **kwargs):
     fig.savefig('marginalized_subgrad_pivots.pdf')
 
 
+
 if __name__ == '__main__':
-    report()
+    FDP_sample = []
+    power_sample = []
+    niter = 50
+    for i in range(niter):
+        FDP, power = test_power()[1]
+        FDP_sample.append(FDP)
+        power_sample.append(power)
+        print("FDP mean", np.mean(FDP_sample))
+        print("power mean", np.mean(power_sample))
