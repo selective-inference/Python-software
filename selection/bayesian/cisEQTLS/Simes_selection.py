a96a4b04a9886e677ce198e941523943b43ef1f1
Snigdha Panigrahi <snigdhapanigrahi@DN0a22c531.SUNet>
2017-02-16 12:04:20 -0800

    updated python script on Simes selection

--- selection/bayesian/cisEQTLS/Simes_selection.py
+++ selection/bayesian/cisEQTLS/Simes_selection.py
@@ -2,60 +2,63 @@ from __future__ import print_function
 from scipy.stats import norm as normal
 import numpy as np
 
+#this function takes the nominal p-values as input, computes the Simes test statistic to delclare if gene is significant or not
+def simes_pvalue(p_value):
+
+    p = p_value.shape[0]
+    p_sorted = np.sort(p_value)
+
+    p_simes = (p/(np.arange(p) + 1.))* p_sorted
+
+    return p_simes
+
 #arguments of function are X with normalized columns, response y, sigma_hat and randomization
-def simes_selection(X, y, sigma_hat, alpha, randomizer= 'gaussian'):
+def simes_selection(X, y, alpha, randomizer= 'gaussian'):
 
     n, p = X.shape
 
     if randomizer == 'gaussian':
         perturb = np.random.standard_normal(p)
 
+    sigma_hat = np.sqrt((1.- (X.T.dot(y)**2))/(n-2))
     T_stats = X.T.dot(y)/sigma_hat
-    randomized_T_stats = np.true_divide(T_stats, sigma_hat) + perturb
+    tau = np.max(sigma_hat)/5.
+    perturb = tau * perturb
+
+    randomized_T_stats = T_stats + perturb
     randomized_T_stats_order = np.sort(np.abs(randomized_T_stats))
-    index_sort = np.argsort(np.abs(randomized_T_stats))
-    order_index = np.arange(p)
+    p_val_randomized = np.sort(2*(1. - normal.cdf(np.abs(randomized_T_stats))))
 
-    cut_offs = 1.- (np.arange(p)+1.)*(alpha/(2.*p))
-    threshold = normal.ppf(cut_offs,0.,1.)
+    indices_order = np.argsort(2*(1. - normal.cdf(np.abs(randomized_T_stats))))
+    indices = np.arange(p)
 
-    if np.any(randomized_T_stats_order>= threshold):
+    simes_p_randomized = np.min((p/(np.arange(p) + 1.))* p_val_randomized)
 
-        exceed_T =  order_index[randomized_T_stats_order>= threshold]
+    if simes_p_randomized <= alpha/2:
 
-        t_0 = exceed_T[0]
+        significant =  indices_order[p_val_randomized <= alpha/2]
 
-        i_0 = index_sort[t_0]
+        i_0 = significant[0]
 
-        J = index_sort[:t_0]
+        t_0 = indices[p_val_randomized <= ((np.arange(p) + 1.)/(2*p))* alpha]
+
+        J = indices_order[:t_0[0]]
 
         T_stats_inactive = T_stats[J]
 
         T_stats_active = T_stats[i_0]
 
-        #print(exceed_T, t_0, index_sort, i_0, J)
+        T_stats_inf = np.append(T_stats_inactive,T_stats_active)
 
-        #return T_stats_inactive, T_stats_active, i_0, np.sign(T_stats_active)
-
-        return i_0, np.sign(T_stats_active)
+        return p_val_randomized-((np.arange(p) + 1.)/(2*p))* alpha, simes_p_randomized, i_0, np.sign(T_stats_active), T_stats_inf
 
     else:
 
-        return 0.,0.,0.,0.
+        return simes_p_randomized
 
 
-#this function takes the nominal p-values as input, computes the Simes test statistic to delclare if gene is significant or not
-def simes_wo_randomization(p_value, alpha):
 
-    p = p_value.shape[0]
-    p_sorted = np.sort(p_value)
 
-    p_adjusted = (p/(np.arange(p) + 1.))* p_sorted
-
-    if np.any(p_adjusted)<= alpha/2.:
-        return 1
-    else:
-        return 0
 
 
 
--- selection/bayesian/cisEQTLS/tests/Simes_sel.py
+++ selection/bayesian/cisEQTLS/tests/Simes_sel.py
@@ -6,20 +6,12 @@ from selection.randomized.api import randomization
 from selection.tests.instance import gaussian_instance
 from selection.bayesian.cisEQTLS.Simes_selection import simes_selection
 
-n = 100
-p = 50
-s = 0
-snr = 0.
-
-gene_0 = pandas.read_table("/Users/snigdhapanigrahi/0.txt", na_values="NA")
-print("shape of data",gene_0.shape[0], gene_0.shape[1])
+gene_0 = pandas.read_table("/Users/snigdhapanigrahi/tiny_example/4.txt", na_values="NA")
+print("shape of data", gene_0.shape[0], gene_0.shape[1])
 X = np.array(gene_0.ix[:,2:])
-print("shape of X", X.shape)
+n, p = X.shape
+
 y=  np.array(gene_0.ix[:,1])
-print("shape of y",y.shape)
 
-#print("norms of columns of X", np.linalg.norm(X[:,2]))
-
-#X, y, true_beta, nonzero, noise_variance = gaussian_instance(n=n, p=p, s=s, sigma=1, rho=0, snr=snr, random_signs=True)
-print(simes_selection(X, y, sigma_hat= 1. , alpha=0.05, randomizer= 'gaussian'))
+print(simes_selection(X, y, alpha=0.05, randomizer= 'gaussian'))
 
