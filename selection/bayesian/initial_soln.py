3513f9ca193ca48116ef702ae8fff506ff4b1c84
Snigdha Panigrahi <snigdhapanigrahi@DN51sljs.SUNet>
2017-04-05 00:23:40 -0700

    removed import

--- selection/bayesian/initial_soln.py
+++ selection/bayesian/initial_soln.py
@@ -5,8 +5,8 @@ from rpy2.robjects.packages import importr
 from rpy2 import robjects
 glmnet = importr('glmnet')
 from selection.tests.instance import gaussian_instance
-import rpy2.robjects.numpy2ri
-rpy2.robjects.numpy2ri.activate()
+#import rpy2.robjects.numpy2ri
+#rpy2.robjects.numpy2ri.activate()
 import numpy as np
 import regreg.api as rr
 
