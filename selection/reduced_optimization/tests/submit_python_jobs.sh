a33568cd0e99a842d5299bd7f2da9bcfb8d7ff77
Snigdha Panigrahi <snigdhapanigrahi@DN51sljs.SUNet>
2017-04-05 23:16:21 -0700

    commit change

--- selection/reduced_optimization/tests/submit_python_jobs.sh
+++ selection/reduced_optimization/tests/submit_python_jobs.sh
@@ -9,7 +9,7 @@ DIR=/scratch/users/snigdha/reduced_opt/outputs/fixed_lasso
 
 mkdir -p $DIR
 
-for i in {0..100}
+for i in {0..1}
 do
 	# bash single_python_run.sbatch $i $DIR
 	sbatch single_python_run.sbatch $i $DIR
