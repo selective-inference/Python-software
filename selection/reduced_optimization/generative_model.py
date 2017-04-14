711b9ae1c1286445de222c1b8acab045b0b4faae
Snigdha Panigrahi <snigdhapanigrahi@DN51sk2p.SUNet>
2017-04-13 23:21:42 -0700

    moved class

--- selection/reduced_optimization/generative_model.py
+++ selection/reduced_optimization/generative_model.py
@@ -28,4 +28,26 @@ class generate_data():
 
         return self.X, Y, self.beta * self.sigma, self.sigma
 
+def generate_data_random(n, p, sigma=1., rho=0., scale =True, center=True):
+
+    X = (np.sqrt(1 - rho) * np.random.standard_normal((n, p)) + np.sqrt(rho) * np.random.standard_normal(n)[:, None])
+
+    if center:
+        X -= X.mean(0)[None, :]
+    if scale:
+        X /= (X.std(0)[None, :] * np.sqrt(n))
+
+    beta_true = np.zeros(p)
+    u = np.random.uniform(0., 1., p)
+    for i in range(p):
+        if u[i] <= 0.9:
+            beta_true[i] = np.random.laplace(loc=0., scale=0.1)
+        else:
+            beta_true[i] = np.random.laplace(loc=0., scale=1.)
+
+    beta = beta_true
+
+    Y = (X.dot(beta) + np.random.standard_normal(n)) * sigma
+
+    return X, Y, beta * sigma, sigma
 
--- selection/reduced_optimization/tests/reduced_lasso_bayesian_model.py
+++ selection/reduced_optimization/tests/reduced_lasso_bayesian_model.py
@@ -8,7 +8,35 @@ from selection.bayesian.initial_soln import selection, instance
 
 from selection.reduced_optimization.lasso_reduced import nonnegative_softmax_scaled, neg_log_cube_probability, selection_probability_lasso, \
     sel_prob_gradient_map_lasso, selective_inf_lasso
-from selection.reduced_optimization.generative_model import generate_data
+#from selection.reduced_optimization.generative_model import generate_data
+
+class generate_data():
+
+    def __init__(self, n, p, sigma=1., rho=0., scale =True, center=True):
+         (self.n, self.p, self.sigma, self.rho) = (n, p, sigma, rho)
+
+         self.X = (np.sqrt(1 - self.rho) * np.random.standard_normal((self.n, self.p)) +
+                   np.sqrt(self.rho) * np.random.standard_normal(self.n)[:, None])
+         if center:
+             self.X -= self.X.mean(0)[None, :]
+         if scale:
+             self.X /= (self.X.std(0)[None, :] * np.sqrt(self.n))
+
+         beta_true = np.zeros(p)
+         u = np.random.uniform(0.,1.,p)
+         for i in range(p):
+             if u[i]<= 0.9:
+                 beta_true[i] = np.random.laplace(loc=0., scale=0.1)
+             else:
+                 beta_true[i] = np.random.laplace(loc=0., scale=1.)
+
+         self.beta = beta_true
+
+    def generate_response(self):
+
+        Y = (self.X.dot(self.beta) + np.random.standard_normal(self.n)) * self.sigma
+
+        return self.X, Y, self.beta * self.sigma, self.sigma
 
 def randomized_lasso_trial(X,
                            y,
