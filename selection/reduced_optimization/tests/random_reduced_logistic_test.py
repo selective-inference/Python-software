1fb1fbd285a36c13e9211344234df9dcabf3a4d3
Snigdha Panigrahi <snigdhapanigrahi@DN51sl1t.SUNet>
2017-04-23 19:04:41 -0700

    try dual

--- selection/reduced_optimization/tests/random_reduced_logistic_test.py
+++ selection/reduced_optimization/tests/random_reduced_logistic_test.py
@@ -39,6 +39,8 @@ def randomized_lasso_trial(X,
     M_est = M_estimator_approx_logistic(loss, epsilon, penalty, randomization, randomizer, estimation)
     M_est.solve_approx()
     active = M_est._overall
+    glm = M_est.initial_soln[active]
+    #print("here",glm.shape)
     active_set = np.asarray([i for i in range(p) if active[i]])
     nactive = np.sum(active)
 
@@ -62,6 +64,8 @@ def randomized_lasso_trial(X,
     samples = grad_lasso.posterior_samples()
     adjusted_intervals = np.vstack([np.percentile(samples, 5, axis=0), np.percentile(samples, 95, axis=0)])
 
+    selective_mean = np.mean(samples, axis=0)
+
     true_val = np.zeros(nactive)
 
     coverage_ad = np.zeros(nactive)
@@ -81,8 +85,10 @@ def randomized_lasso_trial(X,
     naive_cov = coverage_unad.sum() / nactive
     ad_len = ad_length.sum() / nactive
     unad_len = unad_length.sum() / nactive
+    bayes_risk_ad = np.power(selective_mean - true_val, 2.).sum() / nactive
+    bayes_risk_unad = np.power(glm - true_val, 2.).sum() / nactive
 
-    return np.vstack([sel_cov, naive_cov, ad_len, unad_len])
+    return np.vstack([sel_cov, naive_cov, ad_len, unad_len, bayes_risk_ad, bayes_risk_unad])
 
 
 if __name__ == "__main__":
@@ -93,16 +99,18 @@ if __name__ == "__main__":
     snr = 0.
 
 
-    niter = 50
+    niter = 44
     ad_cov = 0.
     unad_cov = 0.
     ad_len = 0.
     unad_len = 0.
+    ad_risk = 0.
+    unad_risk = 0.
 
     for i in range(niter):
 
          ### GENERATE X, Y BASED ON SEED
-         np.random.seed(i)  # ensures different X and y
+         np.random.seed(i+6)  # ensures different X and y
          #X, y, beta, nonzero, sigma = gaussian_instance(n=n, p=p, s=s, sigma=1., rho=0., snr=snr)
          # lam = 1. * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 2000)))).max(0)) * sigma
          X, y, beta, nonzero = logistic_instance(n=n, p=p, s=s, rho=0., snr=snr)
@@ -121,15 +129,19 @@ if __name__ == "__main__":
              unad_cov += lasso[1,0]
              ad_len += lasso[2,0]
              unad_len += lasso[3,0]
+             ad_risk += lasso[4, 0]
+             unad_risk += lasso[5, 0]
              print("\n")
              print("iteration completed", i)
              print("\n")
              print("adjusted and unadjusted coverage", ad_cov, unad_cov)
              print("adjusted and unadjusted lengths", ad_len, unad_len)
+             print("adjusted and unadjusted risks", ad_risk, unad_risk)
 
 
     print("adjusted and unadjusted coverage",ad_cov, unad_cov)
     print("adjusted and unadjusted lengths", ad_len, unad_len)
+    print("adjusted and unadjusted risks", ad_risk, unad_risk)
 
 # if __name__ == "__main__":
 #
