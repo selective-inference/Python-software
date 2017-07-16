61c7325397aec7b0f2f01d3bc7098e95bedcc8f2
Snigdha Panigrahi <snigdhapanigrahi@DN51sljd.SUNet>
2017-07-16 15:19:55 -0700

    updated test

--- selection/SLOPE/tests/slope_run_test.py
+++ selection/SLOPE/tests/slope_run_test.py
@@ -17,16 +17,22 @@ import regreg.api as rr
 
 def test_slope_R(X, Y, W):
     robjects.r('''
-    slope = function(X, Y, W=NA, fdr = 0.1){
+    slope = function(X, Y, W=NA, fdr = NA, sigma = 1){
+
+      if(is.na(sigma)){
+      sigma = NULL}
+
+      if(is.na(fdr)){
+      fdr = 0.1 }
 
       if(is.na(W))
       {
-        result = SLOPE(X, Y, fdr = fdr, lambda = "gaussian", sigma =1)
+        result = SLOPE(X, Y, fdr = fdr, lambda = "gaussian", sigma = sigma)
        } else{
-        result = SLOPE(X, Y, fdr = fdr, lambda = W, sigma =1)
+        result = SLOPE(X, Y, fdr = fdr, lambda = W, sigma = sigma, normalize = FALSE)
       }
 
-      return(list(beta = result$beta, E = result$selected))
+      return(list(beta = result$beta, E = result$selected, lambda_seq = result$lambda, sigma = result$sigma))
     }''')
 
     r_slope = robjects.globalenv['slope']
@@ -37,21 +43,29 @@ def test_slope_R(X, Y, W):
     r_W = robjects.r.matrix(W, nrow=p, ncol=1)
     result = r_slope(r_X, r_Y, r_W)
 
-    return result[0]
+    return result[0], result[1], result[2], result[3]
 
 def compare_outputs():
 
-    n, p = 500, 200
+    n, p = 500, 50
 
     X = np.random.standard_normal((n, p))
     Y = np.random.standard_normal(n)
     W = np.linspace(3, 3.5, p)[::-1]
 
     output_R = test_slope_R(X, Y, W)
+    r_beta = output_R[0]
+    r_E = output_R[1]
+    r_lambda_seq = output_R[2]
+    r_sigma = output_R[3]
+    print("output os est coefs R", r_beta)
 
     pen = slope(W, lagrange=1.)
     loss = rr.squared_error(X, Y)
     problem = rr.simple_problem(loss, pen)
     soln = problem.solve()
+    print("output os est coefs python", soln)
+
+    print("difference in solns", soln-r_beta)
 
 compare_outputs()
