3cc2e11b4debf7cb5ebd0e9611d59e64d75c2f8e
Snigdha Panigrahi <snigdhapanigrahi@DN51slgi.SUNet>
2017-08-31 19:25:44 -0700

    updated randomized lasso inference and test_glm

--- selection/approx_ci/ci_via_approx_density.py
+++ selection/approx_ci/ci_via_approx_density.py
@@ -1,83 +1,142 @@
+from __future__ import print_function
 from math import log
+import sys
+from scipy.stats import norm as normal
+
 import numpy as np
 import regreg.api as rr
-from selection.bayesian.selection_probability_rr import nonnegative_softmax_scaled
-from scipy.stats import norm
-import sys
 
-def myround(a, decimals=1):
-    a_x = np.round(a, decimals=1)* 10.
-    rem = np.zeros(a.shape[0], bool)
-    rem[(np.remainder(a_x, 2) == 1)] = 1
-    a_x[rem] = a_x[rem] + 1.
-    return a_x/10.
+from selection.randomized.M_estimator import M_estimator
+
+class M_estimator_map(M_estimator):
+
+    def __init__(self, loss, epsilon, penalty, randomization, randomization_scale = 1.):
+        M_estimator.__init__(self, loss, epsilon, penalty, randomization)
+        self.randomization_scale = randomization_scale
+
+    def solve_approx(self):
+        self.solve()
+        (_opt_linear_term, _opt_affine_term) = self.opt_transform
+        self._opt_linear_term = np.concatenate(
+            (_opt_linear_term[self._overall, :], _opt_linear_term[~self._overall, :]), 0)
+        self._opt_affine_term = np.concatenate((_opt_affine_term[self._overall], _opt_affine_term[~self._overall]), 0)
+        self.opt_transform = (self._opt_linear_term, self._opt_affine_term)
+
+        (_score_linear_term, _) = self.score_transform
+        self._score_linear_term = np.concatenate(
+            (_score_linear_term[self._overall, :], _score_linear_term[~self._overall, :]), 0)
+        self.score_transform = (self._score_linear_term, np.zeros(self._score_linear_term.shape[0]))
+        self.feasible_point = np.abs(self.initial_soln[self._overall])
+        lagrange = []
+        for key, value in self.penalty.weights.iteritems():
+            lagrange.append(value)
+        lagrange = np.asarray(lagrange)
+        self.inactive_lagrange = lagrange[~self._overall]
+
+        X, _ = self.loss.data
+        n, p = X.shape
+        self.p = p
+
+        nactive = self._overall.sum()
+        score_cov = np.zeros((p, p))
+        X_active_inv = np.linalg.inv(X[:,self._overall].T.dot(X[:,self._overall]))
+        projection_perp = np.identity(n) - X[:,self._overall].dot(X_active_inv).dot( X[:,self._overall].T)
+        score_cov[:nactive, :nactive] = X_active_inv
+        score_cov[nactive:, nactive:] = X[:,~self._overall].T.dot(projection_perp).dot(X[:,~self._overall])
+
+        self.score_target_cov = score_cov[:, :nactive]
+        self.target_cov = score_cov[:nactive, :nactive]
+        self.target_observed = self.observed_score_state[:nactive]
+        self.nactive = nactive
+
+        self.B_active = self._opt_linear_term[:nactive, :nactive]
+        self.B_inactive = self._opt_linear_term[nactive:, :nactive]
 
 
-class neg_log_cube_probability_laplace(rr.smooth_atom):
+    def setup_map(self, j):
+
+        self.A = np.dot(self._score_linear_term, self.score_target_cov[:, j]) / self.target_cov[j, j]
+        self.null_statistic = self._score_linear_term.dot(self.observed_score_state) - self.A * self.target_observed[j]
+
+        self.offset_active = self._opt_affine_term[:self.nactive] + self.null_statistic[:self.nactive]
+        self.offset_inactive = self.null_statistic[self.nactive:]
+
+
+class nonnegative_softmax_scaled(rr.smooth_atom):
+    """
+    The nonnegative softmax objective
+    .. math::
+         \mu \mapsto
+         \sum_{i=1}^{m} \log \left(1 +
+         \frac{1}{\mu_i} \right)
+    """
+
+    objective_template = r"""\text{nonneg_softmax}\left(%(var)s\right)"""
+
     def __init__(self,
-                 q, #equals p - E in our case
-                 lagrange,
-                 randomization_scale = 1., #equals the randomization variance in our case
+                 shape,
+                 barrier_scale=1.,
                  coef=1.,
                  offset=None,
-                 quadratic=None):
-
-        self.b = randomization_scale
-        self.lagrange = lagrange
-        self.q = q
+                 quadratic=None,
+                 initial=None):
 
         rr.smooth_atom.__init__(self,
-                                (self.q,),
+                                shape,
                                 offset=offset,
                                 quadratic=quadratic,
-                                initial=None,
+                                initial=initial,
                                 coef=coef)
 
-    def smooth_objective(self, arg, mode='both', check_feasibility=False, tol=1.e-6):
+        # a feasible point
+        self.coefs[:] = np.ones(shape)
+        self.barrier_scale = barrier_scale
 
-        arg = self.apply_offset(arg)
+    def smooth_objective(self, mean_param, mode='both', check_feasibility=False):
+        """
+        Evaluate the smooth objective, computing its value, gradient or both.
+        Parameters
+        ----------
+        mean_param : ndarray
+            The current parameter values.
+        mode : str
+            One of ['func', 'grad', 'both'].
+        check_feasibility : bool
+            If True, return `np.inf` when
+            point is not feasible, i.e. when `mean_param` is not
+            in the domain.
+        Returns
+        -------
+        If `mode` is 'func' returns just the objective value
+        at `mean_param`, else if `mode` is 'grad' returns the gradient
+        else returns both.
+        """
 
-        arg_u = (arg + self.lagrange)/self.b
-        arg_l = (arg - self.lagrange)/self.b
-        scaled_lagrange = (2* self.lagrange)/self.b
+        slack = self.apply_offset(mean_param)
 
-        ind_arg_1 = np.zeros(self.q, bool)
-        ind_arg_1[(arg_u <0.)] = 1
-        ind_arg_2 = np.zeros(self.q, bool)
-        ind_arg_2[(arg_l >0.)] = 1
-        ind_arg_3 = np.logical_and(~ind_arg_1, ~ind_arg_2)
-        cube_prob = np.zeros(self.q)
-        cube_prob[ind_arg_1] = np.exp(arg_u[ind_arg_1])/2. - np.exp(arg_l[ind_arg_1])/2.
-        cube_prob[ind_arg_2] = -np.exp(-arg_u[ind_arg_2])/2. + np.exp(-arg_l[ind_arg_2])/2.
-        cube_prob[ind_arg_3] = 1- np.exp(-arg_u[ind_arg_3])/2. - np.exp(arg_l[ind_arg_3])/2.
-        neg_log_cube_prob = -np.log(cube_prob).sum()
+        if mode in ['both', 'func']:
+            if np.all(slack > 0):
+                f = self.scale(np.log((slack + self.barrier_scale) / slack).sum())
+            else:
+                f = np.inf
+        if mode in ['both', 'grad']:
+            g = self.scale(1. / (slack + self.barrier_scale) - 1. / slack)
 
-        log_cube_grad = np.zeros(self.q)
-        log_cube_grad[ind_arg_1] = 1./self.b
-        log_cube_grad[ind_arg_2] = np.true_divide((np.exp(-scaled_lagrange[ind_arg_2])+ 1.)/self.b,
-                                                  np.exp(-scaled_lagrange[ind_arg_2])-1.)
-        num_cube_grad = np.true_divide(np.exp(-scaled_lagrange[ind_arg_3]), 2 * self.b) - \
-                        np.true_divide(np.exp((2* arg_l[ind_arg_3])), 2 * self.b)
-        den_cube_grad = np.exp(arg_l[ind_arg_3]) - np.exp(-scaled_lagrange[ind_arg_3])/2. - \
-                        np.exp(2* arg_l[ind_arg_3])/2.
-        log_cube_grad[ind_arg_3] = np.true_divide(num_cube_grad,den_cube_grad)
-        neg_log_cube_grad = -log_cube_grad
-
-        if mode == 'func':
-            return self.scale(neg_log_cube_prob)
+        if mode == 'both':
+            return f, g
         elif mode == 'grad':
-            return self.scale(neg_log_cube_grad)
-        elif mode == 'both':
-            return self.scale(neg_log_cube_prob), self.scale(neg_log_cube_grad)
+            return g
+        elif mode == 'func':
+            return f
         else:
             raise ValueError("mode incorrectly specified")
 
 
 class neg_log_cube_probability(rr.smooth_atom):
     def __init__(self,
-                 q, #equals p - E in our case
+                 q,  # equals p - E in our case
                  lagrange,
-                 randomization_scale = 1., #equals the randomization variance in our case
+                 randomization_scale=1.,  # equals the randomization variance in our case
                  coef=1.,
                  offset=None,
                  quadratic=None):
@@ -97,40 +156,58 @@ class neg_log_cube_probability(rr.smooth_atom):
 
         arg = self.apply_offset(arg)
 
-        arg_u = (arg + self.lagrange)/self.randomization_scale
-        arg_l = (arg - self.lagrange)/self.randomization_scale
-        prod_arg = np.exp(-(2. * self.lagrange * arg)/(self.randomization_scale**2))
-        neg_prod_arg = np.exp((2. * self.lagrange * arg)/(self.randomization_scale**2))
-        cube_prob = norm.cdf(arg_u) - norm.cdf(arg_l)
-        log_cube_prob = -np.log(cube_prob).sum()
+        arg_u = (arg + self.lagrange) / self.randomization_scale
+        arg_l = (arg - self.lagrange) / self.randomization_scale
+        prod_arg = np.exp(-(2. * self.lagrange * arg) / (self.randomization_scale ** 2))
+        neg_prod_arg = np.exp((2. * self.lagrange * arg) / (self.randomization_scale ** 2))
+        cube_prob = normal.cdf(arg_u) - normal.cdf(arg_l)
+
         threshold = 10 ** -10
         indicator = np.zeros(self.q, bool)
         indicator[(cube_prob > threshold)] = 1
         positive_arg = np.zeros(self.q, bool)
-        positive_arg[(arg>0)] = 1
+        positive_arg[(arg > 0)] = 1
         pos_index = np.logical_and(positive_arg, ~indicator)
         neg_index = np.logical_and(~positive_arg, ~indicator)
+
+        log_cube_prob = np.zeros(self.q)
+        log_cube_prob[indicator] = -np.log(cube_prob)[indicator]
+
+        random_var = self.randomization_scale ** 2
+        log_cube_prob[neg_index] = (arg[neg_index] ** 2. / (2. * random_var)) + (
+        arg[neg_index] * self.lagrange[neg_index] / random_var) + \
+                                   (self.lagrange[neg_index] ** 2. / (2. * random_var)) \
+                                   - np.log(
+            1. / np.abs(arg_u[neg_index]) - neg_prod_arg[neg_index] / np.abs(arg_l[neg_index]))
+
+        log_cube_prob[pos_index] = (arg[pos_index] ** 2. / (2. * random_var)) - (
+        arg[pos_index] * self.lagrange[pos_index] / random_var) + \
+                                   (self.lagrange[pos_index] ** 2. / (2. * random_var)) \
+                                   - np.log(
+            1. / np.abs(arg_l[pos_index]) - prod_arg[pos_index] / np.abs(arg_u[pos_index]))
+
+        neg_log_cube_prob = log_cube_prob.sum()
+
         log_cube_grad = np.zeros(self.q)
-        log_cube_grad[indicator] = (np.true_divide(-norm.pdf(arg_u[indicator]) + norm.pdf(arg_l[indicator]),
-                                        cube_prob[indicator]))/self.randomization_scale
+        log_cube_grad[indicator] = (np.true_divide(-normal.pdf(arg_u[indicator]) + normal.pdf(arg_l[indicator]),
+                                                   cube_prob[indicator])) / self.randomization_scale
 
-        log_cube_grad[pos_index] = ((-1. + prod_arg[pos_index])/
-                                     ((prod_arg[pos_index]/arg_u[pos_index])-
-                                      (1./arg_l[pos_index])))/self.randomization_scale
+        log_cube_grad[pos_index] = ((-1. + prod_arg[pos_index]) /
+                                    ((prod_arg[pos_index] / np.abs(arg_u[pos_index])) -
+                                     (1. / np.abs(arg_l[pos_index])))) / self.randomization_scale
 
-        log_cube_grad[neg_index] = ((arg_u[neg_index] -(arg_l[neg_index]*neg_prod_arg[neg_index]))
-                                    /self.randomization_scale)/(1.- neg_prod_arg[neg_index])
-
+        log_cube_grad[neg_index] = ((-1. + neg_prod_arg[neg_index]) /
+                                    ((-neg_prod_arg[neg_index] / np.abs(arg_l[neg_index])) +
+                                     (1. / np.abs(arg_u[neg_index])))) / self.randomization_scale
 
         if mode == 'func':
-            return self.scale(log_cube_prob)
+            return self.scale(neg_log_cube_prob)
         elif mode == 'grad':
             return self.scale(log_cube_grad)
         elif mode == 'both':
-            return self.scale(log_cube_prob), self.scale(log_cube_grad)
+            return self.scale(neg_log_cube_prob), self.scale(log_cube_grad)
         else:
             raise ValueError("mode incorrectly specified")
-
 
 class approximate_conditional_prob(rr.smooth_atom):
 
@@ -176,10 +253,8 @@ class approximate_conditional_prob(rr.smooth_atom):
         active_conj_loss = rr.affine_smooth(self.active_conjugate,
                                             rr.affine_transform(self.map.B_active, offset_active))
 
-        if self.map.randomizer == 'laplace':
-            cube_obj = neg_log_cube_probability_laplace(self.q, self.inactive_lagrange, randomization_scale = 1.)
-        elif self.map.randomizer == 'gaussian':
-            cube_obj = neg_log_cube_probability(self.q, self.inactive_lagrange, randomization_scale = 1.)
+
+        cube_obj = neg_log_cube_probability(self.q, self.inactive_lagrange, randomization_scale = self.map.randomization_scale)
 
         cube_loss = rr.affine_smooth(cube_obj, rr.affine_transform(self.map.B_inactive, offset_inactive))
 
@@ -269,8 +344,6 @@ class approximate_conditional_density(rr.smooth_atom):
                                 quadratic=quadratic,
                                 coef=coef)
 
-        self.coefs[:] = 0.
-
         self.target_observed = self.sel_alg.target_observed
         self.nactive = self.target_observed.shape[0]
         self.target_cov = self.sel_alg.target_cov
@@ -278,54 +351,58 @@ class approximate_conditional_density(rr.smooth_atom):
     def solve_approx(self):
 
         #defining the grid on which marginal conditional densities will be evaluated
-        grid_length = 301
+        self.grid_length = 241
 
-        #self.grid = np.linspace(-15,65, num=grid_length)
-        #self.grid = np.linspace(-5*np.amax(np.absolute(target_observed)), 5*np.amax(np.absolute(target_observed)), num=grid_length)
-        #s_obs = np.round(self.target_observed, decimals =1)
-        self.grid = np.zeros((self.nactive, grid_length))
-
-        print("observed values", self.target_observed)
+        #print("observed values", self.target_observed)
         self.ind_obs = np.zeros(self.nactive, int)
         self.norm = np.zeros(self.nactive)
-        self.h_approx = np.zeros((self.nactive, self.grid.shape[0]))
+        self.h_approx = np.zeros((self.nactive, self.grid_length))
+        self.grid = np.zeros((self.nactive, self.grid_length))
 
         for j in range(self.nactive):
             obs = self.target_observed[j]
-            self.grid[j, :] = np.linspace(self.target_observed[j] - 15., self.target_observed[j] + 15., num=grid_length)
+
+            self.grid[j,:] = np.linspace(self.target_observed[j]-12., self.target_observed[j]+12.,num=self.grid_length)
+
             self.norm[j] = self.target_cov[j,j]
-            if obs < self.grid[0]:
+            if obs < self.grid[j,0]:
                 self.ind_obs[j] = 0
-            elif obs > np.max(self.grid):
-                self.ind_obs[j] = grid_length-1
+            elif obs > np.max(self.grid[j,:]):
+                self.ind_obs[j] = self.grid_length-1
             else:
                 self.ind_obs[j] = np.argmin(np.abs(self.grid[j,:]-obs))
 
             sys.stderr.write("number of variable being computed: " + str(j) + "\n")
             self.h_approx[j, :] = self.approx_conditional_prob(j)
 
-
     def approx_conditional_prob(self, j):
         h_hat = []
 
         self.sel_alg.setup_map(j)
 
-        for i in xrange(self.grid[j, :].shape[0]):
+        for i in range(self.grid[j, :].shape[0]):
             approx = approximate_conditional_prob((self.grid[j, :])[i], self.sel_alg)
-            val = -(approx.minimize2(step=1, nstep=100)[::-1])[0]
+            val = -(approx.minimize2(step=1, nstep=200)[::-1])[0]
 
             if val != -float('Inf'):
                 h_hat.append(val)
-            else:
+            elif val == -float('Inf') and i == 0:
+                h_hat.append(-500.)
+            elif val == -float('Inf') and i > 0:
                 h_hat.append(h_hat[i - 1])
+
+            #sys.stderr.write("point on grid: " + str(i) + "\n")
+            #sys.stderr.write("value on grid: " + str(h_hat[i]) + "\n")
+
+        return np.array(h_hat)
 
     def area_normalized_density(self, j, mean):
 
         normalizer = 0.
-        grad_normalizer = 0.
         approx_nonnormalized = []
+        grad_normalizer = 0.
 
-        for i in range(self.grid.shape[0]):
+        for i in range(self.grid_length):
             approx_density = np.exp(-np.true_divide(((self.grid[j,:])[i] - mean) ** 2, 2 * self.norm[j])
                                     + (self.h_approx[j,:])[i])
             normalizer += approx_density
@@ -338,13 +415,13 @@ class approximate_conditional_density(rr.smooth_atom):
 
         param = self.apply_offset(param)
 
-        approx_normalizer = self.area_normalized_density(j,param)
+        approx_normalizer = self.area_normalized_density(j, param)
 
-        f = (param**2)/(2*self.norm[j]) - (self.target_observed[j]*param)/self.norm[j] + \
+        f = (param ** 2) / (2 * self.norm[j]) - (self.target_observed[j] * param) / self.norm[j] + \
             log(approx_normalizer[1])
 
-        g = param/self.norm[j] - self.target_observed[j]/self.norm[j] + \
-            approx_normalizer[2]/approx_normalizer[1]
+        g = param / self.norm[j] - self.target_observed[j] / self.norm[j] + \
+            approx_normalizer[2] / approx_normalizer[1]
 
         if mode == 'func':
             return self.scale(f)
@@ -355,7 +432,7 @@ class approximate_conditional_density(rr.smooth_atom):
         else:
             raise ValueError("mode incorrectly specified")
 
-    def approx_MLE_solver(self, j, step=1, nstep=100, tol=1.e-5):
+    def approx_MLE_solver(self, j, step=1, nstep=150, tol=1.e-5):
 
         current = self.target_observed[j]
         current_value = np.inf
@@ -391,11 +468,13 @@ class approximate_conditional_density(rr.smooth_atom):
                 step *= 2
 
         value = objective(current)
+
         return current, value
 
     def approximate_ci(self, j):
 
-        param_grid = np.linspace(-15., 15., num=301)
+        grid_num = 301
+        param_grid = np.linspace(-10,10, num=grid_num)
         area = np.zeros(param_grid.shape[0])
 
         for k in range(param_grid.shape[0]):
@@ -413,4 +492,4 @@ class approximate_conditional_density(rr.smooth_atom):
         area_vec = self.area_normalized_density(j, param)[0]
         area = area_vec[self.ind_obs[j]]
 
-        return 2*min(area, 1-area)
+        return 2*min(area, 1.-area)
--- selection/approx_ci/tests/test_glm.py
+++ selection/approx_ci/tests/test_glm.py
@@ -1,119 +1,122 @@
 from __future__ import print_function
 import numpy as np
-import time
+import sys
 import regreg.api as rr
-import selection.tests.reports as reports
 from selection.tests.instance import logistic_instance, gaussian_instance
-from selection.approx_ci.ci_via_approx_density import approximate_conditional_density
-from selection.approx_ci.estimator_approx import M_estimator_approx
-
-from selection.tests.flags import SMALL_SAMPLES, SET_SEED
-from selection.tests.decorators import wait_for_return_value, register_report, set_sampling_params_iftrue
+from selection.approx_ci.randomized_lasso import M_estimator_map, approximate_conditional_density
 from selection.randomized.query import naive_confidence_intervals
 from selection.randomized.query import naive_pvalues
 
-
-@register_report(['cover', 'ci_length', 'truth', 'naive_cover', 'naive_pvalues'])
-@set_sampling_params_iftrue(SMALL_SAMPLES, ndraw=10, burnin=10)
-@wait_for_return_value()
-def test_approximate_ci(n=100,
-                        p=10,
-                        s=3,
-                        snr=5,
-                        rho=0.1,
-                        lam_frac = 1.,
-                        loss='gaussian',
-                        randomizer='gaussian'):
+def test_approximate_inference(X,
+                               y,
+                               true_mean,
+                               sigma,
+                               seed_n = 0,
+                               lam_frac = 1.,
+                               loss='gaussian',
+                               randomization_scale = 1.):
 
     from selection.api import randomization
 
+    n, p = X.shape
+    np.random.seed(seed_n)
     if loss == "gaussian":
-        X, y, beta, nonzero, sigma = gaussian_instance(n=n, p=p, s=s, rho=rho, snr=snr, sigma=1.)
         lam = lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 2000)))).max(0)) * sigma
         loss = rr.glm.gaussian(X, y)
     elif loss == "logistic":
-        X, y, beta, _ = logistic_instance(n=n, p=p, s=s, rho=rho, snr=snr)
-        loss = rr.glm.logistic(X, y)
         lam = lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.binomial(1, 1. / 2, (n, 10000)))).max(0))
+        loss = rr.glm.logistic(X, y)
 
     epsilon = 1. / np.sqrt(n)
 
     W = np.ones(p) * lam
     penalty = rr.group_lasso(np.arange(p),
                              weights=dict(zip(np.arange(p), W)), lagrange=1.)
-    if randomizer=='gaussian':
-        randomization = randomization.isotropic_gaussian((p,), scale=1.)
-    elif randomizer=='laplace':
-        randomization = randomization.laplace((p,), scale=1.)
 
-    M_est = M_estimator_approx(loss, epsilon, penalty, randomization, randomizer)
+    randomization = randomization.isotropic_gaussian((p,), scale=randomization_scale)
+    M_est = M_estimator_map(loss, epsilon, penalty, randomization, randomization_scale = randomization_scale)
+
     M_est.solve_approx()
-    ci = approximate_conditional_density(M_est)
-    ci.solve_approx()
-
     active = M_est._overall
     active_set = np.asarray([i for i in range(p) if active[i]])
-
-    true_support = np.asarray([i for i in range(p) if i < s])
-
     nactive = np.sum(active)
+    sys.stderr.write("number of active selected by lasso" + str(nactive) + "\n")
+    sys.stderr.write("Active set selected by lasso" + str(active_set) + "\n")
+    sys.stderr.write("Observed target" + str(M_est.target_observed) + "\n")
 
-    print("active set, true_support", active_set, true_support)
+    if nactive == 0:
+        return None
 
-    true_vec = beta[active]
+    else:
+        true_vec = np.linalg.inv(X[:, active].T.dot(X[:, active])).dot(X[:, active].T).dot(true_mean)
 
-    print("true coefficients", true_vec)
-
-    if (set(active_set).intersection(set(true_support)) == set(true_support))== True:
-
-        ci_active = np.zeros((nactive, 2))
-        #mle_active = np.zeros(nactive)
-        covered = np.zeros(nactive, np.bool)
-        ci_length = np.zeros(nactive)
-        pivots = np.zeros(nactive)
+        sys.stderr.write("True target to be covered" + str(true_vec) + "\n")
 
         class target_class(object):
             def __init__(self, target_cov):
                 self.target_cov = target_cov
                 self.shape = target_cov.shape
+
         target = target_class(M_est.target_cov)
 
         ci_naive = naive_confidence_intervals(target, M_est.target_observed)
         naive_pvals = naive_pvalues(target, M_est.target_observed, true_vec)
-        naive_covered = np.zeros(nactive)
-        toc = time.time()
+
+        ci = approximate_conditional_density(M_est)
+        ci.solve_approx()
+
+        ci_sel = np.zeros((nactive, 2))
+        sel_MLE = np.zeros(nactive)
+        sel_length = np.zeros(nactive)
 
         for j in range(nactive):
-            ci_active[j, :] = np.array(ci.approximate_ci(j))
-            #mle_active[j] = ci.approx_MLE_solver(j, nstep= 100)[0]
-            if (ci_active[j, 0] <= true_vec[j]) and (ci_active[j,1] >= true_vec[j]):
-                covered[j] = 1
-            ci_length[j] = ci_active[j,1] - ci_active[j,0]
-            print(ci_active[j, :])
-            pivots[j] = ci.approximate_pvalue(j, true_vec[j])
+            ci_sel[j, :] = np.array(ci.approximate_ci(j))
+            sel_MLE[j] = ci.approx_MLE_solver(j, step=1, nstep=150)[0]
+            sel_length[j] = ci_sel[j, 1] - ci_sel[j, 0]
 
-            # naive ci
-            if (ci_naive[j,0]<=true_vec[j]) and (ci_naive[j,1]>=true_vec[j]):
-                naive_covered[j]+=1
+        sel_covered = np.zeros(nactive, np.bool)
+        sel_risk = np.zeros(nactive)
+        naive_covered = np.zeros(nactive)
+        naive_risk = np.zeros(nactive)
 
-        tic = time.time()
-        print('ci time now', tic - toc)
-        return covered, ci_length, pivots, naive_covered, naive_pvals
+        for j in range(nactive):
+
+            sel_risk[j] = (sel_MLE[j] - true_vec[j]) ** 2.
+            naive_risk[j] = (M_est.target_observed[j]- true_vec[j]) ** 2.
+
+            if (ci_sel[j, 0] <= true_vec[j]) and (ci_sel[j, 1] >= true_vec[j]):
+                sel_covered[j] = 1
+            if (ci_naive[j, 0] <= true_vec[j]) and (ci_naive[j, 1] >= true_vec[j]):
+                naive_covered[j] = 1
+
+        print("lengths", sel_length.sum()/nactive)
+        print("selective intervals", ci_sel.T)
+        print("risks", sel_risk.sum() / nactive)
+
+        return np.transpose(np.vstack((ci_sel[:, 0],
+                                       ci_sel[:, 1],
+                                       ci_naive[:,0],
+                                       ci_naive[:, 1],
+                                       sel_MLE,
+                                       M_est.target_observed,
+                                       sel_covered,
+                                       naive_covered,
+                                       sel_risk,
+                                       naive_risk)))
 
 
-def report(niter=50, **kwargs):
+def test_lasso(n, p, s, signal):
+    X, y, beta, nonzero, sigma = gaussian_instance(n=n, p=p, s=s, rho=0., signal=signal, sigma=1.)
+    true_mean = X.dot(beta)
+    lasso = test_approximate_inference(X,
+                                       y,
+                                       true_mean,
+                                       sigma,
+                                       seed_n=0,
+                                       lam_frac=1.,
+                                       loss='gaussian')
 
-    kwargs = {'s': 0, 'n': 200, 'p': 30, 'snr': 7, 'loss': 'gaussian', 'randomizer':'gaussian'}
-    split_report = reports.reports['test_approximate_ci']
-    screened_results = reports.collect_multiple_runs(split_report['test'],
-                                                     split_report['columns'],
-                                                     niter,
-                                                     reports.summarize_all,
-                                                     **kwargs)
+    if lasso is not None:
+        print("output of selection adjusted inference", lasso)
+        return(lasso)
 
-    fig = reports.pivot_plot_plus_naive(screened_results)
-    fig.savefig('approx_pivots_glm.pdf')
-
-
-if __name__=='__main__':
-    report()
