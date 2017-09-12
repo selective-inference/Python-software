6853b610e3fe210d443d1c51aece67963754a234
Jelena Markovic <jelenam@stanford.edu>
2017-09-12 10:29:02 -0700

    plotting pivots

--- selection/randomized/tests/test_opt_weighted_intervals.py
+++ selection/randomized/tests/test_opt_weighted_intervals.py
@@ -9,6 +9,7 @@ from ...tests.instance import (gaussian_instance,
                                poisson_instance)
 from ...tests.flags import SMALL_SAMPLES
 from ...tests.decorators import set_sampling_params_iftrue, set_seed_iftrue
+import matplotlib.pyplot as plt
 
 from scipy.stats import t as tdist
 from ..glm import target as glm_target, glm_nonparametric_bootstrap, pairs_bootstrap_glm
@@ -18,12 +19,13 @@ from ..M_estimator import restricted_Mest
 @set_sampling_params_iftrue(SMALL_SAMPLES, ndraw=1000, burnin=100)
 def test_opt_weighted_intervals(ndraw=20000, burnin=2000):
 
+    results=[]
     cls = lasso
-    for const_info, rand in product(zip([gaussian_instance], [cls.gaussian]), ['laplace']):
+    for const_info, rand in product(zip([gaussian_instance], [cls.gaussian]), ['laplace', 'gaussian']):
 
         inst, const = const_info
 
-        X, Y = inst(n=100, p=10, s=0)[:2]
+        X, Y, beta = inst(n=100, p=10, s=3)[:3]
         n, p = X.shape
 
         W = np.ones(X.shape[1]) * 1
@@ -63,9 +65,35 @@ def test_opt_weighted_intervals(ndraw=20000, burnin=2000):
         opt_sampler.setup_target(boot_target,
                                  form_covariances)
 
-        selective_pvalues = opt_sampler.coefficient_pvalues(unpenalized_mle, sample=S)
-        print("pvalues ", selective_pvalues)
-        selective_CI = opt_sampler.confidence_intervals(unpenalized_mle, sample=S)
-        print(selective_CI)
+        sel_pivots = opt_sampler.coefficient_pvalues(unpenalized_mle, parameter = beta[selected_features], sample=S)
+        print("pivots ", sel_pivots)
+        results.append((sel_pivots,))
+        #selective_CI = opt_sampler.confidence_intervals(unpenalized_mle, sample=S)
+        #print(selective_CI)
 
-        return selective_CI
+    return results
+
+from statsmodels.distributions import ECDF
+
+
+def main(ndraw=10000, burnin=2000, nsim=2):
+
+    sel_pivots_all = [[],[]]
+    for i in range(nsim):
+        for idx, (sel_pivots,) in enumerate(test_opt_weighted_intervals(ndraw=ndraw, burnin=burnin)):
+            sel_pivots_all[idx].append(sel_pivots)
+
+    for idx in range(2):
+
+        fig = plt.figure(num=idx, figsize=(1,1))
+        plt.clf()
+        xval = np.linspace(0,1,50)
+        flat_list = [item for sublist in sel_pivots_all[idx] for item in sublist]
+        plt.plot(xval, ECDF(flat_list)(xval), label='selective')
+        plt.plot(xval, xval, 'k-', lw=1)
+        plt.legend(loc='lower right')
+
+
+    plt.show()
+
+
