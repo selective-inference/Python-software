aefa2121e7db871080b7e416cca8468a921e2674
Snigdha Panigrahi <snigdhapanigrahi@DN525j9t.SUNet>
2017-11-07 13:28:17 -0800

    test for pivots based on approx MLE

--- selection/adjusted_MLE/tests/approx_MLE.py
+++ selection/adjusted_MLE/tests/approx_MLE.py
@@ -73,5 +73,33 @@ def approx_fisher_info(mu, randomization_scale=0.5, threshold=2):
     minimizer = approx_grad_cgf(mu)[2]
     return (1./ variance**2.)* (1./((1./variance) + grad_log_hessian(minimizer, variance)))+ ((randomization_scale ** 2.)/variance)
 
-#print("grad cgf check", approx_grad_cgf(-1)[0])
-print("fisher info check", approx_fisher_info(-2))
+def simulate_truncated(mu, randomization_scale = 0.5, threshold = 2):
+    while True:
+        Z = np.random.normal(mu, 1, 1)
+        W = np.random.normal(0, randomization_scale, 1)
+        if (Z + W > threshold):
+            return Z
+
+def test_pivot(mu, randomization_scale=0.5, threshold=2):
+    Z = np.array([simulate_truncated(mu, randomization_scale=randomization_scale, threshold=threshold) for _ in
+                  range(25000)])
+
+    mu_seq = np.linspace(-7., 6, num=2600)
+    grad_partition = np.zeros(mu_seq.shape[0])
+    for i in range(mu_seq.shape[0]):
+        grad_partition[i] = approx_grad_cgf(mu_seq[i])[0]
+
+    pivot = []
+    approx_MLE = []
+    sd_MLE = 1 / np.sqrt(approx_fisher_info(mu))
+    for k in range(Z.shape[0]):
+        MLE = mu_seq[np.argmin(np.abs(grad_partition - Z[k]))]
+        approx_MLE.append(MLE)
+        pivot.append((MLE - mu) / sd_MLE)
+
+    return np.asarray(pivot), np.asarray(approx_MLE)
+
+print(test_pivot(1))
+
+    #print("grad cgf check", approx_grad_cgf(-1)[0])
+#print("fisher info check", approx_fisher_info(-2))
