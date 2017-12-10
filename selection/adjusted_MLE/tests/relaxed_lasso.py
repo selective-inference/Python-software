e931db63664cce3d788042a2c032ed47ce77b986
Snigdha Panigrahi <>
2017-12-09 23:41:27 -0800

    return tuned relaxed lasso est

--- selection/adjusted_MLE/tests/relaxed_lasso.py
+++ selection/adjusted_MLE/tests/relaxed_lasso.py
@@ -23,9 +23,7 @@ def sim_xy(n, p, nval, rho=0, s=5, beta_type=2, snr=1):
 
     return X, y, X_val, y_val, Sigma, beta, sigma
 
-sim_xy(n=50, p=10, nval=50)
-
-def tuned_lasso(X, Y, X_val,Y_val):
+def tuned_lasso(X, y, X_val,y_val):
     robjects.r('''
         source('~/best-subset/bestsubset/R/lasso.R')
         tuned_lasso_estimator = function(X,Y,X.val,Y.val){
@@ -34,7 +32,7 @@ def tuned_lasso(X, Y, X_val,Y_val):
         Y.val = as.vector(Y.val)
         X.val = as.matrix(X.val)
 
-        rel.lasso = lasso(X,Y,intercept=TRUE, nrelax=5, nlam=50)
+        rel.lasso = lasso(X,Y,intercept=FALSE, nrelax=10, nlam=50)
         beta.hat = as.matrix(coef(rel.lasso))
 
         muhat.val = as.matrix(predict(rel.lasso, X.val))
@@ -47,12 +45,13 @@ def tuned_lasso(X, Y, X_val,Y_val):
     n, p = X.shape
     nval, _ = X_val.shape
     r_X = robjects.r.matrix(X, nrow=n, ncol=p)
-    r_y = robjects.r.matrix(Y, nrow=n, ncol=1)
+    r_y = robjects.r.matrix(y, nrow=n, ncol=1)
 
     r_X_val = robjects.r.matrix(X_val, nrow=nval, ncol=p)
-    r_y_val = robjects.r.matrix(Y_val, nrow=nval, ncol=1)
-    estimator = r_lasso(r_X, r_y, r_X_val, r_y_val)
-    return (estimator)
+    r_y_val = robjects.r.matrix(y_val, nrow=nval, ncol=1)
+    estimator = np.array(r_lasso(r_X, r_y, r_X_val, r_y_val))
+    return estimator
 
-#print(tuned_lasso(np.random.standard_normal((50,10)), np.random.standard_normal(50),
-#                  np.random.standard_normal((50,10)), np.random.standard_normal(50)))
+X, y, X_val, y_val, Sigma, beta, sigma = sim_xy(n=500, p=100, nval=500, rho=0.35, s=5, beta_type=2, snr=0.2)
+rel_LASSO = tuned_lasso(X, y, X_val,y_val)
+print("relaxed LASSO", rel_LASSO)
