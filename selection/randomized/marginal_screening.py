96a73789382352aa4398847bf1e66fde3ff51e83
Jonathan Taylor <jonathan.taylor@stanford.edu>
2018-05-03 10:40:37 -0700

    refactor marginal screening, added summary

--- selection/randomized/marginal_screening.py
+++ selection/randomized/marginal_screening.py
@@ -32,35 +32,35 @@ def BH_selection(p_values, level):
     p_sorted = np.sort(p_values)
     indices = np.arange(m)
     indices_order = np.argsort(p_values)
-    order_sig = np.max(indices[p_sorted - level * (np.arange(m) + 1.) / m <= 0])
-    E_sel = indices_order[:(order_sig+1)]
-    not_sel =indices_order[(order_sig+1):]
+    first_step = p_sorted - level * (np.arange(m) + 1.) / m <= 0
+    if np.any(first_step):
+        order_sig = np.max(indices[first_step])
+        E_sel = indices_order[:(order_sig+1)]
+        not_sel = indices_order[(order_sig+1):]
 
-    active = np.zeros(m, np.bool)
-    active[E_sel] = 1
+        active = np.zeros(m, np.bool)
+        active[E_sel] = 1
 
-    return order_sig + 1, active, np.argsort(p_values[np.sort(not_sel)])
+        return order_sig + 1, active, np.argsort(p_values[np.sort(not_sel)])
+    else:
+        return 0, np.zeros(m, np.bool), np.argsort(p_values)
 
 class marginal_screening(object):
 
     def __init__(self,
-                 X,
-                 Y,
-                 sigma_hat, # SDs of each score
+                 observed_score,
+                 covariance, 
                  randomizer_scale,
                  marginal_level,
                  perturb=None):
 
-        observed_score = -X.T.dot(Y)
+        self.observed_score = observed_score  # - Z if Z \sim N(\mu,\Sigma)
         self.nfeature = p = observed_score.shape[0]
-        self.sigma_hat = sigma_hat
-
+        self.covariance = covariance
+        self.randomized_stdev = np.sqrt(np.diag(covariance) + randomizer_scale**2)
         self.randomizer = randomization.isotropic_gaussian((p,), randomizer_scale)
         self._initial_omega = perturb
-        self.observed_score = observed_score
-
         self.marginal_level = marginal_level
-        self.data = (X, Y)
 
     def fit(self, perturb=None):
 
@@ -73,10 +73,10 @@ class marginal_screening(object):
             self._initial_omega = self.randomizer.sample()
 
         randomized_score = -self.observed_score + self._initial_omega
-        p_values = 2. * (1. - ndist.cdf(np.abs(randomized_score) / self.sigma_hat))
+        p_values = 2. * (1. - ndist.cdf(np.abs(randomized_score) / self.randomized_stdev))
         K, active, sort_notsel_pvals = marginal_screening_selection(p_values, self.marginal_level)
 
-        self.threshold = self.sigma_hat * ndist.ppf(1. - self.marginal_level / 2.)
+        self.threshold = self.randomized_stdev * ndist.ppf(1. - self.marginal_level / 2.)
         self._selected = active
         self._not_selected = ~self._selected
         sign = np.sign(randomized_score)
@@ -85,12 +85,13 @@ class marginal_screening(object):
         self.selection_variable = {'sign': sign,
                                    'variables': self._selected.copy()}
 
-        self.observed_opt_state = self._initial_omega[self._selected] - self.observed_score[self._selected] - \
-                                  np.diag(active_signs).dot(self.threshold[self._selected])
+        self.observed_opt_state = (self._initial_omega[self._selected] - 
+                                   self.observed_score[self._selected] - 
+                                   np.diag(active_signs).dot(self.threshold[self._selected]))
         self.num_opt_var = self.observed_opt_state.shape[0]
 
         opt_linear = np.zeros((p, self.num_opt_var))
-        opt_linear[self._selected, :] = np.diag(active_signs)
+        opt_linear[self._selected,:] = np.diag(active_signs)
         opt_offset = np.zeros(p)
         opt_offset[self._selected] = active_signs * self.threshold[self._selected]
         opt_offset[self._not_selected] = randomized_score[self._not_selected]
@@ -104,7 +105,7 @@ class marginal_screening(object):
         cond_mean = -logdens_linear.dot(self.observed_score + opt_offset)
 
         logdens_transform = (logdens_linear, opt_offset)
-        A_scaling = -np.identity(self.num_opt_var)
+        A_scaling = -np.diag(active_signs)
         b_scaling = np.zeros(self.num_opt_var)
 
         def log_density(logdens_linear, offset, cond_prec, score, opt):
@@ -130,59 +131,69 @@ class marginal_screening(object):
                                                selection_info=self.selection_variable)
         return self._selected
 
-    def selective_MLE(self,
-                      target="selected",
-                      features=None,
-                      parameter=None,
-                      level=0.9,
-                      compute_intervals=False,
-                      dispersion=None,
-                      solve_args={'tol': 1.e-12}):
+    def summary(self,
+                observed_target,
+                cov_target,
+                cov_target_score,
+                alternatives,
+                parameter=None,
+                level=0.9,
+                ndraw=10000,
+                burnin=2000,
+                compute_intervals=False):
         """
+        Produce p-values and confidence intervals for targets
+        of model including selected features
         Parameters
         ----------
-        target : one of ['selected', 'full']
-        features : np.bool
-            Binary encoding of which features to use in final
-            model and targets.
-        parameter : np.array
-            Hypothesized value for parameter -- defaults to 0.
-        level : float
-            Confidence level.
-        ndraw : int (optional)
-            Defaults to 1000.
-        burnin : int (optional)
-            Defaults to 1000.
-        compute_intervals : bool
-            Compute confidence intervals?
-        dispersion : float (optional)
-            Use a known value for dispersion, or Pearson's X^2?
         """
 
         if parameter is None:
-            parameter = np.zeros(self.nfeature)
+            parameter = np.zeros(observed_target.shape[0])
 
-        if target == 'selected':
-            (observed_target, 
-             cov_target, 
-             cov_target_score, 
-             alternatives) = self.selected_targets(features=features,
-                                                   dispersion=dispersion)
-        elif target == 'full':
-            X, y = self.data
-            n, p = X.shape
-            if n > p:
-                (observed_target, 
-                 cov_target, 
-                 cov_target_score, 
-                 alternatives) = self.full_targets(features=features,
-                                                   dispersion=dispersion)
-            else:
-                (observed_target, 
-                 cov_target, 
-                 cov_target_score, 
-                 alternatives) = self.debiased_targets(features=features,
-                                                       dispersion=dispersion)
+        opt_sample = self.sampler.sample(ndraw, burnin)
+
+        pivots = self.sampler.coefficient_pvalues(observed_target,
+                                                  cov_target,
+                                                  cov_target_score,
+                                                  parameter=parameter,
+                                                  sample=opt_sample,
+                                                  alternatives=alternatives)
+        if not np.all(parameter == 0):
+            pvalues = self.sampler.coefficient_pvalues(observed_target,
+                                                       cov_target,
+                                                       cov_target_score,
+                                                       parameter=np.zeros_like(parameter),
+                                                       sample=opt_sample,
+                                                       alternatives=alternatives)
+        else:
+            pvalues = pivots
+
+        intervals = None
+        if compute_intervals:
+            intervals = self.sampler.confidence_intervals(observed_target,
+                                                          cov_target,
+                                                          cov_target_score,
+                                                          sample=opt_sample)
+
+        return pivots, pvalues, intervals
+
+    def selective_MLE(self,
+                      observed_target,
+                      cov_target,
+                      cov_target_score,
+                      alternatives,
+                      level=0.9,
+                      solve_args={'tol':1.e-12}):
+        """
+
+        Parameters
+        ----------
+
+        level : float
+            Confidence level.
+
+        """
 
         return self.sampler.selective_MLE(observed_target,
                                           cov_target,
@@ -190,58 +201,35 @@ class marginal_screening(object):
                                           self.observed_opt_state,
                                           solve_args=solve_args)
 
-    def selected_targets(self, features=None, dispersion=None):
+    def form_targets(self, features):
 
-        X, y = self.data
-        n, p = X.shape
-
-        overall = self._selected
-        score_linear = -X.T.dot(X[:, overall])
-        Q = -score_linear[overall]
+        score_linear = -self.covariance[:,features]
+        Q = -score_linear[features]
         cov_target = np.linalg.inv(Q)
-        observed_target = np.linalg.inv(Q).dot(X[:, overall].T.dot(y))
+        observed_target = np.linalg.inv(Q).dot(-self.observed_score[features])
         crosscov_target_score = score_linear.dot(cov_target)
-        alternatives = ([{1: 'greater', -1: 'less'}[int(s)] for s in self.selection_variable['sign'][overall]])
+        alternatives = ([{1: 'greater', -1: 'less'}[int(s)] for s in 
+                         self.selection_variable['sign'][features]])
 
-        return observed_target, cov_target * dispersion, crosscov_target_score.T * dispersion, alternatives
-
-    @staticmethod
-    def gaussian(X,
-                 Y,
-                 sigma = 1.,
-                 level = 0.10,
-                 randomizer_scale=None):
-
-        n, p = X.shape
-        mean_diag = np.mean((X ** 2).sum(0))
-
-        if randomizer_scale is None:
-            randomizer_scale = np.sqrt(mean_diag) * 0.5 * np.std(Y) * np.sqrt(n / (n - 1.))
-
-        sigma_hat = np.sqrt((sigma**2.) * (np.diag(X.T.dot(X))) + (randomizer_scale**2.))
-
-        return marginal_screening(X, Y, sigma_hat, randomizer_scale, level)
+        return observed_target, cov_target, crosscov_target_score.T, alternatives
 
 class BH(marginal_screening):
 
     def __init__(self,
-                 X,
-                 Y,
-                 sigma_hat, # SDs of each score
+                 observed_score,
+                 covariance, 
                  randomizer_scale,
                  BH_level,
                  perturb=None):
 
-        observed_score = -X.T.dot(Y)
+        self.observed_score = observed_score
         self.nfeature = p = observed_score.shape[0]
-        self.sigma_hat = sigma_hat
-
+        self.covariance = covariance
+        self.randomized_stdev = np.sqrt(np.diag(covariance) + randomizer_scale**2)
         self.randomizer = randomization.isotropic_gaussian((p,), randomizer_scale)
         self._initial_omega = perturb
-        self.observed_score = observed_score
 
         self.BH_level = BH_level
-        self.data = (X, Y)
 
     def fit(self, perturb=None):
 
@@ -254,9 +242,10 @@ class BH(marginal_screening):
             self._initial_omega = self.randomizer.sample()
 
         randomized_score = -self.observed_score + self._initial_omega
-        p_values = 2. * (1. - ndist.cdf(np.abs(randomized_score) / self.sigma_hat))
+        p_values = 2. * (1. - ndist.cdf(np.abs(randomized_score) / self.randomized_stdev))
         K, active, sort_notsel_pvals = BH_selection(p_values, self.BH_level)
-        BH_cutoff = self.sigma_hat * ndist.ppf(1. - (K * self.BH_level) /(2.*p))
+        BH_cutoff = self.randomized_stdev * ndist.ppf(1. - (K * self.BH_level) /(2.*p))
+
         if np.array(BH_cutoff).shape in [(), (1,)]:
             BH_cutoff = np.ones(p) * BH_cutoff
         self.BH_cutoff = BH_cutoff
@@ -271,15 +260,18 @@ class BH(marginal_screening):
 
         threshold = np.zeros(p)
         threshold[self._selected] = self.BH_cutoff[self._selected]
-        cut_off_vector = ndist.ppf(1. - ((K+np.arange(self._not_selected.sum())+1) * self.BH_level)/float(2.* p))
+        cut_off_vector = ndist.ppf(1. - ((K + np.arange(self._not_selected.sum()) + 1)
+                                         * self.BH_level) / float(2.* p))
 
         indices_interior = np.asarray([u for u in range(p) if self._not_selected[u]])
-        threshold[indices_interior[sort_notsel_pvals]] = (self.sigma_hat[self._not_selected])[sort_notsel_pvals] * cut_off_vector
+        threshold[indices_interior[sort_notsel_pvals]] = \
+            (self.randomized_stdev[self._not_selected])[sort_notsel_pvals] * cut_off_vector
 
         self.threshold = threshold
 
-        self.observed_opt_state = self._initial_omega[self._selected] - self.observed_score[self._selected] - \
-                                  np.diag(active_signs).dot(self.threshold[self._selected])
+        self.observed_opt_state = (self._initial_omega[self._selected] - 
+                                   self.observed_score[self._selected] - 
+                                  np.diag(active_signs).dot(self.threshold[self._selected]))
         self.num_opt_var = self.observed_opt_state.shape[0]
 
         opt_linear = np.zeros((p, self.num_opt_var))
@@ -288,7 +280,6 @@ class BH(marginal_screening):
         opt_offset[self._selected] = active_signs * self.threshold[self._selected]
         opt_offset[self._not_selected] = randomized_score[self._not_selected]
 
-        print("check", (np.abs(opt_offset[self._not_selected])< threshold[self._not_selected]).sum(), self._not_selected.sum())
         self.opt_transform = (opt_linear, opt_offset)
 
         cov, prec = self.randomizer.cov_prec
@@ -298,7 +289,9 @@ class BH(marginal_screening):
         cond_mean = -logdens_linear.dot(self.observed_score + opt_offset)
 
         logdens_transform = (logdens_linear, opt_offset)
-        A_scaling = -np.identity(self.num_opt_var)
+        print(active_signs)
+
+        A_scaling = -np.diag(active_signs)
         b_scaling = np.zeros(self.num_opt_var)
 
         def log_density(logdens_linear, offset, cond_prec, score, opt):
@@ -324,20 +317,4 @@ class BH(marginal_screening):
                                                selection_info=self.selection_variable)
         return self._selected
 
-    @staticmethod
-    def gaussian(X,
-                 Y,
-                 sigma = 1.,
-                 level = 0.10,
-                 randomizer_scale=None):
-
-        n, p = X.shape
-        mean_diag = np.mean((X ** 2).sum(0))
-
-        if randomizer_scale is None:
-            randomizer_scale = np.sqrt(mean_diag) * 0.5 * np.std(Y) * np.sqrt(n / (n - 1.))
-
-        sigma_hat = np.sqrt((sigma ** 2.) * (np.diag(X.T.dot(X))) + (randomizer_scale**2.))
-
-        return BH(X, Y, sigma_hat, randomizer_scale, level)
 
