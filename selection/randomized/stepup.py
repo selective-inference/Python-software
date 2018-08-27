8ae0cc7c414464509f5dcc706c480c1be5132f02
Jonathan Taylor <jonathan.taylor@stanford.edu>
2018-08-27 16:05:49 -0700

    WIP, stepup.BH running OK, but coverage not correct

--- selection/randomized/stepup.py
+++ selection/randomized/stepup.py
@@ -15,13 +15,17 @@ from ..algorithms.debiased_lasso import debiasing_matrix
 def stepup_selection(Z_values, stepup_Z):
 
     m = Z_values.shape[0]
-    absZ_sorted = np.sort(np.fabs(Z_values))[::-1]
+    absZ_argsort = np.argsort(np.fabs(Z_values))[::-1]
+    absZ_sorted = np.fabs(Z_values)[absZ_argsort]
     survivors = absZ_sorted - stepup_Z >= 0
     if np.any(survivors):
-        last_survivor = np.max(np.nonzero(survivors)[0])
-        return last_survivor + 1
+        num_selected = np.max(np.nonzero(survivors)[0]) + 1
+        return (num_selected,                    # how many selected
+                absZ_argsort[:num_selected],     # ordered indices of those selected
+                max(stepup_Z[num_selected - 1],  # the last selected is greater than this number 
+                    absZ_sorted[num_selected]))  # conditional on the unselected ones
     else:
-        return 0
+        return 0, None, None
 
 class stepup(marginal_screening):
 
@@ -46,7 +50,8 @@ class stepup(marginal_screening):
 
     def fit(self, perturb=None):
 
-        # condition on all those that survive
+        # condition on all those that survive, their sign,
+        # which was the last past the threshold
         # and the observed (randomized) Z values of those that don't
 
         p = self.nfeature
@@ -59,72 +64,103 @@ class stepup(marginal_screening):
 
         _randomized_score = -self.observed_score_state + self._initial_omega
 
-        K = stepup_selection(_randomized_score, self.step_Z)
-        if K < p:
-            Z_cutoff = self.step_Z[K] # or K \pm 1? check!
-        else:
-            Z_cutoff = 0
+        K, selected_idx, last_cutoff = stepup_selection(_randomized_score, self.step_Z)
 
-        self._selected = np.fabs(_randomized_score) > Z_cutoff
-        self._not_selected = ~self._selected
-        sign = np.sign(_randomized_score)
-        active_signs = sign[self._selected]
-        sign[self._not_selected] = 0
-        self.selection_variable = {'sign': sign.copy(),
-                                   'variables': self._selected.copy(),
-                                   # also conditioning on values of randomized unselected coefficients
-                                   }
+        if K > 0:
 
-        self.observed_opt_state = np.fabs(_randomized_score[self._selected]) - Z_cutoff 
+            _selected_first = np.zeros(p, np.bool)
+            _selected_first[selected_idx[:-1]] = 1
+            _selected_first = np.nonzero(_selected_first)[0]
+            _selected_last = selected_idx[-1]
 
-        self.num_opt_var = self.observed_opt_state.shape[0]
-        opt_linear = np.zeros((p, self.num_opt_var))
-        opt_linear[self._selected,:] = np.identity(self.num_opt_var)
-        opt_offset = np.zeros(p)
-        opt_offset[self._selected] = active_signs * Z_cutoff
-        opt_offset[self._not_selected] = _randomized_score[self._not_selected]
+            self._selected = np.zeros(p, np.bool)
+            self._selected[selected_idx] = 1
+            self._not_selected = ~self._selected
 
-        self.opt_transform = (opt_linear, opt_offset)
-        self._setup = True
+            sign = np.sign(_randomized_score)
+            active_signs_first = sign[_selected_first]
+            active_sign_last = sign[_selected_last]
+            active_signs = sign[self._selected]
+            sign[self._not_selected] = 0
+            self.selection_variable = {'sign': sign.copy(),
+                                       'variables': self._selected.copy(),
+                                       'last':_selected_last,
+                                       # also conditioning on values of randomized unselected coefficients
+                                       }
 
-        _, prec = self.randomizer.cov_prec
+            self.num_opt_var = self._selected.sum()
+            self.observed_opt_state = np.zeros(self.num_opt_var)
+            self.observed_opt_state[:-1] = np.fabs(_randomized_score[_selected_first]) - last_cutoff
+            self.observed_opt_state[-1] = np.fabs(_randomized_score[_selected_last]) - last_cutoff
 
-        if np.asarray(prec).shape in [(), (0,)]:
-            cond_precision = opt_linear.T.dot(opt_linear) * prec
-            cond_cov = np.linalg.inv(cond_precision)
-            logdens_linear = cond_cov.dot(opt_linear.T) * prec  
-        else:
-            cond_precision = opt_linear.T.dot(prec.dot(opt_linear))
-            cond_cov = np.linalg.inv(cond_precision)
-            logdens_linear = cond_cov.dot(opt_linear.T).dot(prec)
+            opt_linear = np.zeros((p, self.num_opt_var))
+            for j in range(self.num_opt_var - 1):
+                opt_linear[_selected_first[j], j] = active_signs_first[j]
+            opt_linear[_selected_last,-1] = active_sign_last
 
-        cond_mean = logdens_linear.dot(-self.observed_score_state - opt_offset)
-        logdens_transform = (logdens_linear, opt_offset)
-        A_scaling = -np.identity(len(active_signs))
-        b_scaling = np.zeros(self.num_opt_var)
+            opt_offset = np.zeros(p)
+            opt_offset[self._selected] = active_signs * last_cutoff
+            opt_offset[self._not_selected] = _randomized_score[self._not_selected]
 
-        def log_density(logdens_linear, offset, cond_prec, score, opt):
-            if score.ndim == 1:
-                mean_term = logdens_linear.dot(score.T + offset).T
+            self.opt_transform = (opt_linear, opt_offset)
+            self._setup = True
+
+            _, prec = self.randomizer.cov_prec
+
+            if np.asarray(prec).shape in [(), (0,)]:
+                cond_precision = opt_linear.T.dot(opt_linear) * prec
+                cond_cov = np.linalg.inv(cond_precision)
+                logdens_linear = cond_cov.dot(opt_linear.T) * prec  
             else:
-                mean_term = logdens_linear.dot(score.T + offset[:, None]).T
-            arg = opt + mean_term
-            return - 0.5 * np.sum(arg * cond_prec.dot(arg.T).T, 1)
+                cond_precision = opt_linear.T.dot(prec.dot(opt_linear))
+                cond_cov = np.linalg.inv(cond_precision)
+                logdens_linear = cond_cov.dot(opt_linear.T).dot(prec)
 
-        log_density = functools.partial(log_density, logdens_linear, opt_offset, cond_precision)
+            cond_mean = logdens_linear.dot(-self.observed_score_state - opt_offset)
+            logdens_transform = (logdens_linear, opt_offset)
 
-        affine_con = constraints(A_scaling,
-                                 b_scaling,
-                                 mean=cond_mean,
-                                 covariance=cond_cov)
+            # identify which is the smallest
+            # and that it is non-negative
+            A_scaling = []
+            for i in range(self.num_opt_var - 1):
+                row = np.zeros(self.num_opt_var)
+                row[-1] = 1
+                row[i] = -1.
+                A_scaling.append(row)
+            last_row = np.zeros(self.num_opt_var)
+            last_row[-1] = -1
+            A_scaling.append(last_row)
+            A_scaling = np.array(A_scaling)
 
-        self.sampler = affine_gaussian_sampler(affine_con,
-                                               self.observed_opt_state,
-                                               self.observed_score_state,
-                                               log_density,
-                                               logdens_transform,
-                                               selection_info=self.selection_variable)
-        return self._selected
+            b_scaling = np.zeros(self.num_opt_var)
+
+            if not np.all(A_scaling.dot(self.observed_opt_state) - b_scaling <= 0):
+                raise ValueError('constraints not satisfied')
+
+            def log_density(logdens_linear, offset, cond_prec, score, opt):
+                if score.ndim == 1:
+                    mean_term = logdens_linear.dot(score.T + offset).T
+                else:
+                    mean_term = logdens_linear.dot(score.T + offset[:, None]).T
+                arg = opt + mean_term
+                return - 0.5 * np.sum(arg * cond_prec.dot(arg.T).T, 1)
+
+            log_density = functools.partial(log_density, logdens_linear, opt_offset, cond_precision)
+
+            affine_con = constraints(A_scaling,
+                                     b_scaling,
+                                     mean=cond_mean,
+                                     covariance=cond_cov)
+
+            self.sampler = affine_gaussian_sampler(affine_con,
+                                                   self.observed_opt_state,
+                                                   self.observed_score_state,
+                                                   log_density,
+                                                   logdens_transform,
+                                                   selection_info=self.selection_variable)
+            return self._selected
+        else:
+            return None
 
     @staticmethod
     def BH(observed_score,
