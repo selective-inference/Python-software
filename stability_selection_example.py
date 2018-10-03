00ec127af5e3fdbfa879cfa38336a06257578ebe
Jonathan Taylor <jonathan.taylor@stanford.edu>
2018-10-02 17:54:50 -0700

    file name for stability

--- stability_selection_example.py
+++ stability_selection_example.py
@@ -151,11 +151,7 @@ if __name__ == "__main__":
             plt.plot(U, sm.distributions.ECDF(naive_P)(U), 'b', linewidth=3, label='Naive')
             plt.plot([0,1], [0,1], 'k--', linewidth=2)
             plt.legend()
-<<<<<<< HEAD
             plt.savefig('ss_example.pdf')
-=======
-            plt.savefig('lasso_example7_split.pdf')
->>>>>>> added fit_args argument
 
             df = pd.DataFrame({'coverage':coverage,
                                'pval':P,
