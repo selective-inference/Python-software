13a4e7e897af08f627f9f9ec268dfed24da68e4e
Jonathan Taylor <jonathan.taylor@stanford.edu>
2018-12-12 01:22:20 -0800

    fixing imports using sklearn

--- learn_selection/fitters.py
+++ learn_selection/fitters.py
@@ -8,7 +8,7 @@ rpy.r('library(splines)')
 rpy.r('library(gbm)')
 rpy.r('library(randomForest)')
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import ensemble
 
 def logit_fit(T, Y, df=20):
     rpy2.robjects.numpy2ri.activate()
@@ -143,13 +143,29 @@ def gbm_fit(T, Y, ntrees=5000):
 
     return fitfns
 
-def random_forest_fit_sk(T, Y, ntrees=5000, **rf_kwargs):
+def gbm_fit_sk(T, Y, **params):
 
     fitfns = []
     for j in range(Y.shape[1]):
         print('variable %d' % (j+1,))
         y = Y[:,j].astype(np.int)
-        clf = RandomForestClassifier(n_estimators=ntrees, **rf_kwargs)
+        clf = ensemble.GradientBoostingClassifier(**params)
+        clf.fit(T, y)
+
+        def fit_fn(clf, t):
+            return clf.predict_proba(t)[:,1]
+
+        fitfns.append(functools.partial(fit_fn, clf))
+
+    return fitfns
+
+def random_forest_fit_sk(T, Y, **params):
+
+    fitfns = []
+    for j in range(Y.shape[1]):
+        print('variable %d' % (j+1,))
+        y = Y[:,j].astype(np.int)
+        clf = ensemble.RandomForestClassifier(**params)
         clf.fit(T, y)
 
         def fit_fn(clf, t):
