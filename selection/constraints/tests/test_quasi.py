eb287df81c19cabc0fbf542e9ea24f87fa6a47d6
Jonathan Taylor <jonathan.taylor@stanford.edu>
2019-09-17 13:09:34 -0700

    more Rfitters changes

--- selection/constraints/tests/test_quasi.py
+++ selection/constraints/tests/test_quasi.py
@@ -8,20 +8,28 @@ from __future__ import division, print_function
 import nose.tools as nt
 import numpy as np
 
-from selection.constraints.quasi_affine import (quadratic_inequality_solver, intersection, sqrt_inequality_solver)
-from selection.tests.flags import SET_SEED
-from selection.tests.decorators import set_seed_iftrue
+from ..quasi_affine import (quadratic_inequality_solver, 
+                            intersection, 
+                            sqrt_inequality_solver)
+from ...tests.flags import SET_SEED
+from ...tests.decorators import set_seed_iftrue
 
 def test_quadratic_solver():
     yield np.testing.assert_almost_equal, quadratic_inequality_solver(7,0.,-28),[[-2.0,2.0]]
-    yield np.testing.assert_almost_equal, quadratic_inequality_solver(1,-1,-5.),[[-1.7912878474779199, 2.7912878474779199]]
-    yield np.testing.assert_almost_equal, quadratic_inequality_solver(1,-1,5.), [[]]
-    yield np.testing.assert_almost_equal, quadratic_inequality_solver(-1,-1,-5.), [[float("-inf"), float("inf")]]
-    yield np.testing.assert_almost_equal, quadratic_inequality_solver(-1,6,-5.), [[float("-inf"), 1.0], [5.0, float("inf")]]
-    yield np.testing.assert_almost_equal, quadratic_inequality_solver(0.,6,-5.),[[float("-inf"), 0.8333333333333334]]
-    yield np.testing.assert_almost_equal, quadratic_inequality_solver(0.,6,5.),[[float("-inf"), -0.8333333333333334]]
+    yield (np.testing.assert_almost_equal, quadratic_inequality_solver(1,-1,-5.),
+          [[-1.7912878474779199, 2.7912878474779199]])
+    yield (np.testing.assert_almost_equal, quadratic_inequality_solver(1,-1,5.), [[]])
+    yield (np.testing.assert_almost_equal, quadratic_inequality_solver(-1,-1,-5.), 
+           [[float("-inf"), float("inf")]])
+    yield (np.testing.assert_almost_equal, 
+           quadratic_inequality_solver(-1,6,-5.), [[float("-inf"), 1.0], [5.0, float("inf")]])
+    yield (np.testing.assert_almost_equal, quadratic_inequality_solver(0.,6,-5.),
+           [[float("-inf"), 0.8333333333333334]])
+    yield (np.testing.assert_almost_equal, 
+           quadratic_inequality_solver(0.,6,5.),[[float("-inf"), -0.8333333333333334]])
     yield nt.assert_raises, ValueError, quadratic_inequality_solver, 0., 0., 5.
-    yield np.testing.assert_almost_equal, quadratic_inequality_solver(1,3,2,"greater than"), [[float("-inf"), -2.], [-1., float("inf")]]
+    yield (np.testing.assert_almost_equal, 
+           quadratic_inequality_solver(1,3,2,"greater than"), [[float("-inf"), -2.], [-1., float("inf")]])
 
 def test_intersection():
     yield np.testing.assert_almost_equal, intersection([], []), []
@@ -42,7 +50,8 @@ def test_sqrt_solver():
     for x in np.linspace(-20, 20):
         hold = (func(x, a, b, c, n) <= 0)
         in_interval = any([contains(x, I) for I in intervals])
-        yield np.testing.assert_almost_equal, np.array(hold, np.float), np.array(in_interval, np.float)
+        yield (np.testing.assert_almost_equal, np.array(hold, np.float), 
+               np.array(in_interval, np.float))
 
 
 def contains(x, I):
