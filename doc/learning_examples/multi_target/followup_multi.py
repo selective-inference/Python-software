32502b38606e36149b8c4ba656e8f28044d8ed6a
Jonathan Taylor <jonathan.taylor@stanford.edu>
2020-02-22 11:03:53 -0800

    updated C software for cox, wrapper

--- doc/learning_examples/multi_target/followup_multi.py
+++ doc/learning_examples/multi_target/followup_multi.py
@@ -5,11 +5,13 @@ from scipy.stats import norm as ndist
 
 import regreg.api as rr
 
-from selection.tests.instance import gaussian_instance
+from selectinf.tests.instance import gaussian_instance
 
-from selection.learning.utils import full_model_inference, pivot_plot, naive_full_model_inference
-from selection.learning.core import normal_sampler, keras_fit
-from selection.learning.Rutils import lasso_glmnet
+from selectinf.learning.utils import (full_model_inference, 
+                                      pivot_plot, 
+                                      split_full_model_inference)
+from selectinf.learning.core import normal_sampler, keras_fit
+from selectinf.learning.Rutils import lasso_glmnet
 
 def simulate(n=400, p=100, s=10, signal=(0.5, 1), sigma=2, alpha=0.1, seed=0, B=2000):
 
@@ -33,10 +35,11 @@ def simulate(n=400, p=100, s=10, signal=(0.5, 1), sigma=2, alpha=0.1, seed=0, B=
     covS = dispersion * X.T.dot(X)
     smooth_sampler = normal_sampler(S, covS)
 
-    def meta_algorithm(X, XTXi, resid, sampler):
+    idx = np.random.choice(np.arange(n), int(n)/2, replace=False)
+
+    def meta_algorithm(X, XTXi, resid, idx, sampler):
 
         n, p = X.shape
-        idx = np.random.choice(np.arange(n), 200, replace=False)
 
         S = sampler(scale=0.) # deterministic with scale=0
         ynew = X.dot(XTXi).dot(S) + resid # will be ok for n>p and non-degen X
@@ -50,7 +53,7 @@ def simulate(n=400, p=100, s=10, signal=(0.5, 1), sigma=2, alpha=0.1, seed=0, B=
     resid = y - X.dot(XTXi.dot(X.T.dot(y)))
     dispersion = np.linalg.norm(resid)**2 / (n-p)
                          
-    selection_algorithm = functools.partial(meta_algorithm, X, XTXi, resid)
+    selection_algorithm = functools.partial(meta_algorithm, X, XTXi, resid, idx)
 
     # run selection algorithm
 
@@ -68,35 +71,15 @@ def simulate(n=400, p=100, s=10, signal=(0.5, 1), sigma=2, alpha=0.1, seed=0, B=
     if df is not None:
 
         observed_set = list(df['variable'])
-        true_target = truth[observed_set]
-
-        np.random.seed(seed)
-        X2, _, _ = gaussian_instance(n=n,
-                                     p=p, 
-                                     s=s,
-                                     equicorrelated=False,
-                                     rho=0.5, 
-                                     sigma=sigma,
-                                     signal=signal,
-                                     random_signs=True,
-                                     center=False,
-                                     scale=False)[:3]
-        stage_1 = np.random.choice(np.arange(n), 200, replace=False)
-        stage_2 = sorted(set(range(n)).difference(stage_1))
-        X2 = X2[stage_2]
-        y2 = X2.dot(truth) + sigma * np.random.standard_normal(X2.shape[0])
-
-        XTXi_2 = np.linalg.inv(X2.T.dot(X2))
-        resid2 = y2 - X2.dot(XTXi_2.dot(X2.T.dot(y2)))
-        dispersion_2 = np.linalg.norm(resid2)**2 / (X2.shape[0] - X2.shape[1])
-
-        naive_df = naive_full_model_inference(X2,
-                                              y2,
-                                              dispersion_2,
+        split_df = split_full_model_inference(X, 
+                                              y,
+                                              idx,
+                                              dispersion,
+                                              truth,
                                               observed_set,
                                               alpha=alpha)
 
-        df = pd.merge(df, naive_df, on='variable')
+        df = pd.merge(df, split_df, on='variable')
         return df
 
 if __name__ == "__main__":
@@ -107,7 +90,7 @@ if __name__ == "__main__":
     iseed = int(np.fabs(np.random.standard_normal() * 1000))
     for i in range(500):
         df = simulate(seed=i+iseed, B=2000)
-        csvfile = 'followup_multi.csv'
+        csvfile = __file__[:-3] + '.csv'
         outbase = csvfile[:-4]
 
         if df is not None and i > 0:
@@ -119,6 +102,7 @@ if __name__ == "__main__":
             df.to_csv(csvfile, index=False)
 
             if len(df['pivot']) > 0:
-                pivot_plot(df, outbase)
+                f = pivot_plot(df, outbase)
+                f.close()
 
 
