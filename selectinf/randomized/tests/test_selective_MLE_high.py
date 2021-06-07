a4c47ba9791b49473a28b7a955eaefdef58aedf5
Snigdha Panigrahi <psnigdha@stat-psnigdha-mbp.local>
2021-06-07 12:18:31 -0400

    MLE code updated

--- selectinf/randomized/tests/test_selective_MLE_high.py
+++ selectinf/randomized/tests/test_selective_MLE_high.py
@@ -4,12 +4,14 @@ import nose.tools as nt
 from selectinf.randomized.lasso import lasso, full_targets, selected_targets, debiased_targets
 from selectinf.tests.instance import gaussian_instance
 
-def test_full_targets(n=200, 
-                      p=1000, 
-                      signal_fac=0.5, 
-                      s=5, sigma=3, 
-                      rho=0.4, 
-                      randomizer_scale=0.5,
+
+def test_full_targets(n=200,
+                      p=1000,
+                      signal_fac=0.5,
+                      s=5,
+                      sigma=3,
+                      rho=0.4,
+                      randomizer_scale=0.7,
                       full_dispersion=False):
     """
     Compare to R randomized lasso
@@ -22,7 +24,7 @@ def test_full_targets(n=200,
                           p=p,
                           signal=signal,
                           s=s,
-                          equicorrelated=False,
+                          equicorrelated=True,
                           rho=rho,
                           sigma=sigma,
                           random_signs=True)[:3]
@@ -51,7 +53,7 @@ def test_full_targets(n=200,
             else:
                 dispersion = None
 
-            if n>p:
+            if n > p:
                 (observed_target,
                  cov_target,
                  cov_target_score,
@@ -81,13 +83,13 @@ def test_full_targets(n=200,
             return pval[beta[nonzero] == 0], pval[beta[nonzero] != 0], coverage, intervals
 
 
-def test_selected_targets(n=2000, 
-                          p=200, 
-                          signal_fac=1.,
-                          s=5, 
-                          sigma=3, 
-                          rho=0.4, 
-                          randomizer_scale=1,
+def test_selected_targets(n=2000,
+                          p=200,
+                          signal_fac=1.2,
+                          s=5,
+                          sigma=2,
+                          rho=0.7,
+                          randomizer_scale=1.,
                           full_dispersion=True):
     """
     Compare to R randomized lasso
@@ -101,7 +103,7 @@ def test_selected_targets(n=2000,
                           p=p,
                           signal=signal,
                           s=s,
-                          equicorrelated=False,
+                          equicorrelated=True,
                           rho=rho,
                           sigma=sigma,
                           random_signs=True)[:3]
@@ -113,11 +115,12 @@ def test_selected_targets(n=2000,
         n, p = X.shape
 
         sigma_ = np.std(Y)
-        W = np.ones(X.shape[1]) * np.sqrt(2 * np.log(p)) * sigma_
+        W = 0.8 * np.ones(X.shape[1]) * np.sqrt(2 * np.log(p)) * sigma_
 
         conv = const(X,
                      Y,
                      W,
+                     ridge_term=0.,
                      randomizer_scale=randomizer_scale * sigma_)
 
         signs = conv.fit()
@@ -134,7 +137,7 @@ def test_selected_targets(n=2000,
              cov_target_score,
              alternatives) = selected_targets(conv.loglike,
                                               conv._W,
-                                              nonzero, 
+                                              nonzero,
                                               dispersion=dispersion)
 
             result = conv.selective_MLE(observed_target,
@@ -143,18 +146,17 @@ def test_selected_targets(n=2000,
             estimate = result['MLE']
             pval = result['pvalue']
             intervals = np.asarray(result[['lower_confidence', 'upper_confidence']])
-            
+
             beta_target = np.linalg.pinv(X[:, nonzero]).dot(X.dot(beta))
 
             coverage = (beta_target > intervals[:, 0]) * (beta_target < intervals[:, 1])
 
-            print("observed_opt_state ", conv.observed_opt_state)
             # print("check ", np.asarray(result['MLE']), np.asarray(result['unbiased']))
 
             return pval[beta[nonzero] == 0], pval[beta[nonzero] != 0], coverage, intervals
 
-def test_instance():
 
+def test_instance():
     n, p, s = 500, 100, 5
     X = np.random.standard_normal((n, p))
     beta = np.zeros(p)
@@ -169,6 +171,7 @@ def test_instance():
 
     M = E.copy()
     M[-3:] = 1
+    print("check ", M)
     dispersion = np.linalg.norm(Y - X[:, M].dot(np.linalg.pinv(X[:, M]).dot(Y))) ** 2 / (n - M.sum())
     (observed_target,
      cov_target,
@@ -191,9 +194,10 @@ def test_instance():
 
     coverage = (beta_target > intervals[:, 0]) * (beta_target < intervals[:, 1])
     print("observed_opt_state ", L.observed_opt_state)
-    #print("check ", np.asarray(result['MLE']), np.asarray(result['unbiased']))
+    # print("check ", np.asarray(result['MLE']), np.asarray(result['unbiased']))
 
     return coverage
+
 
 # def main(nsim=500):
 #
@@ -279,7 +283,7 @@ def main(nsim=500, full=False):
     P0, PA, cover, length_int = [], [], [], []
     from statsmodels.distributions import ECDF
 
-    n, p, s = 500, 100, 5
+    n, p, s = 500, 100, 0
 
     for i in range(nsim):
         if full:
@@ -291,8 +295,7 @@ def main(nsim=500, full=False):
             avg_length = intervals[:, 1] - intervals[:, 0]
         else:
             full_dispersion = True
-            p0, pA, cover_, intervals = test_selected_targets_disperse(n=n, p=p, s=int(p/2),
-                                                              full_dispersion=full_dispersion)
+            p0, pA, cover_, intervals = test_selected_targets(n=n, p=p, s=s, full_dispersion=full_dispersion)
             avg_length = intervals[:, 1] - intervals[:, 0]
 
         cover.extend(cover_)
@@ -303,5 +306,6 @@ def main(nsim=500, full=False):
         #     np.mean(avg_length), 'null pvalue + power + length')
         print("coverage and lengths ", np.mean(cover), np.mean(avg_length))
 
+
 if __name__ == "__main__":
-    main(nsim=100)
+    main(nsim=50)
